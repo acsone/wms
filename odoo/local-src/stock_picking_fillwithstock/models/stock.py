24efd759f7b1a9591a7e42bba1e5b21ac97c8bc8
Jacques-Etienne Baudoux <je@bcim.be>
2016-11-17 15:20:32 +0100

    [FIX] flake8

--- odoo/local-src/stock_picking_fillwithstock/models/stock.py
+++ odoo/local-src/stock_picking_fillwithstock/models/stock.py
@@ -67,10 +67,12 @@ class StockPicking(models.Model):
 # class StockPackOperation(models.Model):
 #     _inherit = "stock.pack.operation"
 #
-#     def _get_remaining_prod_quantities(self, cr, uid, operation, context=None):
-#         # manage extra move creation. If operation is dummmy (and not related a
-#         # move), do not create move at picking validation
+#     def _get_remaining_prod_quantities(self, cr, uid, operation,
+#                                        context=None):
+#         # manage extra move creation. If operation is dummmy (and not related
+#         # a move), do not create move at picking validation
 #         if operation.location_id == operation.location_dest_id:
 #             return {}
-#         return super(StockPackOperation, self)._get_remaining_prod_quantities(
-#             cr, uid, operation, context=context)
+#         return super(StockPackOperation, self).\
+#             _get_remaining_prod_quantities(
+#               cr, uid, operation, context=context)
