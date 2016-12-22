e1aa16404c9c02676b5c62cc427efb32f7c2aec5
Sylvain Van Hoof <sylvainvh@gmail.com>
2016-12-22 09:34:09 +0100

    ALCN-699: Modify the report invoicing to fit with the client requirement
    (cherry picked from commit 261d2a1b10f37a9bfa92a2db21e7221775a49462)

--- odoo/local-src/specific_account/models/account_invoice.py
+++ odoo/local-src/specific_account/models/account_invoice.py
@@ -33,20 +33,44 @@ class AccountInvoice(models.Model):
         compute='_compute_total_amounts',
     )
 
+    amount_untaxed_with_contribution = fields.Monetary(
+        compute='_compute_total_amounts',
+    )
+
+    invoice_apb_ids = fields.Many2many('account.invoice.tax',
+                                       compute='_compute_total_amounts')
+    amount_apb = fields.Monetary(
+        compute='_compute_total_amounts'
+    )
+
+    invoice_contribution_ids = fields.Many2many('account.invoice.tax',
+                                       compute='_compute_total_amounts')
+    amount_contribution = fields.Monetary(
+        compute='_compute_total_amounts'
+    )
+
+    invoice_only_tax_ids = fields.Many2many('account.invoice.tax',
+                                       compute='_compute_total_amounts')
+    amount_only_tax = fields.Monetary(
+        compute='_compute_total_amounts'
+    )
+
+    amount_untaxed_with_contribution = fields.Monetary(
+        compute='_compute_total_amounts',
+    )
+
     @api.depends(
         'invoice_line_ids',
         'invoice_line_ids.quantity',
         'invoice_line_ids.price_unit',
         'invoice_line_ids.price_unit_supplier',
         'invoice_line_ids.price_unit_alcyon',
+        'tax_line_ids',
     )
     def _compute_total_amounts(self):
-        for inv in self:
-            inv.amount_without_discount = sum([
-                l.price_unit * l.quantity
-                for l in inv.invoice_line_ids
-            ])
+        tax_group_apb = self.env.ref('specific_account.tax_group_apb')
 
+        for inv in self:
             inv.amount_supplier_discount = sum([
                 (l.price_unit - l.price_unit_supplier) * l.quantity
                 for l in inv.invoice_line_ids
@@ -60,6 +84,36 @@ class AccountInvoice(models.Model):
             inv.amount_discount_total = (
                 inv.amount_supplier_discount + inv.amount_alcyon_discount
             )
+
+            amount_apb = amount_contribution = amount_only_tax = 0
+            invoice_only_tax_ids = self.env['account.invoice.tax']
+            invoice_contribution_ids = self.env['account.invoice.tax']
+            invoice_apb_ids = self.env['account.invoice.tax']
+
+            for invoice_tax in inv.tax_line_ids:
+                if invoice_tax.tax_id.include_base_amount:
+                    invoice_contribution_ids |= invoice_tax
+                    amount_contribution += invoice_tax.amount
+                elif invoice_tax.tax_id.tax_group_id == tax_group_apb:
+                    invoice_apb_ids |= invoice_tax
+                    amount_apb += invoice_tax.amount
+                else:
+                    invoice_only_tax_ids |= invoice_tax
+                    amount_only_tax += invoice_tax.amount
+            inv.amount_apb = amount_apb
+            inv.amount_contribution = amount_contribution
+            inv.amount_only_tax = amount_only_tax
+            inv.invoice_only_tax_ids = invoice_only_tax_ids
+            inv.invoice_contribution_ids = invoice_contribution_ids
+            inv.invoice_apb_ids = invoice_apb_ids
+
+            inv.amount_without_discount = sum([
+                                          l.price_unit * l.quantity
+                                          for l in inv.invoice_line_ids
+                                              ]) + amount_contribution
+
+            inv.amount_untaxed_with_contribution = \
+                inv.amount_untaxed + amount_contribution
 
     @api.multi
     def get_lines_by_sale(self):
@@ -114,3 +168,38 @@ class AccountInvoice(models.Model):
         values.sort(key=lambda line: line[0])
 
         return values
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    only_tax_ids = fields.Many2many('account.tax', compute='_compute_all_taxes')
+    contribution_ids = fields.Many2many('account.tax',
+                                         compute='_compute_all_taxes')
+    apb_ids = fields.Many2many('account.tax', compute='_compute_all_taxes')
+    amount_contribution = fields.Monetary(compute='_compute_all_taxes')
+
+    @api.multi
+    @api.depends('invoice_line_tax_ids')
+    def _compute_all_taxes(self):
+        tax_group_apb = self.env.ref('specific_account.tax_group_apb')
+
+        for line in self:
+            amount_contribution = 0
+            only_tax_ids = self.env['account.tax']
+            contribution_ids = self.env['account.tax']
+            apb_ids = self.env['account.tax']
+
+            for tax in line.invoice_line_tax_ids:
+                if tax.include_base_amount:
+                    amount_contribution += (tax.amount * line.quantity)
+                    contribution_ids |= tax
+                elif tax.tax_group_id == tax_group_apb:
+                    apb_ids |= tax
+                else:
+                    only_tax_ids |= tax
+
+            line.only_tax_ids = only_tax_ids
+            line.contribution_ids = contribution_ids
+            line.apb_ids = apb_ids
+            line.amount_contribution = amount_contribution
