d6cb200abdf0d667a5b1346a7fe97b74da1b0422
Sylvain Van Hoof <sylvainvh@gmail.com>
2016-12-22 17:09:51 +0100

    ALCN-669: Add the module stock_picking_assignment to assign a picking

---
+++ odoo/local-src/stock_picking_assignment/models/stock_picking.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Sylvain Van Hoof <svh@sylvainvh.be>
+#    Copyright (C) 2016
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import fields, models, api
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    operator_id = fields.Many2one('res.users', string='Operator')
+
+    @api.multi
+    def assign_operator(self):
+        self.write({
+            'operator_id': self.env.uid,
+            'printed': True,
+        })
