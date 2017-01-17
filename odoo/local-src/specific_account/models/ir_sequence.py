359f775712ef15b848681a5cdb3c6b993263c73e
Sylvain Van Hoof <sylvainvh@gmail.com>
2017-01-17 16:25:52 +0100

    ALCN-712: Remove useless import to respect OCA guideline

--- odoo/local-src/specific_account/models/ir_sequence.py
+++ odoo/local-src/specific_account/models/ir_sequence.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Sylvain Van Hoof
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import models, api, fields
+from openerp import models, fields
 
 
 class IrSequence(models.Model):
