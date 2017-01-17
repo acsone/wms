238cfe1d155647bf25342cd27545b859457171bd
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-01-17 10:34:10 +0100

    travis - Download public submodules from Github archive zip files in order to speed up builds

---
+++ travis/git_submodule_update.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Download submodules from Github zip archive url
+# Keep standard update form private repositories
+# listed in `travis/private_repo`
+#
+import os
+
+from git import Repo
+
+dl_dir = 'download'
+os.system('mkdir %s' % dl_dir)
+
+with open('travis/private_repos') as f:
+    private_repos = f.read()
+
+for sub in Repo('.').submodules:
+    if sub.path not in private_repos:
+        url = sub.url
+        if url.startswith('git@github.com:'):
+            url = url.replace('git@github.com:', 'https://github.com/')
+        # remove .git
+        if url.endswith('.git'):
+            url = url[:-4]
+        wget_archive_url = "wget %s/archive/%s.zip" % (url, sub.hexsha)
+        os.system(wget_archive_url)
+        os.system('unzip -q %s -d %s' % (sub.hexsha, dl_dir))
+        os.system('rm %s.zip' % sub.hexsha)
+        os.system('rmdir %s' % sub.path)
+        os.system('mv %s/* %s' % (dl_dir, sub.path))
+    else:
+        sub.update()
