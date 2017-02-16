bdff76a1e33229a67d6617ddc0173266ae2d8656
Sylvain Van Hoof <sylvainvh@gmail.com>
2017-02-16 13:13:46 +0100

    ALCN-191: Remove useless import

--- odoo/local-src/specific_account/models/account_invoice.py
+++ odoo/local-src/specific_account/models/account_invoice.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from collections import defaultdict
-
 from openerp import fields, models
 
 
--- odoo/local-src/specific_report/models/stock_move.py
+++ odoo/local-src/specific_report/models/stock_move.py
@@ -18,8 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from collections import defaultdict
-
 from openerp import models, fields, api
 
 
