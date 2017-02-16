22a293f5fac0d632db14b8eb5f2d5537da854d37
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-02-16 10:49:22 +0100

    Rename module title in manifest

--- odoo/local-src/stock_picking_assignment/__openerp__.py
+++ odoo/local-src/stock_picking_assignment/__openerp__.py
@@ -20,13 +20,13 @@
 ##############################################################################
 
 {
-    'name': 'Stock lot track',
+    'name': 'Stock Picking Assignment',
     'version': '9.0.1.0.0',
     'category': 'Stock Management',
     'author': 'Sylvain Van Hoof',
     'description':
     """
-    Stock lot track
+    Stock Picking Assignment
     """,
     'depends': [
         'stock',
