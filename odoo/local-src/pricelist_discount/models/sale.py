d60d85b60a255da70ff160b515770cecc74c6886
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-02-28 14:51:55 +0100

    Fix import of decimal precision

--- odoo/local-src/pricelist_discount/models/sale.py
+++ odoo/local-src/pricelist_discount/models/sale.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-from odoo.addons.decimal_precision import decimal_precision as dp
+import odoo.addons.decimal_precision as dp
 
 
 class SaleOrder(models.Model):
