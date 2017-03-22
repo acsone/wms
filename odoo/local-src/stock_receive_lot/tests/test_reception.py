f218e1e004addc5283d20e007579a31afbfae724
jcoux <julien.coux@camptocamp.com>
2017-03-22 08:01:56 +0100

    Change owner in tests of stock_receive_lot module

--- odoo/local-src/stock_receive_lot/tests/test_reception.py
+++ odoo/local-src/stock_receive_lot/tests/test_reception.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Julien Coux (Camptocamp)
+# Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase, post_install, at_install
