655f73c13c127a99a0e442c6b573de9ad971e2ff
Sylvain Van Hoof <sylvainvh@gmail.com>
2017-07-04 15:35:41 +0200

    HOTFIX: Fix an another bug when a sale order line is not linked to product and complete the HISTORY file

--- odoo/local-src/specific_sale/models/sale.py
+++ odoo/local-src/specific_sale/models/sale.py
@@ -379,6 +379,7 @@ class SaleOrderLine(models.Model):
             only_wh_stock_quants=True
         )
         for line in self:
+            production_lot_ids = None
             if line.product_id:
                 production_lot_ids = production_lot_model.search([
                     ('product_id', '=', line.product_id.id),
