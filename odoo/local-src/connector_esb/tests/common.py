a87e8c43207274998625282866eed5adcd575117
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-07-05 12:16:21 +0200

    Improve XML comparison output
    * Show all the errors, not the first one
    * Show missing elements and wrong content
    * Show number of times we have the error (e.g. if we have several rows,
      we might miss a element on every row.
    * Ignore comments in the expected XML, they are used as documentation
      only

--- odoo/local-src/connector_esb/tests/common.py
+++ odoo/local-src/connector_esb/tests/common.py
@@ -2,6 +2,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from collections import Counter
 from odoo.addons.component.tests.common import SavepointComponentCase
 
 import xmlunittest
@@ -34,6 +35,15 @@ class ESBXMLTestCase(ESBTestCase, xmlunittest.XmlTestMixin):
         """
         gxml = etree.fromstring(given)
         exml = etree.fromstring(expected)
+
+        # don't check the comments in the examples
+        comments = exml.xpath('//comment()')
+        for c in comments:
+            p = c.getparent()
+            p.remove(c)
+
+        missing_elems = Counter()
+        content_differs = Counter()
         for item in exml.xpath('//' + unique_key):
             # `item` is the el w/ unique_key
             # let's find its match in given xml and go up to the parent
@@ -41,5 +51,31 @@ class ESBXMLTestCase(ESBTestCase, xmlunittest.XmlTestMixin):
                 '//{}[text()="{}"]/..'.format(unique_key, item.text))
             assert parent
             # then compare all the values there
-            for el in item.getparent().iterchildren():
-                self.assertEqual(el.text, parent[0].find(el.tag).text)
+            for expected_elem in item.getparent().iterchildren():
+                elem = parent[0].find(expected_elem.tag)
+                if elem is None:
+                    missing_elems.update([expected_elem.tag])
+                else:
+                    if expected_elem.text != elem.text:
+                        content_differs.update(
+                            [(elem.tag, expected_elem.text, elem.text)]
+                        )
+
+        if missing_elems or content_differs:
+            message = []
+            if missing_elems:
+                message.append(u'Missing elements')
+            for tag, count in missing_elems.items():
+                message.append(u' - {} ({} times)'.format(tag, count))
+            if content_differs:
+                message.append(u'Wrong elements')
+            for (tag, expected, got), count in content_differs.items():
+                message.append(
+                    u" - in '{}' expect {!r}, got {!r} ({} times)".format(
+                        tag, expected, got, count
+                    )
+                )
+
+            raise AssertionError(u'XML does not match:\n\n{}'.format(
+                '\n'.join(message)
+            ))
