74f20780c38ec99bcff060527be00601e4ed0a07
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-07-11 14:01:39 +0200

    Create module specific_data

---
+++ odoo/local-src/specific_data/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Specific datas for Alcyon',
+    'version': '10.0.1.0.0',
+    'author': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'Base',
+    'depends': [
+        'base',
+    ],
+    'website': 'http://www.camptocamp.com',
+    'data': [
+    ],
+    'installable': True,
+}
