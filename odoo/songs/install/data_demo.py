935080fcd666660ad580e73f10b9581ee6caa725
Jacques-Etienne Baudoux <je@bcim.be>
2017-07-24 11:16:17 +0200

    [MOD] demo data

--- odoo/songs/install/data_demo.py
+++ odoo/songs/install/data_demo.py
@@ -146,9 +146,9 @@ def import_stock_bins(ctx):
 @anthem.log
 def import_delivery_round_config(ctx):
     """ Importing delivery round config from csv"""
-    content = resource_stream(req, 'data/demo/delivery_vehicle.csv')
+    content = resource_stream(req, 'data/demo/delivery_template.csv')
     load_csv_stream(ctx, 'round.vehicle', content, delimiter=',')
-    content = resource_stream(req, 'data/demo/delivery_zone.csv')
+    content = resource_stream(req, 'data/demo/delivery_itinerary.csv')
     load_csv_stream(ctx, 'round.zone', content, delimiter=',')
     content = resource_stream(req, 'data/demo/delivery_clients.csv')
     load_csv_stream(ctx, 'round.zone.position', content, delimiter=',')
