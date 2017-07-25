326308fec0fe0b7d093ebe6b63a23ffeed8fdd53
Sylvain Van Hoof <sylvainvh@gmail.com>
2017-07-25 16:28:06 +0200

    ALCN-923: Update purchase-workflow and remove useless import

--- odoo/local-src/specific_purchase/models/purchase_order_cancel.py
+++ odoo/local-src/specific_purchase/models/purchase_order_cancel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Okia SPRL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import models
 
 
 class PurchaseOrderCancel(models.TransientModel):
