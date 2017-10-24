17f590598101254fa08f360c0a39d004a8f67b4e
Sylvain Van Hoof <sylvainvh@gmail.com>
2017-10-24 10:03:13 +0200

    ALCN-946: Remove useless view (validated by Jacques-Etienne)

--- odoo/local-src/delivery_rounds/__manifest__.py
+++ odoo/local-src/delivery_rounds/__manifest__.py
@@ -24,7 +24,6 @@
         'views/picking.xml',
         'views/partner.xml',
         'views/delivery_carrier.xml',
-        'views/sale_order.xml',  # kept for update - TODO: del in next release
         'views/tag.xml',
         # Data
         'data/sequence.xml',
