18637b1010a72bc5d83912b0d92516f1de8f44c6
Sylvain Van Hoof <svh@sylvainvh.be>
2017-11-18 15:55:17 +0100

    ALCN-999: Fix some small bugs and update translations

--- odoo/local-src/delivery_rounds/models/cron_delivery_plan.py
+++ odoo/local-src/delivery_rounds/models/cron_delivery_plan.py
@@ -16,8 +16,12 @@ class CronDeliveryPlan(models.Model):
                                  (5, 'Friday'),
                                  (6, 'Saturday'),
                                  (7, 'Sunday')],
-                                string='Week day')
-    date_overwrite = fields.Date('Date overwrite')
+                                string='Week day',
+                                help='Set the day to execute the plan '
+                                     'each week')
+    date_overwrite = fields.Date('Date overwrite',
+                                 help='Set a date to execute the plan '
+                                      'only once time')
     next_execution = fields.Date('Next execution',
                                  compute='_compute_next_execution',
                                  store=True,
@@ -34,9 +38,6 @@ class CronDeliveryPlan(models.Model):
             if not line.active:
                 continue
 
-            if line.week_day and line.date_overwrite:
-                raise UserError(_('You cannot set a week day and a date '
-                                  'overwrite in the same time.'))
             if line.date_overwrite:
                 line.next_execution = line.date_overwrite
             elif line.week_day:
@@ -44,6 +45,16 @@ class CronDeliveryPlan(models.Model):
                 while today.isoweekday() != line.week_day:
                     today += relativedelta(days=1)
                 line.next_execution = fields.Date.to_string(today)
+
+    @api.constrains('week_day', 'date_overwrite')
+    def constrains_date(self):
+        for line in self:
+            if not line.week_day and not line.date_overwrite:
+                raise UserError(_('You have to set the week day '
+                                  'or the date overwrite'))
+            if line.week_day and line.date_overwrite:
+                raise UserError(_('You cannot set a week day and a date '
+                                  'overwrite in the same time.'))
 
     @api.model
     def create_daily_plan(self, today_overwrite=None):
@@ -54,11 +65,10 @@ class CronDeliveryPlan(models.Model):
         make_plan_wizard = self.env['round.wizard.makeplan']
 
         for daily_plan in daily_plans:
-            execution_date_str = daily_plan.next_execution
             wizard = make_plan_wizard.create({
                 'version_id': daily_plan.version_id.id,
                 'tag_ids': [(6, 0, daily_plan.tag_ids.ids)],
-                'execution_date': execution_date_str,
+                'execution_date': today_str,
                 'assign_moves': self.env.context.get('assign_moves', True),
             })
             wizard.confirm()
@@ -66,7 +76,7 @@ class CronDeliveryPlan(models.Model):
             if daily_plan.date_overwrite:
                 daily_plan.active = False
             else:
-                execution_date = fields.Date.from_string(execution_date_str)
+                execution_date = fields.Date.from_string(today_str)
                 next_execution = execution_date + relativedelta(days=7)
                 next_execution_str = fields.Date.to_string(next_execution)
 
