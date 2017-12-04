e25adae4faf135c0253a99dcbb1f92e09cd3bc16
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-12-04 08:55:36 +0100

    Disable lang install on CI to reduce travis build time

--- odoo/songs/install/pre.py
+++ odoo/songs/install/pre.py
@@ -2,6 +2,7 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+import os
 from base64 import b64encode
 from pkg_resources import resource_string
 
@@ -51,8 +52,11 @@ def setup_company(ctx):
 @anthem.log
 def setup_language(ctx):
     """ Installing language and configuring locale formatting """
-    for code in ('fr_BE', 'nl_BE', 'de_DE'):
-        ctx.env['base.language.install'].create({'lang': code}).lang_install()
+    # Skiping time-consuming installation language on CI
+    if not os.environ.get('CI'):
+        for code in ('fr_BE', 'nl_BE', 'de_DE'):
+            ctx.env['base.language.install'].create(
+                {'lang': code}).lang_install()
 
     ctx.env['res.lang'].search([]).write({
         'grouping': '[3, 0]',
