6cd918c1307fe537ab8743daef1d848db9b9f460
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-02-13 19:20:18 +0100

    import_db2 shell - Add option to copy content to a csv file

--- import_db2/shell.py
+++ import_db2/shell.py
@@ -4,23 +4,48 @@
 
 from IPython.terminal.embed import InteractiveShellEmbed
 import pyodbc
+import csv
+import sys
 
 cursor = None
 
+def create_csv_file(rows, filename):
+    print "Start generating csvfile"
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([i[0] for i in cursor.description])  # headers
+        i = 0
+        nb = len(rows)
+        for r in rows:
+            i += 1
+            progress = float(i) / nb
+            print "\r[ {0:40s} ] {1:.1f}%".format(
+                '#' * int(progress * 40),
+                progress * 100),
+            sys.stdout.flush()
+            writer.writerow(r)
+        print  # add newline
+    return "%s rows saved in %s" % (nb, filename)
 
-def fetchall_dict(query):
+
+def fetchall_dict(query, copy_to=None):
     """ Execute the *query* on db2 database and transform the results in
     a list of dict [{'column_name': value, ...}]
+
+    copy_to: allows to export in a csv file.
     """
     global cursor
     cursor.execute(query)
     rows = cursor.fetchall()
     if rows:
+        if copy_to:
+            return create_csv_file(rows, copy_to)
         rows = [
             {col.lower(): row[idx] for idx, col in enumerate(
                 [d[0] for d in row.cursor_description]
             )} for row in rows
         ]
+
     return rows
 
 
