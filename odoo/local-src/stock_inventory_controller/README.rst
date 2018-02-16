f16d1b490df71543ff703000e77c20d78793387c
Sylvain Van Hoof <svh@sylvainvh.be>
2018-02-16 16:03:45 +0100

    [FIX] Add a module to control inventory

---
+++ odoo/local-src/stock_inventory_controller/README.rst
@@ -0,0 +1,33 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==========================
+Stock Inventory Controller
+==========================
+
+Allow to execute an inventory and catch errors.
+
+Installation
+============
+
+There is no specific installation procedure for this module.
+
+Configuration
+=============
+
+/
+
+Usage
+=====
+
+Simple launch the inventory and watch the result
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sylvain Van Hoof <sylvain@okia.be>
---
+++ odoo/local-src/stock_inventory_controller/models/stock_inventory.py
@@ -0,0 +1,99 @@
+import logging
+
+from odoo import api, fields, models
+from odoo.exceptions import MissingError, UserError
+
+_logger = logging.getLogger(__name__)
+
+
+class StockInventoryLine(models.Model):
+    _inherit = 'stock.inventory.line'
+
+    is_line_failed = fields.Boolean(
+        'Line failed',
+        readonly=True,
+        default=False
+    )
+    fail_message = fields.Char(
+        'Fail message',
+        readonly=True,
+    )
+
+
+class StockInventory(models.Model):
+    _inherit = 'stock.inventory'
+
+    failed_line_ids = fields.One2many(
+        'stock.inventory.line',
+        'inventory_id',
+        string='Failed inventories',
+        domain=[('is_line_failed', '=', True)],
+        copy=False,
+        readonly=True,
+        states={'done': [('readonly', True)]}
+    )
+
+    line_ids = fields.One2many(
+        domain=[('is_line_failed', '=', False)]
+    )
+
+    @api.multi
+    def post_inventory(self):
+        """
+        This method has the api multi but this method is called for each line.
+        :return:
+        """
+        moves = self.mapped('move_ids').filtered(
+            lambda move: move.state != 'done')
+
+        for move in moves:
+            try:
+                move.action_done()
+            except MissingError as me:
+                _logger.error('MissingError: ' + str(me))
+                line = self.line_ids.search(
+                    [('product_id', '=', move.product_id.id)])
+                line.write({
+                    'is_line_failed': True,
+                    'fail_message': 'MissingError: ' + str(me)
+                })
+                pass
+            except UserError as ue:
+                _logger.error('UserError: ' + str(ue))
+                line = self.line_ids.search(
+                    [('product_id', '=', move.product_id.id)])
+                line.write({
+                    'is_line_failed': True,
+                    'fail_message': 'UserError: ' + str(ue)
+                })
+                pass
+            except Exception:
+                raise Exception
+
+    @api.multi
+    def _generate_moves(self):
+        """
+        This method has the api multi but this method is called for each line.
+        :return:
+        """
+        result = None
+        try:
+            result = super(StockInventoryLine, self)._generate_moves()
+        except MissingError as me:
+            _logger.error('MissingError: ' + str(me))
+            self.write({
+                'is_line_failed': True,
+                'fail_message': 'MissingError: ' + str(me)
+            })
+            pass
+        except UserError as ue:
+            _logger.error('UserError: ' + str(ue))
+            self.write({
+                'is_line_failed': True,
+                'fail_message': 'UserError: ' + str(ue)
+            })
+            pass
+        except Exception:
+            raise Exception
+
+        return result
