66dd58346f02b2c54b54fd88fc9727e388b942ad
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-03-16 11:53:59 +0100

    ALCN-1218: Changes on so line must send so through connector
    Two fields (qty_delivered, product_qty_canceled) when changed on a
    sale_order_line must activate the process of the sale order by the
    connector.
    And add the queue job channel for the esb.

--- odoo/local-src/connector_esb/models/sale_order/listener.py
+++ odoo/local-src/connector_esb/models/sale_order/listener.py
@@ -11,16 +11,32 @@ class SaleExportListener(Component):
     _inherit = 'base.connector.listener'
     _apply_on = ['sale.order']
 
-    EXPORT_DESCRIPTION = "Export a sales order to ESB's webservice"
+    EXPORT_DESCRIPTION = "Export sale order {} to ESB's webservice"
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_create(self, record, fields=None):
         record.with_delay(
-            description=self.EXPORT_DESCRIPTION
+            description=self.EXPORT_DESCRIPTION.format(record.name or '')
         ).esb_export_record()
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_write(self, record, fields=None):
         record.with_delay(
-            description=self.EXPORT_DESCRIPTION
+            description=self.EXPORT_DESCRIPTION.format(record.name)
         ).esb_export_record()
+
+
+class SaleLineExportListener(Component):
+    _name = 'esb.sale.order.line.export.listener'
+    _inherit = 'base.connector.listener'
+    _apply_on = ['sale.order.line']
+
+    EXPORT_DESCRIPTION = "Export sale order {} to ESB's webservice"
+
+    @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+    def on_record_write(self, record, fields=None):
+        if set(fields) & set(['qty_delivered', 'product_qty_unavailable']):
+            so = record.order_id
+            so.with_delay(
+                description=self.EXPORT_DESCRIPTION.format(so.name or '')
+            ).esb_export_record()
