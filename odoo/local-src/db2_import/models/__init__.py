9c8560827ca9d757bdd07812e83bcd1082534bbb
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-04-11 16:11:01 +0200

    ALCN-1173 Use job to initialize and start esb connector

--- odoo/local-src/db2_import/models/__init__.py
+++ odoo/local-src/db2_import/models/__init__.py
@@ -2,3 +2,5 @@ from . import base
 from . import db2_importer
 from . import inventory
 from . import queue_job
+from . import esb_backend_timestamp
+from . import ir_cron
--- odoo/songs/install/connector.py
+++ odoo/songs/install/connector.py
@@ -3,37 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import anthem
-from datetime import datetime, timedelta
-
-from odoo import fields
 
 
 @anthem.log
-def enable_pharmacy_export(ctx):
-    """Enable cron for pharmacy export at 01:00"""
-    tomorrow = datetime.today() + timedelta(days=1)
-    next_call = tomorrow.replace(hour=1, minute=0, second=0)
-    cronjob = ctx.env.ref('connector_esb.ir_cron_esb_export_pharmacy')
-    cronjob.write({
-        'active': True,
-        'nextcall': fields.Datetime.to_string(next_call),
-    })
-
-
-@anthem.log
-def enable_promotion_alcyon_export(ctx):
-    """Enable cron for promotion alcyon export at 01:30"""
-    tomorrow = datetime.today() + timedelta(days=1)
-    next_call = tomorrow.replace(hour=1, minute=30, second=0)
-    cronjob = ctx.env.ref('connector_esb.ir_cron_esb_export_promotion_alcyon')
-    cronjob.write({
-        'active': True,
-        'nextcall': fields.Datetime.to_string(next_call),
-    })
+def activate_connector(ctx):
+    """Activate cron jobs and set some timestamp for ESB connector."""
+    # This song is used for testing the job that is called on initialisation
+    ctx.env['esb.backend.timestamp'].reset_timestamp()
+    ctx.env['ir.cron'].activate_connector()
 
 
 @anthem.log
 def main(ctx):
-    """Activate cron jobs for ESB connector."""
-    enable_pharmacy_export(ctx)
-    enable_promotion_alcyon_export(ctx)
+    activate_connector(ctx)
