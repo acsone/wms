a1f5ace8bc7610146b6d54a7e1750e577f43b0e2
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-04-25 18:32:20 +0200

    Change to be deleted categories on products

--- odoo/songs/upgrade/10_18_7_update.sql
+++ odoo/songs/upgrade/10_18_7_update.sql
@@ -1,3 +1,7 @@
 -- remove_old_pesky_views
 -- The rewrite of shipping_costs caused some problems with the views.
 DELETE FROM ir_ui_view where arch_db ~'costs_on_in';
+
+-- change product with ketamine category to stupefiant before removal of the ketamine categ
+UPDATE product_template SET categ_id = (SELECT res_id FROM ir_model_data WHERE name = 'product_categ_stupefiant')
+  WHERE categ_id = (SELECT res_id FROM ir_model_data WHERE name = 'product_categ_ketamine');
