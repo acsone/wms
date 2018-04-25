9404efabbc82c4216698411e24cedb84b353ce37
Jacques-Etienne Baudoux <je@bcim.be>
2018-04-25 15:12:04 +0200

    Drop Wage journal, fix type of cash journal

--- odoo/songs/install/accounting.py
+++ odoo/songs/install/accounting.py
@@ -170,16 +170,14 @@ def import_account_journal(ctx):
         'name': 'Opérations Diverses'
     })
 
+    # Cash journal must be of type Miscellaneous as entries are encoded
+    # manually
+    cash = ctx.env['account.journal'].search(
+        [('name', '=', 'Cash')])
+    cash.write({'type': 'general'})
+
     content = resource_stream(req, 'data/install/account.journal.csv')
     load_csv_stream(ctx, 'account.journal', content, delimiter=',')
-
-    # Odoo will load this journal and create an translation in French
-    # However even if you load the extra_i18n file, Odoo will not
-    # overwrite the translation.
-    wage_journal = ctx.env.ref('__setup__.wage_journal')
-    wage_journal.with_context(lang='fr_BE').write({
-        'name': 'Salaire'
-    })
 
     # Set the flag "update_posted" on following journals
     # These journals have no XMLid
