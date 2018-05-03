e5b65d4d6d1a717cad3c41377adf9efba6b5dcce
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-05-03 12:02:18 +0200

    Fix license to LGPL in specific_helpdesk as we depends on helpdesk prorietary module

--- odoo/local-src/specific_helpdesk/__manifest__.py
+++ odoo/local-src/specific_helpdesk/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {'name': 'Specific Helpdesk for Alcyon',
  'version': '10.0.1.0.0',
  'author': 'Camptocamp',
- 'license': 'AGPL-3',
+ 'license': 'LGPL-3',
  'category': 'Helpdesk',
  'depends': [
      'account',
