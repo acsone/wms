5a6f1fe6f27c2b1d744bcc57c2e5cc2e95dfc182
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-05-04 09:35:37 +0200

    By default do not launch inventory and cron updates after jobs have been done In production those steps will be done manually

--- odoo/local-src/db2_import/models/queue_job.py
+++ odoo/local-src/db2_import/models/queue_job.py
@@ -2,6 +2,7 @@
 # Copyright 2018 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo import api, models
+import os
 
 CHANNEL = 'root.db2.create_or_update'
 END_STATES = ['done', 'failed']
@@ -18,19 +19,21 @@ class QueueJob(models.Model):
     def write(self, vals):
         res = super(QueueJob, self).write(vals)
 
-        for rec in self:
-            if rec.channel == CHANNEL and vals.get('state') in END_STATES:
-                # only do inventory if all importers are in final_update
-                not_final = self.env['db2.importer'].search(
-                    [('mode', '!=', 'final_update')])
-                if not_final:
-                    break
-                count_jobs_todo = self.search_count(
-                    [('state', 'not in', END_STATES),
-                     ('channel', '=', CHANNEL)])
-                if count_jobs_todo == 0:
-                    # launch inventory
-                    self.env['stock.inventory'].initial_inventory()
-                    self.env['esb.backend.timestamp'].reset_timestamp()
-                    self.env['ir.cron'].activate_connector()
+        if os.environ.get('AUTO_INVENTORY'):
+
+            for rec in self:
+                if rec.channel == CHANNEL and vals.get('state') in END_STATES:
+                    # only do inventory if all importers are in final_update
+                    not_final = self.env['db2.importer'].search(
+                        [('mode', '!=', 'final_update')])
+                    if not_final:
+                        break
+                    count_jobs_todo = self.search_count(
+                        [('state', 'not in', END_STATES),
+                         ('channel', '=', CHANNEL)])
+                    if count_jobs_todo == 0:
+                        # launch inventory
+                        self.env['stock.inventory'].initial_inventory()
+                        self.env['esb.backend.timestamp'].reset_timestamp()
+                        self.env['ir.cron'].activate_connector()
         return res
