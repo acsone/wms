6f54a955d85347f64961ecf62ac76574462f7125
Sylvain Van Hoof <sylvain@okia.be>
2018-05-15 15:22:03 +0200

    ALCN-1321: Fix a problem when a product.product is created

--- odoo/local-src/stock_orderpoint_product/models/product.py
+++ odoo/local-src/stock_orderpoint_product/models/product.py
@@ -24,7 +24,16 @@ class ProductTemplate(models.Model):
             return
 
         for product_tmpl in self:
-            product = product_tmpl.product_variant_ids[0]
+            # In some case (see the method create on product.product)
+            # the product_template may not have a variant.
+            # In this case, we have to skip this constrains
+            product_variant = product_tmpl.with_context(
+                active_test=False
+            ).product_variant_ids
+
+            if not product_variant:
+                continue
+            product = product_variant[0]
 
             rules = product.orderpoint_ids \
                 .filtered(lambda r: r.company_id == self.env.user.company_id)
@@ -33,6 +42,7 @@ class ProductTemplate(models.Model):
                     'product_min_qty': product.orderpoint_min,
                     'product_max_qty': product.orderpoint_max,
                     'qty_multiple': product.orderpoint_qty_multiple,
+                    'active': product.active,
                 })
             else:
                 product.orderpoint_ids.create({
@@ -41,7 +51,35 @@ class ProductTemplate(models.Model):
                     'product_min_qty': product.orderpoint_min,
                     'product_max_qty': product.orderpoint_max,
                     'qty_multiple': product.orderpoint_qty_multiple,
-                    'active': True,
                     'location_id': self.env.ref(
                         'stock.stock_location_stock').location_id.id,
+                    'active': product.active,
                 })
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def create(self, vals):
+        """
+        When a product.product is created, Odoo will create the product
+        and only after that create the product.template. At the end, Odoo
+        will write the link between the product and the template.
+        :param vals:
+        :return:
+        """
+        orderpoint_min = vals.pop('orderpoint_min', 0)
+        orderpoint_max = vals.pop('orderpoint_max', 0)
+        orderpoint_qty_multiple = vals.pop('orderpoint_qty_multiple', 0)
+
+        result = super(ProductProduct, self).create(vals)
+
+        if orderpoint_min or orderpoint_max or orderpoint_qty_multiple:
+            result.product_tmpl_id.write({
+                'orderpoint_min': orderpoint_min,
+                'orderpoint_max': orderpoint_max,
+                'orderpoint_qty_multiple': orderpoint_qty_multiple,
+            })
+
+        return result
