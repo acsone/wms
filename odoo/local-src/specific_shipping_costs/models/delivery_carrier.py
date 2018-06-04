e643b5d316baa44a341013c34e0c008fbc2caa74
Jacques-Etienne Baudoux <je@bcim.be>
2018-06-04 11:15:35 +0200

    Prevent to delete a delivery carrier if linked on partner

--- odoo/local-src/specific_shipping_costs/models/delivery_carrier.py
+++ odoo/local-src/specific_shipping_costs/models/delivery_carrier.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Camptocamp SA
+# Copyright 2018 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models
+from odoo import fields, models, _
+from odoo.exceptions import UserError
 
 
 class DeliveryCarrier(models.Model):
@@ -11,3 +13,9 @@ class DeliveryCarrier(models.Model):
     use_specific_cost_calculation = fields.Boolean(
         string='Alcyon specific cost'
     )
+
+    def unlink(self):
+        if self.env['res.partner'].search([
+                ('property_delivery_carrier_id', 'in', self.ids)]):
+            raise UserError(
+                _('You cannot delete a record linked from a partner'))
