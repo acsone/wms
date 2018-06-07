a3d3d93d53986f4d59933d5e748555866062acc5
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-06-07 16:10:00 +0200

    Remove sale orders of type 2

--- odoo/songs/upgrade/10_20_2_update.sql
+++ odoo/songs/upgrade/10_20_2_update.sql
@@ -9,3 +9,28 @@ UPDATE product_supplierinfo SET sequence = 100 WHERE date_end IS NULL;
 --   dccsui = so.name::int WHERE sale_order_line.order_id = so.id and sale_order_line.sequence = dccnli and dccres > 0 and dccres <= 100;
 UPDATE sale_order_line SET discount3 = discount WHERE discount > 0 and discount <= 100;
 UPDATE sale_order_line SET discount = 0 WHERE discount > 0;
+
+DELETE FROM sale_order WHERE
+
+-- remove sale orders of type 2
+-- do this only if db2_pentcdcl is present as we need to read from it
+-- we let the sale order in 'sale' state as they created pickings and
+-- pack operations. This would require more in depth changes that might
+-- not be worth it for testing purpose.
+DO $$
+BEGIN
+    IF EXISTS
+        ( SELECT 1
+            FROM  information_schema.tables
+            WHERE table_schema = 'public'
+            AND   table_name = 'db2_pentcdcl'
+        )
+    THEN
+        RAISE NOTICE 'Calling delete sale order type 2, this can take several minutes';
+        DELETE FROM sale_order
+            USING db2_pentcdcl
+            WHERE sale_order.name = eccsui::varchar
+              AND ecctyc = '2'
+              AND state in ('draft', 'done');
+    END IF;
+END$$;
