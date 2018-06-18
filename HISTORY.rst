4f9c1d0bc76b74351858fee36485ad8451ad68fc
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-06-18 13:56:56 +0200

    Fix filename for product export (esb)

--- HISTORY.rst
+++ HISTORY.rst
@@ -45,6 +45,7 @@ Unreleased
 
 * ALCN-864: Fixing the csv document generated as a delivery note by improving float number format and adding no suite, lot name, tax amount.
 * ALCN-1420: Fix sale order export to esb: Verify prices default to 0, fix delivery carrier esb_ref, communication channel default to phone
+* ALCN-1424: Fix esb export product: change xml filename
 
 **Build**
 
--- odoo/songs/upgrade/10_21_1_update.py
+++ odoo/songs/upgrade/10_21_1_update.py
@@ -7,7 +7,17 @@ from anthem.lyrics.modules import update_translations
 
 
 @anthem.log
+def update_esb_backend_data(ctx):
+    """Changing erroneous filename of esb product export
+    """
+    sql = ("UPDATE esb_backend_timestamp "
+           "SET export_filename = 'Products_{date}.xml' where kind='product'")
+    ctx.env.cr.execute(sql)
+
+
+@anthem.log
 def post(ctx):
     """ Applying post 10.21.1 """
     update_translations(ctx, ['specific_sale',
                               'specific_followup'])
+    update_esb_backend_data(ctx)
