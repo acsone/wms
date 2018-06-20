3acbde5123e49e00f1c5e9a4a1646dcb9e5bc1be
Jacques-Etienne Baudoux <je@bcim.be>
2018-06-20 10:27:09 +0200

    Set scrap default destination location to quality control

--- odoo/local-src/specific_stock/models/stock_scrap.py
+++ odoo/local-src/specific_stock/models/stock_scrap.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Sylvain VAN HOOF (Okia SPRL)
+# Copyright 2018 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class StockScrap(models.Model):
     _inherit = 'stock.scrap'
+
+    def _get_default_scrap_location_id(self):
+        # Set destination location to quality control
+        try:
+            return self.env.ref('__setup__.stock_location_scrap_quality').id
+        except ValueError:
+            return super(StockScrap, self)._get_default_scrap_location_id()
+
+    scrap_location_id = fields.Many2one(default=_get_default_scrap_location_id)
 
     @api.onchange('product_id')
     def onchange_product_id(self):
