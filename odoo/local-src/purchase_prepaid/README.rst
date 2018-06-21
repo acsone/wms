74fbef4feea09fba5189b1c2d2849fa9d2f3c6b1
Jacques-Etienne Baudoux <je@bcim.be>
2018-06-21 14:10:06 +0200

    ALCN-757: Cut-off. Add modules purchase_prepaid, specific_cutoff

---
+++ odoo/local-src/purchase_prepaid/README.rst
@@ -0,0 +1,18 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===================
+Purchase Prepayment
+===================
+
+Allow to mark a PO for prepayment. This will set the invoice status to invoice
+and allow to encode an invoice while the goods are not yet received.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Jacques-Etienne Baudoux <je@bcim.be>
---
+++ odoo/local-src/specific_cutoff/models/account_cutoff.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class AccountCutoff(models.Model):
+    _inherit = 'account.cutoff'
+
+    @api.model
+    def _cron_cutoff_refund(self, type):
+        invoices = self.env['account.invoice'].search([
+            ('state', 'in', ('draft', 'proforma2')),
+            ('type', '=', type),
+            ('accrual_move_id', '=', False)])
+        if not invoices:
+            return
+        # Cron is expected to run at begin of new period. We need the last day
+        # of previous month. Support some time difference and compute last day
+        # of previous period.
+        last_day = datetime.today()
+        if last_day.day > 20:
+            last_day += relativedelta(months=1)
+        last_day = last_day.replace(day=1)
+        last_day -= relativedelta(days=1)
+        wizard = self.env['account.move.accrue'].with_context(
+            active_model=invoices[0]._name,
+            active_ids=invoices.ids).create({'date': last_day})
+        wizard.action_accrue()
+
+    @api.model
+    def _cron_cutoff_expense_refund(self):
+        self._cron_cutoff_refund('in_refund')
+
+    @api.model
+    def _cron_cutoff_revenue_refund(self):
+        self._cron_cutoff_refund('out_refund')
