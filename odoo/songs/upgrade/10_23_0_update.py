a94b0a2ca59e1f14ddac916d0031bcd801848018
Denis Leemann <denis.leemann@camptocamp.com>
2018-08-09 18:49:02 +0200

    fixup! ALCYN-431: User mciolii must be mciolli

--- odoo/songs/upgrade/10_23_0_update.py
+++ odoo/songs/upgrade/10_23_0_update.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import anthem
+from anthem.lyrics.records import add_xmlid
 
 
 @anthem.log
@@ -15,6 +16,14 @@ def fix_esb_backend_document_zip_name(ctx):
 
 
 @anthem.log
+def update_user_xmlid(ctx):
+    user = ctx.env.ref('__setup__.res_user_mciolii', raise_if_not_found=False)
+    if user:
+        add_xmlid(ctx, user, '__setup__.res_user_mciolli')
+
+
+@anthem.log
 def pre(ctx):
     """ Applying pre 10.23.0 """
     fix_esb_backend_document_zip_name(ctx)
+    update_user_xmlid(ctx)
