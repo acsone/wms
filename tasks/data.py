9c379aab842333f5ec826f63cb2ddc4aeb040b3c
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-09-05 08:51:23 +0200

    Add product name translation
    ALCYN-70
    
    Add an invoke task to generate compatible csv files for the translation
    of product names.
    The files are generated from multiple raw csv files given by Alcyon.
    All Pipe chars are removed from the translation string.
    Add songs to import the generated files.

--- tasks/data.py
+++ tasks/data.py
@@ -118,3 +118,87 @@ def make_csv_diff(ctx, filename=None):
 
     create_csv_new(added)
     create_csv_changes(changed)
+
+
+@task(name='make-product-translation')
+def make_product_translation(ctx, files=None):
+    """This task generate product translation files.
+
+    Use this task to generate the translation files for product names
+    from the raw files provided by Alcyon.
+
+    The files that were provided to create this task add the following header
+
+    sku | denom_erp-en_GB | denom_erp-fr_BE | denom_erp-nl_BE
+
+    To use call the task like this :
+    
+    invoke data.make_product_translation --files raw_file_1.csv,raw_file_2.csv
+
+    With as many file as needed.
+    """
+
+    path_to_new_file = 'odoo/data/install/'
+    xml_id_format = '__import__.product_{}_product_template'
+
+    def make_translation_file(raw_files, language, new_file_name):
+        """Make a translation file for one language."""
+
+        col_header = ['id/id', 'name']
+        with open(new_file_name, 'wb') as new_file:
+            writer = csv.DictWriter(new_file, fieldnames=col_header)
+            writer.writeheader()
+            for file_path in raw_files:
+                with open(file_path) as raw_file:
+                    reader = csv.DictReader(raw_file, delimiter=';')
+                    # Find the column that match the language to extract
+                    language_col = ''
+                    for header in reader.fieldnames:
+                        if language in header:
+                            language_col = header
+                            break
+                    else:
+                        print('Language {} not found in file {}.'.format(
+                            language,
+                            file_path
+                            ))
+                        print('Aborting !')
+                        exit()
+                    for row in reader:
+                        if not len(row[language_col]):
+                            # Empty traduction are skipped
+                            continue
+                        r = {'id/id': xml_id_format.format(row['sku']),
+                            'name': row[language_col].translate(None, '|').strip()
+                            }
+                        writer.writerow(r)
+
+
+    raw_files = files.split(',')
+    if len(raw_files) < 1:
+        print('Which file need to be worked on ?')
+        exit()
+    # Check the existance of the files to work on
+    print('Generating translation from the following files :')
+    for file_name in raw_files:
+        if os.path.isfile(file_name):
+            print(file_name)
+        else:
+            print('\nFile {} can not be found, aborting !'.format(file_name))
+            exit()
+
+    make_translation_file(
+        raw_files,
+        'nl_BE',
+        path_to_new_file + 'product_name_nl_BE.csv'
+        )
+    make_translation_file(
+        raw_files,
+        'en_GB',
+        path_to_new_file + 'product_name_en_US.csv'
+        )
+    make_translation_file(
+        raw_files,
+        'fr_BE',
+        path_to_new_file + 'product_name_fr_BE.csv'
+        )
