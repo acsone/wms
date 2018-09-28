9a9fb698be8d851416685a4da9bb428e05e81887
Jacques-Etienne Baudoux <je@bcim.be>
2018-09-28 11:43:04 +0200

    fix version, moved vars in if statement

--- odoo/local-src/stock_unit/models/stock_quant.py
+++ odoo/local-src/stock_unit/models/stock_quant.py
@@ -19,24 +19,24 @@ class StockQuant(models.Model):
                 "Reserve by packaging. Current domain: %s. "
                 "Preferred domain: %s", domain, preferred_domain_list)
             config_param = self.env['ir.config_parameter']
-            pallet_factor = float(config_param.get_param(
-                'stock.reservation_unit_pallet_factor', 1))
-            box_factor = float(config_param.get_param(
-                'stock.reservation_unit_box_factor', 1))
-            wrap_factor = float(config_param.get_param(
-                'stock.reservation_unit_wrap_factor', 1))
             min_qty = float(config_param.get_param(
                 'stock.reservation_unit_min_quantity', 0))
             preferred_domain_list = []
             if qty >= min_qty:
+                pallet_factor = float(config_param.get_param(
+                    'stock.reservation_unit_pallet_factor', 1))
                 pallet_qty = move.product_id.unit_in_pallet * pallet_factor
                 if pallet_qty and qty > pallet_qty:
                     preferred_domain_list.append([('qty', '>=', pallet_qty)])
 
+                box_factor = float(config_param.get_param(
+                    'stock.reservation_unit_box_factor', 1))
                 box_qty = move.product_id.unit_in_box * box_factor
                 if box_qty and qty > box_qty:
                     preferred_domain_list.append([('qty', '>=', box_qty)])
 
+                wrap_factor = float(config_param.get_param(
+                    'stock.reservation_unit_wrap_factor', 1))
                 wrap_qty = move.product_id.unit_in_shrink_wrap * wrap_factor
                 if wrap_qty and qty > wrap_qty:
                     preferred_domain_list.append([('qty', '>=', wrap_qty)])
