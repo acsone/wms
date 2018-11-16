19564d3f3c5d20fc6926f48938d1b622607fd346
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-11-16 11:12:02 +0100

    Change customer export name to use name_get

--- odoo/local-src/connector_esb/models/customer/exporter.py
+++ odoo/local-src/connector_esb/models/customer/exporter.py
@@ -17,10 +17,17 @@ class CustomerExportMapper(Component):
         return bool(work.timestamp and work.timestamp.kind == 'customer')
 
     direct = [
-        ('email', 'Email'),
-        ('name', 'Firstname'),
+        (falsy2emptystring('email'), 'Email'),
         (falsy2emptystring('ref'), 'ErpId'),
     ]
+
+    @mapping
+    def compute_firstname(self, record):
+        try:
+            name = record.name_get()[0][1]
+        except:
+            name = record.name
+        return {'Firstname': name or ''}
 
     @mapping
     def compute_statistic_code(self, record):
