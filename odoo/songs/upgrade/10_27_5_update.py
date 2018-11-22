b39acc901a0c97bc205c392aad9cb5ac63779184
Denis Leemann <denis.leemann@camptocamp.com>
2018-11-22 17:00:15 +0100

    Update 10_27_5_update.py

--- odoo/songs/upgrade/10_27_5_update.py
+++ odoo/songs/upgrade/10_27_5_update.py
@@ -8,8 +8,13 @@ from anthem.lyrics.records import add_xmlid
 
 @anthem.log
 def add_xmlid_users(ctx):
+    ctx.env.cr.execute("""
+        SELECT res_id from ir_model_data
+        WHERE model='res.users' AND module!='__setup__';
+    """)
+    noupdate_users_ids = ctx.env.cr.fetchall()
     users = ctx.env['res.users'].with_context(
-        active_test=False).search([])
+        active_test=False).search([('id', 'not in', noupdate_users_ids)])
     for user in users:
         add_xmlid(
             ctx, user,
@@ -19,5 +24,5 @@ def add_xmlid_users(ctx):
 
 
 def post(ctx):
-    """ POST 10.27.5 """
+    """ POST 10.28.1 """
     add_xmlid_users(ctx)
