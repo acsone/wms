0996fcdbade69c0db1d403f2173f40b728783a38
Sylvain Van Hoof <sylvain@okia.be>
2018-12-01 19:13:34 +0100

    fixup! ALCYN-1695: Add a flag on product to not print label for these product

--- odoo/songs/upgrade/10_29_2_update.py
+++ odoo/songs/upgrade/10_29_2_update.py
@@ -3,13 +3,7 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from pkg_resources import resource_stream
-
 import anthem
-from anthem.lyrics.loaders import load_csv_stream
-from anthem.lyrics.records import add_xmlid
-
-from ..common import req
 
 
 @anthem.log
