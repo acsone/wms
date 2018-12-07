c222aa065143c947b298e337d530f1c8873879bb
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-12-07 01:11:35 +0100

    Fix stock_delivery_note missing migrated method
    When moving featuers from specific_report to stock_delivery_note
    the method get_lots got lost.

--- odoo/local-src/stock_delivery_note/models/stock_move.py
+++ odoo/local-src/stock_delivery_note/models/stock_move.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class StockMove(models.Model):
@@ -14,3 +14,31 @@ class StockMove(models.Model):
                                     store=True)
 
     order_id = fields.Many2one('sale.order', related='order_line_id.order_id')
+
+    @api.multi
+    def get_lots(self):
+        """
+        Return all lots for the stock move
+        :return: Return a list of tuple
+        """
+        qty_by_lot = {}
+
+        quants = self.linked_move_operation_ids.mapped('reserved_quant_id')
+        for quant in quants:
+            if not quant.lot_id:
+                continue
+            lot = quant.lot_id
+
+            existing_qty = qty_by_lot.get(lot.name, [])
+            if existing_qty:
+                qty_by_lot[lot.name] = [existing_qty[0] +
+                                        quant.qty, existing_qty[1]]
+            else:
+                qty_by_lot[lot.name] = [quant.qty, lot.life_date or '']
+
+        result = [[key, value[0], value[1]]
+                  for key, value
+                  in qty_by_lot.iteritems()]
+
+        # Sort lot by name
+        return sorted(result, key=lambda lot: lot[0])
