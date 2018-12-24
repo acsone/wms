7b1951a865fe8f8b843b75e34ae594749e274375
Jacques-Etienne Baudoux <je@bcim.be>
2018-12-24 16:49:16 +0100

    fixup! fixup! Fix unit tests

--- odoo/local-src/specific_shipping_costs/tests/test_shipping_costs.py
+++ odoo/local-src/specific_shipping_costs/tests/test_shipping_costs.py
@@ -209,7 +209,7 @@ class TestShippingCosts(SavepointCase):
         op = pick.pack_operation_product_ids
         op = op.filtered(lambda r: r.product_id.id == self.p1.id)
         op.write({'qty_done': 1})
-        result = pick.with_context(test_mode=True).do_transfer()
+        pick.with_context(test_mode=True).do_transfer()
         self.dr1._deliver(background=False)
         self.assertEqual(self.get_shipping_cost(self.so1), 0)
         self.assertEqual(self.get_shipping_cost(self.so2), self.fee)
@@ -236,7 +236,7 @@ class TestShippingCosts(SavepointCase):
         op = pick.pack_operation_product_ids
         op.write({'qty_done': 1})
 
-        result = pick.with_context(test_mode=True).do_transfer()
+        pick.with_context(test_mode=True).do_transfer()
         self.dr2._deliver(background=False)
         self.assertEqual(self.get_shipping_cost(self.so2), self.fee)
         self.assertEqual(self.get_shipping_cost(so3), self.fee)
@@ -272,7 +272,7 @@ class TestShippingCosts(SavepointCase):
         op = pick.pack_operation_product_ids
         op = op.filtered(lambda r: r.product_id.id == self.p1.id)
         op.write({'qty_done': 1})
-        result = pick.with_context(test_mode=True).do_transfer()
+        pick.with_context(test_mode=True).do_transfer()
         self.dr1._deliver(background=False)
         self.assertEqual(self.get_shipping_cost(self.so1), 0)
         self.assertEqual(self.get_shipping_cost(self.so2), 0)
@@ -299,7 +299,7 @@ class TestShippingCosts(SavepointCase):
         op = pick.pack_operation_product_ids
         op.write({'qty_done': 1})
 
-        result = pick.with_context(test_mode=True).do_transfer()
+        pick.with_context(test_mode=True).do_transfer()
         self.dr2._deliver(background=False)
         self.assertEqual(self.get_shipping_cost(self.so2), 0)
         self.assertEqual(self.get_shipping_cost(so3), self.fee)
