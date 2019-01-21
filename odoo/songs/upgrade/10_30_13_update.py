d72abfc2adc7ade0fa89e1da99ddf2f6fc325fc0
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-01-21 15:31:53 +0100

    Refine migration query for carrier on procurement group
    Set a "manual" carrier on groups linked to "comptoir" and "long terme"
    if it was empty or if it was "Livraison Alcyon", which will prevent new
    stock moves to be grouped with them.

--- odoo/songs/upgrade/10_30_13_update.py
+++ odoo/songs/upgrade/10_30_13_update.py
@@ -14,9 +14,17 @@ def set_special_delivery_round_as_manual(ctx):
                       WHERE module = 'delivery_rounds'
                       AND name = 'delivery_carrier_manual_round_change')
     FROM stock_picking p, round_instance r
-    WHERE g.carrier_id IS NULL
+    WHERE (
+      g.carrier_id IS NULL
+      OR
+      g.carrier_id = (SELECT res_id
+                      FROM ir_model_data
+                      WHERE module = '__setup__'
+                      AND name = 'deliver_carrier_alcyon')
+    )
     AND g.id = p.group_id
     AND r.id = p.delivery_round_id
+    AND p.state not in ('done', 'cancel')
     AND r.template_id in (
       SELECT res_id FROM ir_model_data
       WHERE module = '__setup__'
