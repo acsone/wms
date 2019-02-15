23ba0e61389c0adc595a3fe66864ce74d4744ce9
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2019-02-15 14:11:47 +0100

    fix lint issue

--- odoo/local-src/product_additional/models/stock_picking.py
+++ odoo/local-src/product_additional/models/stock_picking.py
@@ -100,7 +100,6 @@ class StockPicking(models.Model):
                 'picking_id': picking.id,
                 'picking_type_id': picking.picking_type_id.id,
                 'origin': picking.name,
-                'partner_id': picking.partner_id.id,
                 'group_id': picking.group_id.id,
                 'is_additional_move': True,
             }
