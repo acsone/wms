e9169ffe3b2456f6320e8cd4b501fb04b52afc62
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-03-13 11:29:50 +0100

    Remove duplicated import

--- odoo/local-src/connector_esb/models/__init__.py
+++ odoo/local-src/connector_esb/models/__init__.py
@@ -22,7 +22,6 @@ from . import res_lang
 from . import sale_order
 from . import sale_order_line
 from . import res_users
-from . import sale_order
 from . import product_supplierinfo
 from . import queue_job
 from . import buyx_gety
