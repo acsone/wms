261745fbc122633ef2e759cd6d62897695873cc4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-05-22 11:21:42 +0200

    Refactor stock_inventory_controller
    Move the error handling in 'StockInventory.action_done()', because this
    is really the whole part we want to rollback on error during
    'StockInventory.post_inventory()'. The intention seems clearer.
    
    The previous implementation had a problem when the inventory lines were
    created in the same transaction than the one where they are confirmed,
    as the wizard to change the qty on hand of a product is doing for
    instance. In case of failure, it would crash with a "record not found
    error". Here, we ignore lines that don't exist in the new transaction as
    anyway they will be rollbacked as well.
    
    The previous implementation was buggy when the same product was used in
    several inventory lines: as the line was found with a search on the
    same product than the move line, if we had one valid line and an invalid
    one, both would be invalid. This is fixed by adding a new Many2one
    referencing the inventory line that created the move line. Note: if an
    inventory is started before we add the field, we will not be able to
    find the line so in this case we raise an error. We could write a
    migration but it would as imperfect as the previous solution as we could
    only rely on the identical product.
    
    The try-except on MissingError seems useless (no reason to happen) and
    indeed, I could find zero occurrences of this error in the inventory
    lines in production: removed it.

--- odoo/local-src/stock_inventory_controller/models/stock_inventory.py
+++ odoo/local-src/stock_inventory_controller/models/stock_inventory.py
@@ -1,10 +1,29 @@
 import logging
+from contextlib import contextmanager
 
 import odoo
-from odoo import api, fields, models
-from odoo.exceptions import MissingError, UserError
+from odoo import _, api, exceptions, fields, models
 
 _logger = logging.getLogger(__name__)
+
+
+class InventoryError(Exception):
+    """Error happening during action_done() of the inventory
+
+    errors is a list of (line, exception raised by the line)
+    """
+
+    def __init__(self, errors, msg=None):
+        if not msg:
+            msg = _('Error during Inventory')
+        super(InventoryError, self).__init__(msg)
+        self.errors = errors
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    inventory_line_id = fields.Many2one(comodel_name='stock.inventory.line')
 
 
 class StockInventoryLine(models.Model):
@@ -14,6 +33,13 @@ class StockInventoryLine(models.Model):
         'Line failed', readonly=True, default=False
     )
     fail_message = fields.Char('Fail message', readonly=True)
+
+    def _get_move_values(self, qty, location_id, location_dest_id):
+        values = super(StockInventoryLine, self)._get_move_values(
+            qty, location_id, location_dest_id
+        )
+        values['inventory_line_id'] = self.id
+        return values
 
 
 class StockInventory(models.Model):
@@ -32,6 +58,49 @@ class StockInventory(models.Model):
     line_ids = fields.One2many(domain=[('is_line_failed', '=', False)])
 
     @api.multi
+    def action_done(self):
+        @contextmanager
+        def open_new_env():
+            with api.Environment.manage():
+                with odoo.registry(self.env.cr.dbname).cursor() as new_cr:
+                    yield self.env(cr=new_cr)
+
+        # save the state before we create and validate moves
+        try:
+            return super(StockInventory, self).action_done()
+        except InventoryError as err:
+            bullets = []
+            # Open a new cursor to save the error in the lines, as we'll
+            # raise the error after, it will rollback everything.
+            with open_new_env() as new_env:
+                for line, original_error in err.errors:
+                    bullets.append(
+                        u' - %s: %s'
+                        % (line.product_id.display_name, original_error.name)
+                    )
+
+                    line = line.with_env(new_env)
+                    # If the line does not exist in the new cr, it means
+                    # that it has been created in the same transaction:
+                    # either in a unit test, either by the wizard
+                    # "stock.change.product.qty". We don't care about
+                    # writing back the failure message in these cases,
+                    # because the inventory and its lines will be
+                    # rollbacked with the main transaction anyway
+                    if not line.exists():
+                        continue
+                    line.write(
+                        {
+                            'is_line_failed': True,
+                            'fail_message': original_error.name,
+                        }
+                    )
+
+            raise exceptions.UserError(
+                _(u'Cannot validate inventory:\n\n%s') % ('\n'.join(bullets),)
+            )
+
+    @api.multi
     def post_inventory(self):
         """
         This method has the api multi but this method is called for each line.
@@ -40,38 +109,24 @@ class StockInventory(models.Model):
         moves = self.mapped('move_ids').filtered(
             lambda move: move.state != 'done'
         )
-
-        with api.Environment.manage():
-            with odoo.registry(self.env.cr.dbname).cursor() as new_cr:
-                new_self = self.with_env(self.env(cr=new_cr))
-                exception = False
-                for move in moves:
-                    try:
-                        move.action_done()
-                    except MissingError as me:
-                        _logger.error('MissingError: ' + str(me))
-                        line = new_self.line_ids.search(
-                            [('product_id', '=', move.product_id.id)]
+        errors = []
+        for move in moves:
+            try:
+                move.action_done()
+            except exceptions.UserError as err:
+                _logger.error('UserError: %s', err)
+                line = move.inventory_line_id
+                if not line:
+                    # This is only in case we are trying to validate an
+                    # inventory started before and validated after we added the
+                    # field 'inventory_line_id' in the addon. Can be removed
+                    # once we have deployed the addon sinc 'inventory_line_id' will
+                    # always be populated then.
+                    raise exceptions.UserError(
+                        _(
+                            'Error during validation. Please restart an inventory.'
                         )
-                        line.write(
-                            {
-                                'is_line_failed': True,
-                                'fail_message': 'MissingError: ' + str(me),
-                            }
-                        )
-                        exception = me
-                    except UserError as ue:
-                        _logger.error('UserError: ' + str(ue))
-                        line = new_self.line_ids.search(
-                            [('product_id', '=', move.product_id.id)]
-                        )
-                        line.write(
-                            {
-                                'is_line_failed': True,
-                                'fail_message': 'UserError: ' + str(ue),
-                            }
-                        )
-                        exception = ue
-                if exception:
-                    new_cr.commit()
-                    raise exception
+                    )
+                errors.append((line, err))
+        if errors:
+            raise InventoryError(errors)
