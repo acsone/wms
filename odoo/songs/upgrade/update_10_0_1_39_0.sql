8579207af866fa08f593a41f46365b41a43959b1
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-07-01 10:43:38 +0200

    Fix menus for stock pack operations
    2 actions and 1 menu have been created manually previously in the DB.
    The menu was not using the expected action. Remove the menu and actions
    created manually and replace them.
    
    Change the pivot view to split by operators by default.

--- odoo/songs/upgrade/update_10_0_1_39_0.sql
+++ odoo/songs/upgrade/update_10_0_1_39_0.sql
@@ -3,3 +3,9 @@ DELETE FROM ir_filters
     WHERE model_id = 'stock.pack.operation'
     AND name = 'Nbr lignes Picking'
     AND context LIKE '%' || 'write_uid' || '%';
+
+-- stock.pack.operation actions created manually, replaced by an action in addon
+DELETE FROM ir_actions WHERE id in (619, 608);
+
+-- stock.pack.operation menu created manually, replaced by a menu in addon
+DELETE FROM ir_ui_menu WHERE id = 447;
