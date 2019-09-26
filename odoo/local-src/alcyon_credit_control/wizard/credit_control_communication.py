db24fed7409bb1775aa9c4e707ad3b935cba6d6a
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2019-09-26 09:28:50 +0200

    Add credit control cusomizations
    Change sort order of printed credit control lines
    Add client reference on report
    Move total invoiced and due under corresponding column
    Change some column header and remove invoice name
    Change some translation
    Add non reconciled payment on credit control print
    
    ALCYN-2139

---
+++ odoo/local-src/alcyon_credit_control/wizard/credit_control_communication.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, fields, models
+
+
+class CreditCommunication(models.TransientModel):
+    _inherit = "credit.control.communication"
+
+    payment_not_reconciled = fields.Many2many(
+        'account.move.line', string='Un reconciled payment'
+    )
+
+    @api.model
+    @api.returns('credit.control.line')
+    def _get_credit_lines(self, line_ids, partner_id, level_id, currency_id):
+        """Sort the lines to print on invoice date."""
+        cr_lines = super(CreditCommunication, self)._get_credit_lines(
+            line_ids, partner_id, level_id, currency_id
+        )
+        cr_lines.sorted(key=lambda r: r.date_sent)
+        return cr_lines
+
+    @api.model
+    def _generate_comm_from_credit_lines(self, lines):
+        comms = super(
+            CreditCommunication, self
+        )._generate_comm_from_credit_lines(lines)
+        comms._get_payment()
+        return comms
+
+    @api.multi
+    def _get_payment(self):
+        """Get not reconciled payment for the client of the control line."""
+        for comm in self:
+            comm.payment_not_reconciled = self.env['account.move.line'].search(
+                [
+                    ('partner_id', '=', comm.partner_id.id),
+                    ('reconciled', '=', False),
+                    ('credit', '>', 0),
+                    ('account_id.internal_type', '=', 'receivable'),
+                    ('journal_id.type', '=', 'sale'),
+                ]
+            )
+
+    @api.model
+    def _get_total(self):
+        total = super(CreditCommunication, self)._get_total()
+        total += sum(self.mapped('payment_not_reconciled.balance'))
+        return total
+
+    @api.model
+    def _get_total_due(self):
+        total = super(CreditCommunication, self)._get_total_due()
+        total += sum(self.mapped('payment_not_reconciled.balance'))
+        return total
