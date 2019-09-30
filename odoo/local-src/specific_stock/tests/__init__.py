2760b925da4d12ed770b91b60b2683d64f775a17
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2019-09-30 15:58:59 +0200

    Improve stock.location name
    By default when a location as the usage field set as 'view' its name
    is not computed with its parents location. The same behaviour is
    requested  when the field 'act_as_view' is set.
    
    ALCYN-2239

--- odoo/local-src/specific_stock/tests/__init__.py
+++ odoo/local-src/specific_stock/tests/__init__.py
@@ -4,5 +4,6 @@ from . import test_lot_checksum
 from . import test_picking_transfer
 from . import test_product_category_expired_times
 from . import test_stock_inventory
+from . import test_stock_location_name
 from . import test_time_limit_order
 from . import test_lot_voice_identifier
