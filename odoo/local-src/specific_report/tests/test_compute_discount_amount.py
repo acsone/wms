1f2ad2785094fd7b7198e27dfb3e66a8ffd3597d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-10-30 10:53:16 +0100

    Run compute discount amount tests in post_install
    We have had a random error once in
    test_discount_base_rounding_before_tax, run the tests in post_install
    in case it was due to a dependency's loading order.

--- odoo/local-src/specific_report/tests/test_compute_discount_amount.py
+++ odoo/local-src/specific_report/tests/test_compute_discount_amount.py
@@ -6,6 +6,9 @@ from odoo.tests.common import TransactionCase
 
 
 class TestComputeDiscountAmount(TransactionCase):
+    post_install = True
+    at_install = False
+
     def setUp(self):
         super(TestComputeDiscountAmount, self).setUp()
 
