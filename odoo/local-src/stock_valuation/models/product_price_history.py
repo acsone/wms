51268f253cc45e66dfe55c9436f043dc57fd8c64
Jacques-Etienne Baudoux <je@bcim.be>
2019-11-12 15:22:58 +0100

    Fix stock valuation computation
    * Compute stock valuation with PMP based on current stock
    * Fix PMP computation. There is one move per procurement in the
    reception, and so it creates one entry per move in the product price
    history. We also have to order price by id DESC. + prefer distinct on
    instead of limit 1

--- odoo/local-src/stock_valuation/models/product_price_history.py
+++ odoo/local-src/stock_valuation/models/product_price_history.py
@@ -8,6 +8,7 @@ from odoo import fields, models
 
 class ProductPriceHistory(models.Model):
     _inherit = 'product.price.history'
+    _order = 'datetime desc, id desc'
 
     # override to add an index
     product_id = fields.Many2one(index=True)
