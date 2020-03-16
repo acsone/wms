605614ca037827090f3ba3d0df7cd0a19171abc5
laurent.corron <laurent.corron@acsone.eu>
2020-03-16 10:05:51 +0100

    [IMP] barcode labels generation 8x2 and 4x2 refs #38667

--- odoo/local-src/stock_location_report/set_blue.sql
+++
@@ -1,9 +0,0 @@
-update stock_location
-set color='blue'
-where zone='A' and (
-       (box='4' and corridor in ('A','C','E'))
-    or (box='A' and corridor in ('B','D'))
-    or (corridor='A' and shelf in ('9', '10'))
-    or (corridor in ('B','C','D') and shelf in ('13','14'))
-);
-
