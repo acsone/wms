da53a8dc2089afedbda7a53361a3da32024efe5c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-03-17 09:18:56 +0100

    [FIX] specific_account: Use two queries to get the SO to invoice and to refund Before this change, only one query was used to get the SO to invoice and refund for a given partner. Unfortunately, the query was done before creating the invoices and the same result was used to create the refund. It appears that if a SO was configured to get it's own invoive (no merge), this invoice was still part of the revordset used to call the refund creation. Since this invoice was not mergeable with others, this second call fails since there is no more line to invoice. With this change we query a firest time to get the SO to invoice and after the invoice creation we query again to get the SO to refund. As result 2 differents recordset are used and we avoid to mix invoiced SO with invoice to refund... refs #39511

--- odoo/local-src/specific_account/tests/test_sale_order_invoicing.py
+++ odoo/local-src/specific_account/tests/test_sale_order_invoicing.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, tools
 from odoo.tests.common import SavepointCase
 
 
@@ -12,18 +14,25 @@ class TestSaleOrderInvoicing(SavepointCase):
     def setUpClass(cls):
         super(TestSaleOrderInvoicing, cls).setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+
+        cls.AccountInvoice = cls.env["account.invoice"]
+        cls.StockReturnPicking = cls.env['stock.return.picking']
+        cls.SaleOrder = cls.env["sale.order"]
+
         cls.partner = cls.env['res.partner'].create(
             {'name': u"TEST", 'customer': True, 'ref': "42"}
         )
         cls.product = cls.env['product.product'].create(
-            {'name': u"TEST", 'type': 'consu'}
+            {'name': u"TEST", 'type': 'consu', 'invoice_policy': 'delivery'}
         )
         cls.orders = cls.env['sale.order']
+        cls.unique_orders = cls.env['sale.order']
+        cls.mergeable_orders = cls.env['sale.order']
         # Generate 2 SO to invoice together
         # + 2 others SO to invoice separately
         for x in range(4):
             is_unique_invoice = bool(x % 2)
-            order = cls.env['sale.order'].create(
+            order = cls.SaleOrder.create(
                 {
                     'partner_id': cls.partner.id,
                     'partner_invoice_id': cls.partner.id,
@@ -35,7 +44,7 @@ class TestSaleOrderInvoicing(SavepointCase):
                             {
                                 'name': u"Line {}".format(i),
                                 'product_id': cls.product.id,
-                                'product_uom_qty': 1,
+                                'product_uom_qty': 4,
                                 'product_uom': cls.product.uom_id.id,
                                 'price_unit': i,
                             },
@@ -47,6 +56,10 @@ class TestSaleOrderInvoicing(SavepointCase):
                 }
             )
             cls.orders |= order
+            if is_unique_invoice:
+                cls.unique_orders |= order
+            else:
+                cls.mergeable_orders |= order
 
     def _process_picking(self, picking):
         picking.force_assign()
@@ -55,8 +68,8 @@ class TestSaleOrderInvoicing(SavepointCase):
             pack.qty_done = pack.product_qty
         picking.do_new_transfer()
 
-    def test_is_unique_invoice(self):
-        for order in self.orders:
+    def _deliver_orders(self, orders):
+        for order in orders:
             # validate SO
             order.action_confirm()
             # process deliveries
@@ -72,6 +85,80 @@ class TestSaleOrderInvoicing(SavepointCase):
             for picking in picking_outs:
                 self._process_picking(picking)
                 self.assertEqual(picking.state, 'done')
+
+    def _return_orders(self, orders):
+        for pick in orders.mapped('picking_ids'):
+            # Create return picking
+            default_data = self.StockReturnPicking.with_context(
+                active_ids=pick.ids, active_id=pick.ids[0]
+            ).default_get(
+                [
+                    'move_dest_exists',
+                    'original_location_id',
+                    'product_return_moves',
+                    'parent_location_id',
+                    'location_id',
+                ]
+            )
+            return_wiz = self.StockReturnPicking.with_context(
+                active_ids=pick.ids, active_id=pick.ids[0]
+            ).create(default_data)
+            return_wiz.product_return_moves.write(
+                {"quantity": 4.0, "to_refund_so": True}  # Return all products
+            )
+            res = return_wiz.create_returns()
+            return_pick = (
+                self.env['stock.picking']
+                .browse(res['res_id'])
+                .with_context(__no_pick_receive_note_check=True)
+            )
+
+            # Validate picking
+            return_pick.force_assign()
+            return_pick.pack_operation_product_ids.write({'qty_done': 16})
+            return_pick.do_new_transfer()
+
+    def test_is_unique_invoice(self):
+        self._deliver_orders(self.orders)
         invoice_ids = self.orders.action_invoice_create(final=True)
         # 1 invoice for 2 SO + 2 invoices for SO invoiced separately => 3
         self.assertEqual(len(invoice_ids), 3)
+
+    def test_00(self):
+        """
+        Data:
+            1 SO delivered and invoiced SO (is_unique_invoice)
+        Test case:
+            Return delivered SO
+            Deliver a new is_unique_invoice SO for the same partner with
+            invoice policy 'all_at_once'
+            Create invoice for this partner
+            (At this stage we have 2 SO to invoices separately;
+            the new one and the refund of the one returned)
+        Expected result:
+            2 invoices must be created. 1 invoice and 1 refund
+        """
+        # data
+        delivered_and_invoiced_so = self.unique_orders[0]
+        second_so = self.unique_orders[1]
+        self._deliver_orders(delivered_and_invoiced_so)
+        with tools.mute_logger('odoo.addons.queue_job.models.base'):
+            self.SaleOrder.with_context(
+                test_queue_job_no_delay=True
+            )._job_invoices_by_partner(self.partner.id, fields.Datetime.now())
+
+        # test case
+        # return the delivered_and_invoiced_so
+        self._return_orders(delivered_and_invoiced_so)
+        # deliver a new so
+        self._deliver_orders(second_so)
+        invoices = self.AccountInvoice.search([])
+        # create invoices
+        with tools.mute_logger('odoo.addons.queue_job.models.base'):
+            self.SaleOrder.with_context(
+                test_queue_job_no_delay=True
+            )._job_invoices_by_partner(self.partner.id, fields.Datetime.now())
+
+        # expected result
+        new_invoices = self.AccountInvoice.search([]) - invoices
+        self.assertEqual(len(new_invoices), 2)
