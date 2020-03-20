c84c7a5e9273fdbbf5847ee4cc155a3dbec7bb63
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-03-20 14:28:33 +0100

    test_delivery_rounds; mute warning for 'test_queue_job_no_delay' ctx key found. NO JOB scheduled.

--- odoo/local-src/test_delivery_rounds/tests/test_alcyn2130.py
+++ odoo/local-src/test_delivery_rounds/tests/test_alcyn2130.py
@@ -655,9 +655,10 @@ class TestDeliveryRoundRefillAndBackorders(TestDeliveryRound):
         )
         self.delivery_round_2.button_resetdraft()
         # deliver round 1 without processing the pick of so1
-        self.delivery_round_1.with_context(
-            test_queue_job_no_delay=1
-        ).button_deliver()
+        with tools.mute_logger('odoo.addons.queue_job.models.base'):
+            self.delivery_round_1.with_context(
+                test_queue_job_no_delay=1
+            ).button_deliver()
         self.delivery_round_1.button_done()
         # the pickings of so2 are automatically assigned to the delivery round 2
         self.assertEqual(
