92d29e39159dc9248b7486de598b6526cc87ae2b
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-04-07 18:06:56 +0200

    Bump version: 1.49.2 → 1.49.3

--- odoo/local-src/alc_all/__manifest__.py
+++ odoo/local-src/alc_all/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Alc All",
     "description": """
         Alcyon Odoo App""",
-    "version": "10.0.1.49.2",
+    "version": "10.0.1.49.3",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
