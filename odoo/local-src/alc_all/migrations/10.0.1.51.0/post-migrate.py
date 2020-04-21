6d905e49a60a486b063df3a83afbc427be4426dd
Arnaud Pineux <arnaud.pineux@acsone.eu>
2020-04-21 16:33:35 +0200

    [PRE-COMMIT]

--- odoo/local-src/alc_all/migrations/10.0.1.51.0/post-migrate.py
+++ odoo/local-src/alc_all/migrations/10.0.1.51.0/post-migrate.py
@@ -3,7 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
-from odoo import api, SUPERUSER_ID
+
+from odoo import SUPERUSER_ID, api
 
 _logger = logging.getLogger(__name__)
 
@@ -14,8 +15,7 @@ def migrate(cr, version):
         return
 
     env = api.Environment(cr, SUPERUSER_ID, {})
-    module = env['ir.module.module'].search(
-        [('name', '=', 'db2_import')])
+    module = env['ir.module.module'].search([('name', '=', 'db2_import')])
     if module:
         module.write({'state': 'to remove'})
     return
