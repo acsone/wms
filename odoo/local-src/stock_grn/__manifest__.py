6d1da661a8cd7ee4b8d86565cd2f63dbd9ef2368
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-04-23 09:43:49 +0200

    [IMP] stock_grn: Speedup grn_date column initialization Avoid the initialization of this new related column by the ORM since the orm underperforms in such a case fixes #40179

--- odoo/local-src/stock_grn/__manifest__.py
+++ odoo/local-src/stock_grn/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {'name': 'Goods Received Note',
- 'version': '0.1',
+ 'version': '10.0.1.0.1',
  'author': "BCIM",
  'maintainer': 'QANSEE',
  'category': 'Delivery',
---
+++ odoo/local-src/stock_grn/migrations/10.0.1.0.1/pre-migrate.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    _logger.info('Create and initialize the grn_date field on stock.picking')
+
+    cr.execute(
+        """
+        SELECT
+            1
+        FROM
+            information_schema.columns
+        WHERE
+            table_name='stock_picking'
+            AND column_name='grn_date';
+    """
+    )
+    if cr.fetchall():
+        _logger.into("Column already exists; Skip!")
+        return
+
+    cr.execute(
+        """
+        ALTER  TABLE
+            stock_picking
+        ADD COLUMN
+            grn_date TIMESTAMP WITHOUT TIME ZONE;
+        """
+    )
+    cr.execute(
+        """
+        UPDATE
+            stock_picking
+        SET
+            grn_date = stock_grn.date
+        FROM
+            stock_grn
+        WHERE stock_picking.grn_id = stock_grn.id
+    """
+    )
