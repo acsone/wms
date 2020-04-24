ee07b136be5e1129f8452ad636c04de6790207d0
Arnaud Pineux <arnaud.pineux@acsone.eu>
2020-04-24 11:36:12 +0200

    [PRE-COMMIT]

--- odoo/local-src/delivery_rounds/migrations/1.1/post-migrate.py
+++ odoo/local-src/delivery_rounds/migrations/1.1/post-migrate.py
@@ -16,14 +16,13 @@ def migrate(cr, version):
     _logger.info('Remove customer instance on delivery round that are empty')
 
     env = api.Environment(cr, SUPERUSER_ID, {})
-    instances = env['round.instance.customer'].search([
-        ('picking_ids', '=', False)]
+    instances = env['round.instance.customer'].search(
+        [('picking_ids', '=', False)]
     )
     instances.unlink()
 
-    pickings = env['stock.picking'].search([
-        ('delivery_round_customer_id', '!=', False),
-        ('state', '=', 'cancel')]
+    pickings = env['stock.picking'].search(
+        [('delivery_round_customer_id', '!=', False), ('state', '=', 'cancel')]
     )
     instances = pickings.mapped('delivery_round_customer_id')
     instances._remove_if_empty()
