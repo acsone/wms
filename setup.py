7a956f64572b9387f7dae6cc9dc76bbe62342eed
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-05-25 16:26:29 +0200

    Use PEP 517

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     version=cfg.get("acsoo", "series") + "." + cfg.get("acsoo", "version"),
     name="odoo-addons-alcyon",
     description="Alcyon Odoo Addons",
-    setup_requires=["setuptools-odoo"],
     odoo_addons={"odoo_version_override": "10.0"},
     install_requires=["odoo-autodiscover", "click-odoo-contrib>=1.10.1"],
 )
