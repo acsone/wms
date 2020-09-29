25c5d7ec4ca0700abc0f1fc93a1612a71bc5f93d
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-09-29 11:40:29 +0200

    [IMP] alc_sale_product_qty_backorder: Move logic to compute the qty into backorder on a SO line in its own addon refs #46789

---
+++ odoo/addons/alc_sale_product_qty_backorder/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Sale Product Qty Backorder",
+    "description": """
+        Alcyon: Product Qty into backorder on the sale order line""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["procurement_sale", "sale_cancel_remaining"],
+    "data": [],
+    "demo": [],
+}
---
+++ odoo/addons/alc_sale_product_qty_backorder/models/sale_order_line.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import odoo.addons.decimal_precision as dp
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = "sale.order.line"
+
+    product_qty_backorder = fields.Float(
+        string="Qty into back order",
+        digits=dp.get_precision("Product Unit of Measure"),
+        compute="_compute_product_qty_backorder",
+        store=True,
+        help="As long as no quantity has been delivered, the BO quantity is "
+        "the quantity unavailable at the time of the order minus the canceled "
+        "quantity. Otherwise it is the quantity remaining to be delivered.",
+    )
+
+    @api.multi
+    @api.depends(
+        "qty_delivered",
+        "product_qty_canceled",
+        "product_qty_unavailable",
+        "product_qty_remains_to_deliver",
+    )
+    def _compute_product_qty_backorder(self):
+        """
+        As long as no quantity has been delivered, the BO quantity is the
+        quantity unavailable at the time of the order minus the canceled
+        quantity. Otherwise it is the quantity remaining to be delivered.
+        """
+        for record in self:
+            if record.qty_delivered == 0 and record.product_qty_canceled == 0:
+                record.product_qty_backorder = record.product_qty_unavailable
+            else:
+                record.product_qty_backorder = record.product_qty_remains_to_deliver
