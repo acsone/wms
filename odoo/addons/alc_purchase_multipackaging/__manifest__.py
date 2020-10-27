5d9af24d91ec251469645118741013eed5fdee58
Lindsay <lindsay.marion@acsone.eu>
2020-10-27 09:13:01 +0100

    [IMP] Add multipackaging info on purchase screen

---
+++ odoo/addons/alc_purchase_multipackaging/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Purchase Multipackaging",
+    "description": """
+        Allow to have multipackaging in the purchase screen""",
+    "version": "10",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "depends": ["purchase_order_line_packaging_qty", "purchase"],
+    "data": [],
+    "demo": [],
+}
