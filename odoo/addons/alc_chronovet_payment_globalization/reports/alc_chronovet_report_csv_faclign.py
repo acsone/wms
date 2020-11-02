af18f3a2faa130aad58e5b659ce0db539d00e158
Lindsay <lindsay.marion@acsone.eu>
2020-11-02 16:45:04 +0100

    [IMP] Add faclign csv export + typo correction in facpied export

--- odoo/addons/alc_chronovet_payment_globalization/reports/alc_chronovet_report_csv_faclign.py
+++ odoo/addons/alc_chronovet_payment_globalization/reports/alc_chronovet_report_csv_faclign.py
@@ -15,47 +15,43 @@ class AlcChronovetReportCsvFaclign(models.AbstractModel):
         file.writeheader()
         invoices = self._get_all_invoices(account_move)
         for invoice in invoices:
-            file.writerow(
-                {
-                    "CFACT": invoice.partner_id.ref,
-                    "CLIVR": invoice.partner_id.ref,
-                    "NOM": invoice.partner_id.name,
-                    "REGROUP": "",
-                    "TYPE": invoice.type,
-                    "COMMANDE": invoice.purchase_id.id,
-                    "NCOMM": "",
-                    "DATCDE": invoice.purchase_id.date_order,
-                    "NLIVR": "",
-                    "NFACT": invoice.number,
-                    "DATFACT": invoice.date_invoice,
-                    "CDART": invoice.invoice_line_ids[0].product_id.code,
-                    "DESART": invoice.invoice_line_ids[0].product_id.description,
-                    "PRIXUN": invoice.invoice_line_ids[0].price_unit,  # TBC
-                    "PRIXREM": "",
-                    "QTFACT": invoice.quantity,
-                    "MONTHT": invoice.amount_untaxed,
-                    "PDSBRUT": "",
-                    "PDSNET": "",
-                    "GTIN14": invoice.purchase_id.product_id.barcode,
-                    "LABORATOIRE": "",
-                    "CLASSE": "",
-                    "ESCOMPTE": 0,
-                    "TVA": invoice.tax_line_ids[0].tax_id.amount,
-                    "PROMO": invoice.discount,
-                    "PROPRIETAIRE": "",
-                    "CLTHER": "",
-                    "SCLTHER": "",
-                    "SSCLTHER": "",
-                    "CATEG": "",
-                    "MONTESC": "",
-                    "TOTALHT": invoice.amount_untaxed,
-                    "MONTTVA": invoice.amount_tax,
-                    "TOTALTTC": invoice.amount_total,
-                    "DATLIV": invoice.purchase_id.date_done,  # TBC --date done on stock picking
-                    "CIP": "",
-                    "PUCAT": "",
-                }
-            )
+            for invoice_line in invoice.invoice_line_ids:
+                for sale_order in invoice_line.sale_line_ids:
+                    file.writerow(
+                        {
+                            "CFACT": invoice.partner_id.ref,
+                            "CLIVR": invoice.partner_id.ref,
+                            "NOM": invoice.partner_id.name,
+                            "TYPE": invoice.type,
+                            "COMMANDE": sale_order.order_id.name,
+                            "NCOMM": sale_order.order_id.b2c_ref,
+                            "DATCDE": sale_order.date_order,
+                            "NLIVR": sale_order.order_id.picking_ids[0].name,
+                            "NFACT": invoice.number,
+                            "DATFAC": invoice.date_invoice,
+                            "CDART": invoice_line.product_id.default_code,
+                            "DESART": invoice_line.product_id.name,
+                            "PRIXUN": invoice_line.price_unit,
+                            "PRIXREM": invoice_line.price_subtotal
+                            / invoice_line.quantity,
+                            "QTFACT": invoice_line.quantity,
+                            "MONTHT": invoice_line.price_subtotal,
+                            "GTIN14": invoice_line.product_id.barcode,
+                            "LABORATOIRE": invoice_line.product_id.supplier_id.name,
+                            "TVA": invoice_line.invoice_line_tax_ids[0].amount,
+                            "PROPRIETAIRE": sale_order.order_id.partner_id.name,
+                            "CATEG": invoice_line.product_id.categ_id.parent_id.name
+                            if invoice_line.product_id.categ_id.parent_id
+                            else invoice_line.product_id.categ_id.name,
+                            "TOTALHT": invoice_line.price_subtotal,
+                            "MONTTVA": invoice_line.price_subtotal
+                            * invoice_line.invoice_line_tax_ids[0].amount
+                            / 100.0,
+                            "TOTALTTC": invoice_line.price_subtotal
+                            * (1 + invoice_line.invoice_line_tax_ids[0].amount / 100.0),
+                            "DATLIV": sale_order.order_id.picking_ids[0].date_done,
+                        }
+                    )
 
     def csv_report_options(self):
         res = super(AlcChronovetReportCsvFaclign, self).csv_report_options()
@@ -64,40 +60,29 @@ class AlcChronovetReportCsvFaclign(models.AbstractModel):
                 "CFACT",
                 "CLIVR",
                 "NOM",
-                "REGROUP",
                 "TYPE",
                 "COMMANDE",
                 "NCOMM",
                 "DATCDE",
                 "NLIVR",
                 "NFACT",
-                "DATFACT",
+                "DATFAC",
                 "CDART",
                 "DESART",
                 "PRIXUN",
                 "PRIXREM",
                 "QTFACT",
                 "MONTHT",
-                "PDSBRUT",
-                "PDSNET",
                 "GTIN14",
                 "LABORATOIRE",
                 "CLASSE",
-                "ESCOMPTE",
                 "TVA",
-                "PROMO",
                 "PROPRIETAIRE",
-                "CLTHER",
-                "SCLTHER",
-                "SSCLTHER",
                 "CATEG",
-                "MONTESC",
                 "TOTALHT",
                 "MONTTVA",
                 "TOTALTTC",
                 "DATLIV",
-                "CIP",
-                "PUCAT",
             ]
         )
         res["delimiter"] = ";"
