cdae7a4651104ec3ace4af902ffed29a9b3d1786
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-11-12 14:51:04 +0100

    [ADD] alc_clubvetshop: New B2C backend for clubvetshop refs #47458

---
+++ odoo/addons/alc_clubvetshop/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc ClubVetShop",
+    "description": """
+        Alcyon: ClubVetShop Infos""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": [
+        "alc_b2c_connector",
+        "alc_b2c_connector_pricelist_discount",
+        "account_banking_sepa_direct_debit",
+        "account_payment_mode",
+    ],
+    "data": [
+        "data/account_payment_mode.xml",
+        "data/alc_b2c_backend.xml",
+        "data/res_partner.xml",
+    ],
+    "demo": [],
+}
