8159e91c70d646e0dec041baca2d66bef3104646
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-12-03 08:18:10 +0100

    [IMP] alc_stock_putaway: Add addon to improve putway management refs #47602 refs #47601

---
+++ odoo/addons/alc_stock_putaway/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Stock Putaway",
+    "description": """
+        Alcyon: Manage stock putway""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["stock_storage_type", "stock_storage_type_putaway_abc"],
+    "data": [],
+    "demo": [],
+}
