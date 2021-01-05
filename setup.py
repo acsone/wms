f7d5a50fac9d1565b9c01d60af9177d8733f4eb1
Lindsay <lindsay.marion@acsone.eu>
2021-01-05 11:49:37 +0100

    Revert "Squashed commit of the following:"
    This reverts commit 3ed21c9724db7c8cb9aa03c233d809302d52ad75.

--- setup.py
+++ setup.py
@@ -27,7 +27,5 @@ setup(
         [console_scripts]
         click-odoo-cubiscan-import=scripts.cubiscan_import:main
     """,
-    extras_require={
-        "tests": ["xmlunittest", "freezegun==0.3.14", "unittest2", "responses"]
-    },
+    extras_require={"tests": ["xmlunittest", "freezegun", "unittest2", "responses"]},
 )
