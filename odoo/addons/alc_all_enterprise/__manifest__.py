b2016122bee1bc4e7f6f81240f51045e7b497fbc
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-01-06 08:30:14 +0100

    [IMP] alc_all_enterprise: New main enterprise addon refs #39504

---
+++ odoo/addons/alc_all_enterprise/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ACSONE SA/NV
+
+{
+    "name": "Alc All Enterprise",
+    "description": """
+        Alcyon addons under Odoo Enterprise licence""",
+    "version": "10.0.1.61.1",
+    "license": "Other proprietary",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": [
+        "alc_oeel_helpdesk",
+        "alc_oeel_purchase_report",
+        "alc_oeel_stock_picking_backorder_helpdesk",
+    ],
+    "data": [],
+    "demo": [],
+    "application": True,
+    "installable": True,
+}
