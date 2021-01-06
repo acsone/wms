ad84d52189b76bcc247f47bc81f74fd824b43575
Lindsay <lindsay.marion@acsone.eu>
2021-01-06 18:06:57 +0100

    [IMP] Add news filters

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -101,6 +101,7 @@
         "alc_placedesvetos_to_magento",
         "alc_placedesvetos_payment_globalization",
         "alc_purchase_multipackaging",
+        "alc_product_audit",
         "alc_product_packaging",
         "alc_product_packaging_stock_reserve",
         "alc_product_uom_updatable",
