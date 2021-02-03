5638ee5407ee7309a33886d55ec4f98860a3cd88
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-02-03 09:08:56 +0100

    [IMP] product_abc_classification: Add translations and improve UI fixes #48783

--- odoo/addons/alc_product_abc_classification/models/abc_classification_level.py
+++ odoo/addons/alc_product_abc_classification/models/abc_classification_level.py
@@ -2,7 +2,7 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 from odoo.addons.stock_storage_type_putaway_abc.models.stock_location import (
     ABC_SELECTION,
@@ -13,5 +13,17 @@ class AbcClassificationLevel(models.Model):
 
     _inherit = "abc.classification.level"
     _order = "percentage desc, id desc"
+    _rec_name = "display_name"
 
     name = fields.Selection(ABC_SELECTION, required=True)
+
+    display_name = fields.Char(compute="_compute_display_name")
+
+    @api.depends("name")
+    def _compute_display_name(self):
+        # required since name is a selection field... otherwise the display
+        # name is the value not the label of the selection
+        field_name = self._fields["name"]
+        label_by_value = dict(field_name._description_selection(self.env))
+        for rec in self:
+            rec.display_name = label_by_value[rec.name]
