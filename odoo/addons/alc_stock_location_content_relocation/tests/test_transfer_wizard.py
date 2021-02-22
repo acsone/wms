e31274fadb2a836e17b1f6d917ca788d313aa9dd
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-02-22 11:51:53 +0100

    [FIX] alc_stock_location_content_relocation: Remove unknown fields from tests

--- odoo/addons/alc_stock_location_content_relocation/tests/test_transfer_wizard.py
+++ odoo/addons/alc_stock_location_content_relocation/tests/test_transfer_wizard.py
@@ -10,12 +10,6 @@ class TestTransferWizard(SavepointCase):
     def setUpClass(cls):
         super(TestTransferWizard, cls).setUpClass()
 
-        cls.picking_zone_medoc = cls.env["picking.zone"].create(
-            {"code": "01", "name": "Medicament"}
-        )
-        cls.picking_zone_ali = cls.env["picking.zone"].create(
-            {"code": "02", "name": "Aliment"}
-        )
         cls.stock_location = cls.env.ref("stock.stock_location_stock")
         cls.location_ali = cls.env["stock.location"].create(
             {
@@ -23,7 +17,6 @@ class TestTransferWizard(SavepointCase):
                 "usage": "internal",
                 "act_as_view": True,
                 "location_id": cls.stock_location.id,
-                "picking_zone_id": cls.picking_zone_ali.id,
             }
         )
         cls.location_medoc = cls.env["stock.location"].create(
@@ -32,25 +25,13 @@ class TestTransferWizard(SavepointCase):
                 "usage": "internal",
                 "act_as_view": True,
                 "location_id": cls.stock_location.id,
-                "picking_zone_id": cls.picking_zone_medoc.id,
             }
         )
         cls.zone_gustave = cls.env["stock.location"].create(
             {"name": "G", "location_id": cls.location_medoc.id}
         )
         cls.location = cls.env["stock.location"].create(
-            {
-                "name": "GD80B2",
-                "kind": "bin",
-                "zone": "G",
-                "corridor": "D",
-                "shelf": "80",
-                "height": "B",
-                "box": "2",
-                "location_id": cls.zone_gustave.id,
-                "bin_checksum_1": "45",
-                "bin_checksum_2": "45",
-            }
+            {"name": "GD80B2", "location_id": cls.zone_gustave.id}
         )
         cls.env["stock.location"]._parent_store_compute()
         cls.warehouse = cls.env.ref("stock.warehouse0")
@@ -63,11 +44,8 @@ class TestTransferWizard(SavepointCase):
                 "sequence_id": picking_sequence.id,
                 "default_location_src_id": cls.stock_location.id,
                 "default_location_dest_id": location_out.id,
-                "subcode": "PICK",
-                "groupbypartner": True,
                 "color": 7,
                 "sequence": 4,
-                "picking_zone_id": cls.picking_zone_medoc.id,
             }
         )
 
@@ -78,11 +56,8 @@ class TestTransferWizard(SavepointCase):
                 "sequence_id": picking_sequence.id,
                 "default_location_src_id": cls.stock_location.id,
                 "default_location_dest_id": location_out.id,
-                "subcode": "PICK",
-                "groupbypartner": True,
                 "color": 7,
                 "sequence": 4,
-                "picking_zone_id": cls.picking_zone_ali.id,
             }
         )
 
