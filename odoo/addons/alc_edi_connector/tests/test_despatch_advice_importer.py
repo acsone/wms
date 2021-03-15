7a44656306999e6d26b68654d502f543ed978851
Lindsay <lindsay.marion@acsone.eu>
2021-03-15 15:48:31 +0100

    [IMP] Add tracking of DO on the PO

--- odoo/addons/alc_edi_connector/tests/test_despatch_advice_importer.py
+++ odoo/addons/alc_edi_connector/tests/test_despatch_advice_importer.py
@@ -6,6 +6,9 @@ import base64
 
 import mock
 
+from odoo import fields
+from odoo.tools import file_open
+
 from odoo.addons.queue_job.job import Job
 
 from .common import AlcEdiConnectorCase
@@ -17,6 +20,15 @@ class TestUblDespatchAdviceImporter(AlcEdiConnectorCase):
         super(TestUblDespatchAdviceImporter, cls).setUpClass()
         cls.import_task_def = cls.edi_backend._get_task("ubl.despatch.advice.importer")
         cls.DespatchAdviceImport = cls.env["despatch.advice.import"]
+        with file_open(
+            "alc_edi_connector/tests/files/despatch_advice_tmpl.xml", "rb"
+        ) as f:
+            cls.despatch_advice_xml1 = f.read()
+
+        with file_open(
+            "alc_edi_connector/tests/files/despatch_advice_2.xml", "rb"
+        ) as f:
+            cls.despatch_advice_xml2 = f.read()
 
     def test_01(self):
         """
@@ -87,3 +99,140 @@ class TestUblDespatchAdviceImporter(AlcEdiConnectorCase):
             job.perform()
             self.assertEqual(patched_process_content.call_count, 1)
             self.assertEqual(patched_process_content.call_args[0], (attachment,))
+
+    def test_04(self):
+        """
+        Data: process DespatchAdvice
+        Test case: Check that after updating PO, the DO is attatched to
+        Expected result: We retrieve the DO at the PO level
+        """
+        xml_content = self.despatch_advice_xml1.format(
+            order_id=self.purchase_order.name,
+            line_1_id=self.line1.id,
+            line_1_qty=self.line1.product_qty,
+            line_1_product_ref=self.product_1.default_code,
+            line_1_backorder_qty=12,
+            line_2_id=self.line2.id,
+            line_2_qty=self.line2.product_qty,
+            line_2_product_ref=self.product_2.default_code,
+            line_2_backorder_qty=0,
+        )
+
+        attachment_in = self.env["ir.attachment"].create(
+            {
+                "name": "despatch_advice_tmpl.xml",
+                "datas": base64.b64encode(xml_content),
+                "datas_fname": "despatch_advice_tmpl.xml",
+            }
+        )
+
+        self.env["despatch.advice.import"].process_attachment(attachment_in)
+        attachment_out = self.env["ir.attachment"].search(
+            [
+                ("name", "=", "despatch_advice_tmpl.xml"),
+                ("res_id", "=", self.purchase_order.id),
+            ]
+        )
+        self.assertTrue(attachment_out)
+
+    def test_05(self):
+        """
+        Data: process DespatchAdvice
+        Test case: Check that after updating PO, the DO is attatched to
+        Expected result: We retrieve the DO at the PO level
+        """
+
+        self.product_3 = self.env["product.product"].create(
+            {
+                "name": "Product 1",
+                "seller_ids": [
+                    (0, 0, {"name": self.supplier.id, "product_code": "P3"})
+                ],
+            }
+        )
+        self.product_4 = self.env["product.product"].create(
+            {
+                "name": "Product 2",
+                "seller_ids": [
+                    (0, 0, {"name": self.supplier.id, "product_code": "P4"})
+                ],
+            }
+        )
+
+        self.purchase_order2 = self.env["purchase.order"].create(
+            {
+                "partner_id": self.supplier.id,
+                "date_order": fields.Datetime.now(),
+                "date_planned": fields.Datetime.now(),
+                "currency_id": self.currency_euro.id,
+            }
+        )
+        self.purchase_order3 = self.env["purchase.order"].create(
+            {
+                "partner_id": self.supplier.id,
+                "date_order": fields.Datetime.now(),
+                "date_planned": fields.Datetime.now(),
+                "currency_id": self.currency_euro.id,
+            }
+        )
+        self.line3 = self.purchase_order2.order_line.create(
+            {
+                "order_id": self.purchase_order2.id,
+                "product_id": self.product_3.id,
+                "name": self.product_3.name,
+                "date_planned": fields.Datetime.now(),
+                "product_qty": 10,
+                "product_uom": self.env.ref("product.product_uom_unit").id,
+                "price_unit": 15,
+            }
+        )
+        self.line4 = self.purchase_order3.order_line.create(
+            {
+                "order_id": self.purchase_order3.id,
+                "product_id": self.product_4.id,
+                "name": self.product_4.name,
+                "date_planned": fields.Datetime.now(),
+                "product_qty": 5,
+                "product_uom": self.env.ref("product.product_uom_unit").id,
+                "price_unit": 25,
+            }
+        )
+        self.purchase_order2.button_approve()
+        self.purchase_order3.button_approve()
+
+        xml_content = self.despatch_advice_xml2.format(
+            line_1_id=self.line3.id,
+            line_1_qty=self.line3.product_qty,
+            line_1_product_ref=self.product_3.default_code,
+            line_1_order_id=self.purchase_order2.name,
+            line_1_backorder_qty=0,
+            line_2_id=self.line4.id,
+            line_2_qty=self.line4.product_qty,
+            line_2_order_id=self.purchase_order3.name,
+            line_2_product_ref=self.product_4.default_code,
+            line_2_backorder_qty=0,
+        )
+
+        attachment_in = self.env["ir.attachment"].create(
+            {
+                "name": "despatch_advice_2.xml",
+                "datas": base64.b64encode(xml_content),
+                "datas_fname": "despatch_advice_2.xml",
+            }
+        )
+
+        self.env["despatch.advice.import"].process_attachment(attachment_in)
+        attachment_out1 = self.env["ir.attachment"].search(
+            [
+                ("name", "=", "despatch_advice_2.xml"),
+                ("res_id", "=", self.purchase_order2.id),
+            ]
+        )
+        self.assertTrue(attachment_out1)
+        attachment_out2 = self.env["ir.attachment"].search(
+            [
+                ("name", "=", "despatch_advice_2.xml"),
+                ("res_id", "=", self.purchase_order3.id),
+            ]
+        )
+        self.assertTrue(attachment_out2)
--- odoo/addons/alc_edi_connector/wizards/despatch_advice_import.py
+++ odoo/addons/alc_edi_connector/wizards/despatch_advice_import.py
@@ -4,7 +4,7 @@
 
 import base64
 
-from odoo import models
+from odoo import _, models
 
 from odoo.addons.queue_job.job import job
 
@@ -18,3 +18,37 @@ class DespatchAdviceImport(models.TransientModel):
             base64.b64decode(attachment.datas), attachment.name
         )
         self.process_data(parsed_despatch_document)
+
+        # After process document -- attach DespatchAdvice to all
+        lines = parsed_despatch_document.get("lines")
+
+        body = _("DespatchAdvice received")
+        title = _("Connector EDI")
+
+        order_names = list({line["ref"] for line in lines})
+        orders = self.env["purchase.order"].search([("name", "in", order_names)])
+
+        for order in orders:
+            attachment_out = self.env["ir.attachment"].search(
+                [
+                    ("name", "=", attachment.name),
+                    ("res_id", "=", order.id),
+                    ("res_model", "=", order._name),
+                ]
+            )
+            if not attachment_out:
+                attachment_out = self.env["ir.attachment"].create(
+                    {
+                        "name": attachment.name,
+                        "res_id": order.id,
+                        "res_model": order._name,
+                        "datas": base64.b64encode(attachment.datas),
+                        "datas_fname": attachment.name,
+                    }
+                )
+                order.message_post(
+                    body=body,
+                    subject=title,
+                    subtype="mt_note",
+                    attachment_ids=attachment_out.ids,
+                )
