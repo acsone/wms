c8c4850feda07b31e95c9beb5095b445f783a788
Lindsay <lindsay.marion@acsone.eu>
2021-03-26 11:05:33 +0100

    [FIX] Create and Edit pricelist items

--- odoo/addons/alc_pricelist_items/models/product_pricelist.py
+++ odoo/addons/alc_pricelist_items/models/product_pricelist.py
@@ -11,20 +11,22 @@ class ProductPricelist(models.Model):
 
     @api.multi
     def display_items(self):
-
+        tree_view_id = self.env.ref(
+            "alc_pricelist_items.view_product_pricelist_item_tree"
+        ).id
+        form_view_id = self.env.ref("product.product_pricelist_item_form_view").id
         self.ensure_one()
         action = {
             "name": _("Price list items"),
             "type": "ir.actions.act_window",
-            "view_mode": "tree",
+            "view_mode": "tree, form",
             "res_model": "product.pricelist.item",
             "search_view_id": self.env.ref(
                 "alc_pricelist_items.product_pricelist_item_search_view"
             ).id,
-            "view_id": self.env.ref(
-                "alc_pricelist_items.view_product_pricelist_item_tree"
-            ).id,
+            "views": [(tree_view_id, "tree"), (form_view_id, "form")],
             "domain": [("pricelist_id", "=", self.id)],
+            "context": {"default_pricelist_id": self.id},
             "target": "current",
         }
         return action
