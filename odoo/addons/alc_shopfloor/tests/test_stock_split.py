4c6bb9f6318e9e8b50ab5327584b699f76dc77cc
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-04-30 09:31:20 +0200

    [FIX] alc_shopfloor: Add licence

--- odoo/addons/alc_shopfloor/tests/test_stock_split.py
+++ odoo/addons/alc_shopfloor/tests/test_stock_split.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
+# Copyright 2021 ACSONE SA/NV (https://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import SavepointCase
