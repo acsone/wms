bc142d115889c6475b397c84c45bee3cc9839272
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-05-04 14:46:49 +0200

    [IMP] alc_shopfloor_app: Add shofloor application menu

---
+++ odoo/addons/alc_shopfloor_app/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Shopfloor App",
+    "description": """
+        Alcyon: Shopfloor App""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["alc_shopfloor", "alc_shopfloor_mobile", "alc_shopfloor_user"],
+    "data": [],
+    "demo": [],
+}
