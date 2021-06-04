61dca6fd277f759a166425468b3ebb1778d99edf
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-06-04 14:26:55 +0200

    [IMP] alc_all: location barcode should start with 'L#'

---
+++ odoo/addons/alc_all/migrations/10.0.1.69.5/pre-migrate.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    # initialize location
+    cr.execute(
+        """
+            UPDATE
+                stock_location sl
+            SET
+                barcode = concat('L#', barcode)
+            WHERE
+                barcode like ('L#%')
+        """
+    )
