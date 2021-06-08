c01e9184ee2a53308fe8417d8af44315718544df
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-06-08 09:40:11 +0200

    wip

---
+++ odoo/addons/alc_weighing_widget/static/src/js/web_weighing_widget.js
@@ -0,0 +1,84 @@
+odoo.define("web_weighing_widget", function(require) {
+  "use strict";
+
+  var form_widgets = require("web.form_widgets");
+  var core = require("web.core");
+  var session = require("web.session");
+  var Model = require("web.Model");
+
+  var WeighingWidget = form_widgets.FieldFloat.extend({
+    init: function() {
+      this.proxyUrl = null;
+      this.status = "FIXED";
+      this.weightPoolingIntervalId = null;
+      this._super.apply(this, arguments);
+    },
+
+    process_modifiers: function() {
+      this._super();
+      this.set({readonly: true});
+    },
+    start: function() {
+      var self = this;
+      this.view.on("change:actual_mode", this, this.on_mode_change);
+      return $.when(this._super())
+        .then(() => {
+          new Model("res.users")
+            .call("read", [[session.uid], ["pywebdriver_proxy_ip"]])
+            .done(result => {
+              const data = result[0];
+              self.proxyUrl = data.pywebdriver_proxy_ip;
+            });
+        })
+        .then(() => {
+          self.on_mode_change();
+        });
+    },
+
+    start_weight_pooling: function() {
+      this.weightPoolingIntervalId = setInterval(async function() {
+        const response = await fetch(self.proxyUrl + "/hw_proxy/" + "weight");
+        const data = await response.json();
+        if (self.view.get("actual_mode") !== "view") {
+          self.set_value(data.value);
+          self.set_status(data.status);
+        }
+      }, 500);
+    },
+
+    stop_weight_pooling: function() {
+      if (this.weightPoolingIntervalId) {
+        clearInterval(this.weightPoolingIntervalId);
+      }
+    },
+
+    on_mode_change: function() {
+      if (this.view.get("actual_mode") === "view") {
+        this.start_weight_pooling();
+      } else {
+        this.stop_weight_pooling();
+      }
+    },
+
+    set_status: function(status) {
+      if (this.view.get("actual_mode") !== "view") {
+        this.status = status;
+        const input = this.$("input");
+        let color = "red";
+        if (status === "FIXED") {
+          color = null;
+        }
+        input.style.backgroundColor = color;
+      }
+    },
+
+    is_syntax_valid: function() {
+      return this.status === "FIXED";
+    },
+  });
+  core.form_widget_registry.add("weighing_widget", WeighingWidget);
+
+  return {
+    WeighingWidget: WeighingWidget,
+  };
+});
