b6e073467c3570677f8a66a0649772a1f1e02447
nans <nans.lefebvre@acsone.eu>
2021-06-10 07:20:28 +0200

    [IMP] alc_logiweb: add GLS parcel shop parameter

--- odoo/addons/alc_logiweb/services/sales_service.py
+++ odoo/addons/alc_logiweb/services/sales_service.py
@@ -4,6 +4,9 @@
 
 from odoo.addons.component.core import Component
 
+SCHEMA_CARRIER = {"type": "string", "nullable": True, "required": False}
+SCHEMA_GLS_PARCEL_SHOP = {"type": "string", "required": False, "nullable": True}
+
 
 class SalesService(Component):
 
@@ -11,10 +14,21 @@ class SalesService(Component):
 
     def _validator_create(self):
         res = super(SalesService, self)._validator_create()
-        res["carrier"] = {
-            "type": "string",
-            "nullable": True,
-            "required": False,
-            "allowed": ["GLS_BE", "GLS_FR"],
-        }
+        carriers = SCHEMA_CARRIER.copy()
+        carriers["allowed"] = self.env["sale.order"]._b2c_carriers().keys()
+        res["carrier"] = carriers
+        res["gls_parcel_shop"] = SCHEMA_GLS_PARCEL_SHOP
+        return res
+
+    def _sale_order_to_search_result(self, sale_order):
+        res = super(SalesService, self)._sale_order_to_search_result(sale_order)
+        res["carrier"] = sale_order._carriers_to_b2c(sale_order.carrier_id)
+        res["gls_parcel_shop"] = sale_order.gls_parcel_shop or None
+        return res
+
+    @property
+    def _sale_info_schema(self):
+        res = super(SalesService, self)._sale_info_schema
+        res["carrier"] = SCHEMA_CARRIER
+        res["gls_parcel_shop"] = SCHEMA_GLS_PARCEL_SHOP
         return res
