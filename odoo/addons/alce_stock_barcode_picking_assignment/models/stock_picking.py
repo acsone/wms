a0edad62aac977045b153deafc8263a83bd55bcd
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-06-14 18:38:13 +0200

    [IMP] alce_stock_barcode_picking_assignment: Start the picking by scanning the operator badge refs #51844

--- odoo/addons/alce_stock_barcode_picking_assignment/models/stock_picking.py
+++ odoo/addons/alce_stock_barcode_picking_assignment/models/stock_picking.py
@@ -1,8 +1,42 @@
 # -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
+import json
+import logging
+import re
+from functools import reduce
 
 from odoo import _, models
 from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+SALT = '"zeGE"'
+
+
+def textToChars(text):
+    return map(ord, list(text))
+
+
+def applySaltToChar(salt, code):
+    return reduce(lambda a, b: a ^ b, textToChars(salt), code)
+
+
+def decipher(salt):
+    """decrypt token from QRCode printed on the employee badge and
+    encoded using the methods proposed here:
+    https://stackoverflow.com/questions/18279141/javascript-string-encryption-and-decryption
+    """
+
+    def decode(encoded):
+        encoded = encoded.replace(" ", "")
+        regex = r".{1,2}"
+        vals = re.findall(regex, encoded)
+        vals = map(lambda s: int(s, 16), vals)
+        vals = map(lambda s: applySaltToChar(salt, s), vals)
+        vals = map(unichr, vals)
+        return "".join(vals)
+
+    return decode
 
 
 class StockPicking(models.Model):
@@ -14,10 +48,19 @@ class StockPicking(models.Model):
         an operator
         """
         if not self.operator_id:
-            user = self.env["res.users"].get_user(barcode.replace("U#", ""))
-            if user:
-                self.operator_id = user
-                return None
-            raise UserError(_("Please start operation first"))
+            try:
+                payload = decipher(SALT)(barcode)
+                login = json.loads(payload)["login"]
+                user = self.env["res.users"].search([("login", "=", login)])
+                if user:
+                    self.operator_id = user
+                    return None
+            except Exception:
+                _logger.exception(
+                    "Unable to decode user barcode or wrong user barcode %s", barcode
+                )
+            raise UserError(
+                _("Please scan your user badge first to start the operations")
+            )
 
         return super(StockPicking, self).on_barcode_scanned(barcode)
