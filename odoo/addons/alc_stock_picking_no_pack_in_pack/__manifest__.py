19ab76888a3d63f16502ae521d977e65d48c4d62
Lindsay <lindsay.marion@acsone.eu>
2021-06-14 09:49:18 +0200

    [IMP] No pack in pack for Logiweb (GLS) orders

---
+++ odoo/addons/alc_stock_picking_no_pack_in_pack/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Stock Picking No Pack In Pack",
+    "description": """
+        Prevent pack in pack""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "depends": [
+        "stock",
+        "sale",
+        "sale_stock",
+        "stock_picking_subcode",
+        "alc_delivery_carrier_gls",
+        "delivery_rounds",
+    ],
+    "data": [],
+    "demo": [],
+}
