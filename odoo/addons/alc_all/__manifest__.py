a2b462f04598fdf5095a1e5f4d4e96faf2b464c8
Lindsay <lindsay.marion@acsone.eu>
2021-07-15 09:27:20 +0200

    fix translation

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -134,6 +134,7 @@
         "alc_stock_move_list",
         "alc_stock_picking_no_pack_in_pack",
         "alc_stock_picking_number_package",
+        "alc_stock_picking_gls_check_validation",
         "alc_stock_putaway",
         "alc_stock_put_remaining_to_reserve",
         # "alc_stock_location_content_relocation",
