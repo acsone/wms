c29dc9a391d6a6e344065eccc5580615b803a468
Lindsay <lindsay.marion@acsone.eu>
2021-07-15 09:24:27 +0200

    [FIX] alc_delivery_carrier_gls_b2c: Name2 in gls export infos

--- odoo/addons/alc_delivery_carrier_gls_b2c/models/__init__.py
+++ odoo/addons/alc_delivery_carrier_gls_b2c/models/__init__.py
@@ -1,1 +1,2 @@
 from . import stock_quant_package
+from . import res_partner
--- odoo/addons/alc_delivery_carrier_gls_b2c/models/stock_quant_package.py
+++ odoo/addons/alc_delivery_carrier_gls_b2c/models/stock_quant_package.py
@@ -10,9 +10,6 @@ class StockQuantPackage(models.Model):
 
     b2c_ref = fields.Char(related="gls_picking_id.sale_id.b2c_ref", readonly=True)
     sale_name = fields.Char(related="gls_picking_id.sale_id.name", readonly=True)
-    company_name = fields.Char(
-        related="gls_picking_id.sale_id.partner_id.suite", readonly=True
-    )
 
     def _gls_prepare_shipment(self):
         res = super(StockQuantPackage, self)._gls_prepare_shipment()
@@ -20,13 +17,7 @@ class StockQuantPackage(models.Model):
         if self.b2c_ref:
             reference = "{}-{}".format(self.b2c_ref, self.sale_name)
         res["ShipmentReference"] = [reference[:40]]
-        res["ShipmentUnit"][0][
-            "Note1"
-        ] = self.gls_picking_id.sale_id.partner_id.comment[:50]
-        return res
+        comment = self.gls_picking_id.sale_id.partner_id.comment
+        res["ShipmentUnit"][0]["Note1"] = comment[:50] if comment else ""
 
-    def _gls_prepare_address(self):
-        address_payload = super(StockQuantPackage, self)._gls_prepare_address()
-        if self.gls_picking_id.sale_id.partner_id.suite:
-            address_payload["Name2"] = self.gls_picking_id.sale_id.partner_id.suite
-        return address_payload
+        return res
