d736a3fb7de5320dc99ba2205a9093cab44fc29b
nans <nans.lefebvre@acsone.eu>
2021-07-19 17:28:05 +0200

    [MIG] alc_delivery_rounds_gls: mark existing GLS pickings as delivered

--- odoo/addons/alc_delivery_rounds_gls/__manifest__.py
+++ odoo/addons/alc_delivery_rounds_gls/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Alcyon GLS Delivery Rounds",
     "description": "Alcyon GLS Delivery Rounds",
-    "version": "10.0.1.0.0",
+    "version": "10.0.2.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
