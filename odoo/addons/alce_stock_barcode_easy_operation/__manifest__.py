b08c32f34a00f74b1cc5f5c27e2bbd44ae52ccee
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-07-28 11:31:31 +0200

    [IMP] alce_stock_barcode_easy_operation: Avoid to scan unwanted lot or more than expected qty with barcode app refs #52616

--- odoo/addons/alce_stock_barcode_easy_operation/__manifest__.py
+++ odoo/addons/alce_stock_barcode_easy_operation/__manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# Copyright 2021 ACSONE SA/NV
 
 {
-    "name": "Specific Barcode",
+    "name": "Alce Stock Barcode Easy Operation",
     "version": "10.0.1.0.0",
-    "author": "BCIM",
-    "maintainer": "Camptocamp",
+    "author": "BCIM, ACSONE SA/NV",
+    "maintainer": "ACSONE SA/NV",
     "category": "Stock Management",
     "depends": ["stock_barcode"],
     "data": [],
--- odoo/addons/alce_stock_barcode_easy_operation/models/stock_picking.py
+++ odoo/addons/alce_stock_barcode_easy_operation/models/stock_picking.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# Copyright 2021 ACSONE SA/NV
 
 import re
 
 from odoo import _, models
 from odoo.exceptions import UserError
+from odoo.tools import float_compare
 
 LOT_BARCODE = re.compile(r"#(?P<product_default_code>\w+)#(?P<lot_name>\w+)#?")
 
@@ -62,9 +64,61 @@ class StockPicking(models.Model):
                 }
             }
         qty_done = op.qty_done
+        new_qty = qty_done + 1
+        if (
+            float_compare(
+                new_qty,
+                op.product_qty,
+                precision_digits=op._fields["qty_done"].digits[1],
+            )
+            > 0
+        ):
+            return {
+                "warning": {
+                    "title": _("Expected quantity exceeded"),
+                    "message": _(
+                        "Too much product scanned for operation with product %s.\n"
+                        "Expected %s."
+                    )
+                    % (product_default_code, op.product_qty),
+                }
+            }
+        new_lot_qty = oplot.qty + 1
+        if (
+            float_compare(
+                new_lot_qty,
+                oplot.qty_todo,
+                precision_digits=oplot._fields["qty"].digits[1],
+            )
+            > 0
+        ):
+            remaining_pack_lot_ids = op.pack_lot_ids.filtered(
+                lambda a: a.qty < a.qty_todo
+            )
+            remaining_lot_names = u", ".join(
+                remaining_pack_lot_ids.mapped("lot_id.name")
+            )
+            return {
+                "warning": {
+                    "title": _("Expected lot quantity exceeded"),
+                    "message": _(
+                        "Too much lot %s scanned for operation with product %s.\n"
+                        "Expected %s lot %s\n"
+                        "Remaining lot(s) to scan: %s"
+                    )
+                    % (
+                        lot_name,
+                        product_default_code,
+                        op.product_qty,
+                        lot_name,
+                        remaining_lot_names,
+                    ),
+                }
+            }
+
         # Force writing in DB
-        op.write({"qty_done": qty_done + 1})
-        oplot.write({"qty": oplot.qty + 1})
+        op.write({"qty_done": new_qty})
+        oplot.write({"qty": new_lot_qty})
         # Now update the UI
         op.qty_done = qty_done + 1
         return None
@@ -92,8 +146,27 @@ class StockPicking(models.Model):
                 }
             }
         qty_done = op.qty_done
+        new_qty = qty_done + 1
+        if (
+            float_compare(
+                new_qty,
+                op.product_qty,
+                precision_digits=op._fields["qty_done"].digits[1],
+            )
+            > 0
+        ):
+            return {
+                "warning": {
+                    "title": _("Expected quantity exceeded"),
+                    "message": _(
+                        "Too much product scanned for operation with product %s.\n"
+                        "Expected %s."
+                    )
+                    % (product.default_code, op.product_qty),
+                }
+            }
         # Force writing in DB
-        op.write({"qty_done": qty_done + 1})
+        op.write({"qty_done": new_qty})
         # Now update the UI
         op.qty_done = qty_done + 1
         return None
