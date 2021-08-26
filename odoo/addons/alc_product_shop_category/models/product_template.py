103f8760104cfe355bda30777437adc6698f3a54
nans <nans.lefebvre@acsone.eu>
2021-08-26 19:55:46 +0200

    [ADD] alc_product_shop_category: separate web and internal categories

---
+++ odoo/addons/alc_product_shop_category/models/product_template.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    categ_id = fields.Many2one(domain=[("is_web", "=", False)])
+    categ_ids = fields.Many2many(domain=[("is_web", "=", True)])
