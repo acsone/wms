4e52d6b988813991a37ccccd0e952d03db1b7d8f
nans <nans.lefebvre@acsone.eu>
2021-09-13 18:11:58 +0200

    [IMP] alc_pim: add alc_product_pharmacy attributes

--- odoo/addons/alc_pim/__manifest__.py
+++ odoo/addons/alc_pim/__manifest__.py
@@ -14,6 +14,7 @@
         "alc_product_shop_category",
         "alc_product_link_notice",
         "product_animal_species",
+        "alc_product_pharmacy",
         # data dependencies
         "product_dimension",
         "stock_production_lot_expiry",
