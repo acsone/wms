b835e8e78250301d11672faf107a5e144752818c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-09-27 16:36:25 +0200

    [IMP] alc_delivery_rounds_geooptimize: Add translations and fix display refs #53424

--- odoo/addons/alc_delivery_rounds_geooptimize/models/round_instance.py
+++ odoo/addons/alc_delivery_rounds_geooptimize/models/round_instance.py
@@ -487,10 +487,12 @@ class RoundInstance(models.Model):
         if self.print_all_deliveryslip_after_geo_optimization:
             action = self.print_all_deliveryslip()
             self.env.user.notify_info(
-                _("Your delivery slip for %s is now ready. Click to print"),
+                title=self.display_name,
+                meessage=_("Your delivery slip are now ready. Click to print"),
                 sticky=True,
                 action=action,
                 action_link_name=_("Print"),
+                show_reload=True,
             )
 
     def _sort_round_instance_customers(self):
