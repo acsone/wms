c461bafb4a0acb2f51bc684efd749f6ff5d57366
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-10-14 17:25:50 +0200

    [IMP] stock_operation_recompute: Preserve operation infos on recomputed pack operations

--- odoo/addons/stock_operation_recompute/models/stock_move.py
+++ odoo/addons/stock_operation_recompute/models/stock_move.py
@@ -3,13 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 from odoo.tools.float_utils import float_compare
 
 _logger = logging.getLogger(__name__)
+
+OperationKey = namedtuple(
+    "OperationKey", ["product_id", "location_id", "package_id", "lot_id", "picking_id"]
+)
 
 
 class StockMove(models.Model):
@@ -24,37 +27,87 @@ class StockMove(models.Model):
                 "linked_move_operation_ids.operation_id"
             )
 
+    @api.model
+    def _to_operation_key(self, operation, lot=None):
+        key = {
+            "product_id": operation.product_id.id,
+            "location_id": operation.location_id.id,
+            "package_id": operation.package_id.id,
+            "lot_id": lot.id if lot else None,
+            "picking_id": operation.picking_id.id,
+        }
+        return OperationKey(**key)
+
+    def _backup_operation_to_recompute_info(self):
+        ret = {}
+        for rec in self:
+            for op in rec.mapped("linked_move_operation_ids.operation_id"):
+                values = op._backup_for_recompute()[0]
+                if op.pack_lot_ids:
+                    for pack_lot in op.pack_lot_ids:
+                        key = rec._to_operation_key(op, pack_lot.lot_id)
+                        ret[key] = values
+                else:
+                    key = rec._to_operation_key(op)
+                    ret[key] = values
+        return ret
+
+    @api.model
+    def _recover_pack_op_datas(self, operation, operations_data_backup):
+        key = self._to_operation_key(operation=operation)
+        backup = operations_data_backup.get(key)
+        if not backup:
+            return
+        self._update_operation_from_backup(operation, backup.copy())
+
+    @api.model
+    def _recover_pack_op_lot_datas(self, operation, operations_data_backup):
+        if not operation.pack_lot_ids:
+            return
+        operation_updated = False
+        for pack_lot in operation.pack_lot_ids:
+            lot = pack_lot.lot_id
+            key = self._to_operation_key(operation=operation, lot=lot)
+            backup = operations_data_backup.get(key)
+            if not backup:
+                continue
+            if not operation_updated:
+                self._update_operation_from_backup(operation, backup.copy())
+                operation_updated = True
+            lot_backup = backup["lots"].get(lot.id)
+            if lot_backup:
+                self._update_lot_operation_from_backup(pack_lot, lot_backup.copy())
+
+    @api.model
+    def _update_operation_from_backup(self, operation, backup):
+        backup.pop("lots", None)
+        operation.write(backup)
+
+    @api.model
+    def _update_lot_operation_from_backup(self, lot_operation, backup):
+        lot_operation.write(backup)
+
+    @api.model
+    def _recover_pack_ops_datas(self, new_ops, operations_data_backup):
+        for operation in new_ops:
+            if operation.pack_lot_ids:
+                self._recover_pack_op_lot_datas(operation, operations_data_backup)
+            else:
+                self._recover_pack_op_datas(operation, operations_data_backup)
+
     def _recompute_pack_op(self):  # noqa: C901
+        # Re-reserve quants
+        self.action_assign(no_prepare=True)
+
+        # preserve some informations
+        operations_data_backup = self._backup_operation_to_recompute_info()
+
         moves_by_picking = defaultdict(list)
         for m in self:
             moves_by_picking[m.picking_id].append(m.id)
         moves_by_picking = {k: self.browse(v) for k, v in moves_by_picking.items()}
 
         for picking, moves in moves_by_picking.items():
-            # Re-reserve quants
-            moves.action_assign(no_prepare=True)
-
-            # Backup qty done
-            ops = moves.mapped("linked_move_operation_ids.operation_id")
-            for op in ops:
-                _logger.debug(
-                    "Old operation %s %s",
-                    op,
-                    [
-                        u"{}: {}/{}".format(plot.lot_id, plot.qty, plot.qty_todo)
-                        for plot in op.pack_lot_ids
-                    ],
-                )
-            qty_done = {}
-            for op in ops:
-                done = qty_done.setdefault(op.location_id.id, {})
-                if op.product_id.tracking == "none" and op.qty_done:
-                    # Set 0 as lot_id for products without tracking
-                    done[0] = op.qty_done
-                    continue
-                for l in op.pack_lot_ids:
-                    if l.qty:
-                        done[l.lot_id.id] = l.qty
 
             # Check if product additional has been done
             additional_ctx = {}
@@ -67,12 +120,13 @@ class StockMove(models.Model):
                 additional_ctx = dict(skip_additional=True)
 
             # Delete pack op
+            ops = moves.mapped("linked_move_operation_ids.operation_id")
             ops.with_context(**additional_ctx).unlink()
 
             # Re-generate pack ops - similar to do_prepare_partial
             forced_qties = {}
             picking_quants = moves.env["stock.quant"]
-            for move in self:
+            for move in moves:
                 if move.state not in ("assigned", "confirmed", "waiting"):
                     continue
                 move_quants = move.reserved_quant_ids
@@ -108,30 +162,8 @@ class StockMove(models.Model):
                 lambda o, moves=moves: o.product_id in moves.mapped("product_id")
             )
 
-            # Recover the qty done
-            for location_id, lines in qty_done.iteritems():
-                for lot_id, qty in lines.iteritems():
-                    nop = new_ops.filtered(
-                        lambda op, loc_id=location_id: op.location_id.id == loc_id
-                    )
-                    # lot_id == 0 on products without tracking
-                    if not lot_id:
-                        nop.qty_done = qty
-                    else:
-                        nol = nop.pack_lot_ids.filtered(
-                            lambda line, l_id=lot_id: line.lot_id.id == l_id
-                        )
-                        if not nol:
-                            raise UserError(
-                                _(
-                                    "Internal Error. "
-                                    "Cannot match done lot in new pack operation"
-                                )
-                            )
-                        nol.qty = qty
-
-            new_ops.save()
-
+            # Recover data
+            self._recover_pack_ops_datas(new_ops, operations_data_backup)
             # recompute the remaining quantities all at once
             picking.do_recompute_remaining_quantities()
             for pack in new_ops:
@@ -154,4 +186,3 @@ class StockMove(models.Model):
 
     def force_recompute_pack_operation(self):
         self._recompute_pack_op()
-
