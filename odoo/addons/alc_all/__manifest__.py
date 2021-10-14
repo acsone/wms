85f38ab82e3ce817efd91bcb2957005fe4f557c9
Lindsay <lindsay.marion@acsone.eu>
2021-10-14 14:19:02 +0200

    [IMP] Force to re-geolocalize partner if address has changed

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -106,6 +106,7 @@
         "alc_gls_putinpack",
         "alc_label_printing_gls",
         "alc_logiweb",
+        "alc_partner_force_re_geolocalization",
         "alc_partner_manual_sale_order",
         "alc_placedesvetos",
         "alc_placedesvetos_payment_globalization",
