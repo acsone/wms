9ceb69f2d9696127c4700ce050028e4ab1e0307c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-10-15 17:26:27 +0200

    [FIX] alc_partner_force_re_geolocalization: Never call external ressources into tests...

--- odoo/addons/alc_partner_force_re_geolocalization/tests/test_force_relocalization.py
+++ odoo/addons/alc_partner_force_re_geolocalization/tests/test_force_relocalization.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import random
 
 from odoo.tests.common import SavepointCase
 
@@ -19,16 +20,27 @@ class TestForceRelocalization(SavepointCase):
             "ref": "12344566777878",
             "customer": True,
             "is_b2c_customer": False,
+            "partner_latitude": 50.62998,
+            "partner_longitude": 4.86337,
         }
         cls.partner1 = cls.env["res.partner"].create(vals_partner)
-        cls.partner1.geo_localize()
+
+    def _geolocalize(self):
+        partner_latitude = 50.62998 + random.randint(0, 1000) / 1000
+        partner_longitude = 4.86337 + random.randint(0, 1000) / 1000
+        self.partner1.write(
+            {
+                "partner_latitude": partner_latitude,
+                "partner_longitude": partner_longitude,
+            }
+        )
 
     def test_change_address(self):
         self.assertFalse(self.partner1.coordinates_should_be_checked)
         self.partner1.write({"street": "rue du malcampé"})
         self.assertTrue(self.partner1.coordinates_should_be_checked)
 
-        self.partner1.geo_localize()
+        self._geolocalize()
         self.assertFalse(self.partner1.coordinates_should_be_checked)
 
     def test_change_address_twice(self):
@@ -38,9 +50,9 @@ class TestForceRelocalization(SavepointCase):
         self.partner1.write({"street": "rue de laloux"})
         self.assertTrue(self.partner1.coordinates_should_be_checked)
 
-        self.partner1.geo_localize()
+        self._geolocalize()
         self.assertFalse(self.partner1.coordinates_should_be_checked)
 
     def test_just_geolocalize(self):
-        self.partner1.geo_localize()
+        self._geolocalize()
         self.assertFalse(self.partner1.coordinates_should_be_checked)
