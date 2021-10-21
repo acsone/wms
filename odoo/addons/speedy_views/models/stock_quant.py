58751b8bca716753723fcbedc3d0ba771b01c6c7
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-10-21 18:34:01 +0200

    [IMP] speedy_views: Add partial index to speed up product qty computation

--- odoo/addons/speedy_views/models/stock_quant.py
+++ odoo/addons/speedy_views/models/stock_quant.py
@@ -3,10 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
-from odoo import fields, models
+from odoo import api, fields, models
+
+from .utils import create_index
 
 
 class StockQuant(models.Model):
     _inherit = "stock.quant"
 
     negative_move_id = fields.Many2one(index=True)
+
+    @api.model_cr
+    def init(self):
+        # index for incoming and ongoing move in product qty compute
+        index_name = "stock_quant_product_id_qty_in_loc_idx"
+        create_index(
+            self.env.cr,
+            index_name,
+            self._table,
+            "(product_id) include (id, product_id, qty, location_id)",
+        )
