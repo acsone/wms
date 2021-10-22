fbe1c4e9c33a9cc831a9d87d3e69b73ed63a7f18
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-10-22 16:08:04 +0200

    [FIX] speedy_views: Log when an index is created

--- odoo/addons/speedy_views/models/utils.py
+++ odoo/addons/speedy_views/models/utils.py
@@ -12,6 +12,7 @@ _logger = logging.getLogger(__name__)
 def create_index(cr, index_name, table, expression):
     cr.execute("SELECT indexname FROM pg_indexes WHERE indexname = %s", (index_name,))
     if not cr.fetchone():
+        _logger.info("Create index %s on %s %s", index_name, table, expression)
         cr.execute(
             "CREATE INDEX %s " "ON %s %s",
             (AsIs(index_name), AsIs(table), AsIs(expression)),
