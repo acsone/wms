b62fcf850443bcc25566471fb19679378724f6e1
Lindsay <lindsay.marion@acsone.eu>
2021-11-09 08:55:56 +0100

    [IMP] Add custom filters on invoices

---
+++ odoo/addons/alc_invoices_audit/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Invoices Audit",
+    "description": """
+        Add custom filters for invoicing""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "depends": ["alc_sale_invoicing_policy", "specific_partner"],
+    "data": ["views/account_invoice.xml"],
+    "demo": [],
+}
