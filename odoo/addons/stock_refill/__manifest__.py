770818f456daace1e0949d6983bdebdb71377596
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-11-16 15:23:42 +0100

    [IMP] stock_refill: Allows to ignore putaway into reseve at picking type level
    refs #54761

--- odoo/addons/stock_refill/__manifest__.py
+++ odoo/addons/stock_refill/__manifest__.py
@@ -9,7 +9,7 @@
     "maintainer": "Camptocamp",
     "category": "Stock Management",
     "depends": ["stock", "stock_quant_bylocation"],
-    "data": ["views/stock_location.xml"],
+    "data": ["views/stock_picking_type.xml", "views/stock_location.xml"],
     "installable": True,
     "auto_install": False,
     "license": "AGPL-3",
