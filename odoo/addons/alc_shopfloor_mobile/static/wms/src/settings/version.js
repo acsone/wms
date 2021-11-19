8e7d4f98402ae451e1c7a02865b9faf3c6e483ff
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-11-19 14:19:25 +0100

    Bump version: 1.78.8 → 1.78.9

--- odoo/addons/alc_shopfloor_mobile/static/wms/src/settings/version.js
+++ odoo/addons/alc_shopfloor_mobile/static/wms/src/settings/version.js
@@ -3,4 +3,4 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-document.querySelector(".version-number").textContent = "1.78.8";
+document.querySelector(".version-number").textContent = "1.78.9";
