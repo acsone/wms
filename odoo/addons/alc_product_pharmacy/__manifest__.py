99e30884d0aa7ef2fc33a6572ce14eca58035029
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-11-29 10:23:05 +0100

    [FIX] Add migration scritpts to avoid issues due to fields renamed or moved to other addon

--- odoo/addons/alc_product_pharmacy/__manifest__.py
+++ odoo/addons/alc_product_pharmacy/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Alc Pharmacy Product fields",
     "description": """Alcyon Pharmacy Product fields""",
-    "version": "10.0.1.0.0",
+    "version": "10.0.2.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
