374efc024918a8306c1f837928baceec50225a1d
nans <nans.lefebvre@acsone.eu>
2022-01-31 13:54:19 +0100

    [ADD] shopinvader_search_engine_export: force product export

---
+++ odoo/addons/shopinvader_search_engine_export/models/product_template.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    def shopinvader_manual_export(self):
+        self.mapped("product_variant_ids").shopinvader_manual_export()
