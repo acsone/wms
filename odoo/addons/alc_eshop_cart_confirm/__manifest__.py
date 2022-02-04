67e5179c1d7a30ce003ac87da11574d03ea555b5
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-02-04 13:53:03 +0100

    [IMP] alc_eshop_cart_Confirm: Add new method to confirm the card without launching a payment process

---
+++ odoo/addons/alc_eshop_cart_confirm/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Eshop Cart Confirm",
+    "description": """
+        Alcyon manage cart confirmation""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["shopinvader", "sale_confirm_background"],
+    "data": [],
+    "demo": [],
+}
