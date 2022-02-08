1a45e57fe72696c15d909d6cf3364c2fc4892dec
nans <nans.lefebvre@acsone.eu>
2022-02-08 08:46:51 +0100

    [REF] alc_elasticsearch_security, pricelist_role_name: extract shared roles

---
+++ odoo/addons/pricelist_role_name/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Pricelist Role Names",
+    "description": """Pricelist Role Names""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["product"],
+    "application": False,
+    "data": [],
+    "demo": [],
+    "external_dependencies": {"python": ["slugify"]},
+}
