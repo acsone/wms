e9c1fe49bfa70ea2bd19a05bef07bc2413cfcd1a
Lindsay <lindsay.marion@acsone.eu>
2022-02-09 08:20:35 +0100

    [IMP] Exclude logiweb SO from automatic cancellation

---
+++ odoo/addons/alc_sale_processing_finalizer_exclude_logiweb/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Sale Processing Finalizer Exclude Logiweb",
+    "description": """
+        exclude logiweb sale orders from automatic cancellation""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "depends": ["alc_logiweb", "alc_sale_processing_finalizer"],
+    "data": [],
+    "demo": [],
+}
