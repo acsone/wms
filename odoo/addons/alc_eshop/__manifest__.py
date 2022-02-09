962754aa60046de2c62a269cf9d152f49180e43c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-02-09 12:50:54 +0100

    [ADD] alc_eshop_cart_recovery: Add new module to allows to recover a cart from one browser to an other. In the same time ensure that the recovered cart is for the authenticated partner

--- odoo/addons/alc_eshop/__manifest__.py
+++ odoo/addons/alc_eshop/__manifest__.py
@@ -15,6 +15,7 @@
         "alc_storage",
         "alc_eshop_auth_jwt",
         "alc_eshop_cart_confirm",
+        "alc_eshop_cart_recovery",
         "alc_eshop_filter_data",
         "alc_shopinvader_category",
         "shopinvader_auth_jwt",
