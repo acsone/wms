4396c5a8301aa107f7419b4462bab064dd11f3be
nans <nans.lefebvre@acsone.eu>
2022-02-10 21:31:24 +0100

    [TEST] alc_search_engine: test shopinvader product export

---
+++ odoo/addons/alc_search_engine/tests/test_export.py
@@ -0,0 +1,33 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from .common import TestExport
+
+
+class TestExportFlow(TestExport):
+    def test_discounts(self):
+        values_discount = {
+            "date_start": self.tomorrow,
+            "date_end": self.tomorrow,
+            "discount_sale": 10,
+        }
+        self.supplierinfo_model.create(self.get_supplierinfo_vals(**values_discount))
+        values_promo = {
+            "date_start": self.in_two_days,
+            "date_end": self.in_two_days,
+            "ratio_main_product": 2,
+            "ratio_promotional_product": 3,
+        }
+        self.supplierinfo_model.create(self.get_supplierinfo_vals(**values_promo))
+
+        self.product.update_price_cache()
+
+        # when
+        parser = self.export.get_json_parser()
+        binding = self.product.shopinvader_bind_ids
+        data = binding.jsonify(parser)[0]
+
+        # then
+        self.assertEqual(data["supplier_discount"], [values_discount])
+        self.assertEqual(data["supplier_promotion"], [values_promo])
