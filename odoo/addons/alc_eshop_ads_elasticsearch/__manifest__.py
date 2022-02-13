aa4ccf27a2717af4c97e54a416d790a9d5d4129a
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-02-13 11:07:25 +0100

    [ADD] alc_eshop_ads_elasticsearch: New addon to export adds to elasticsearch
    refs #57187

---
+++ odoo/addons/alc_eshop_ads_elasticsearch/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Eshop Ads Elasticsearch",
+    "description": """
+        Alcyon: Manage publication of alcyon ads to ES""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["alc_eshop_ads", "connector_elasticsearch", "queue_job_cron"],
+    "data": [
+        "views/alc_eshop_ads.xml",
+        "views/se_backend_elasticsearch.xml",
+        "data/ir_cron.xml",
+        "data/se_index.xml",
+    ],
+    "demo": [],
+}
