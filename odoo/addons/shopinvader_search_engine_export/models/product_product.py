6dbbdfc8ada762fb17bff76a8e4b394a66ed3e95
nans <nans.lefebvre@acsone.eu>
2022-02-14 11:59:24 +0100

    [IMP] shopinvader_search_engine_export: make manual export delayable

--- odoo/addons/shopinvader_search_engine_export/models/product_product.py
+++ odoo/addons/shopinvader_search_engine_export/models/product_product.py
@@ -4,11 +4,14 @@
 
 from odoo import models
 
+from odoo.addons.queue_job.job import job
+
 
 class ProductProduct(models.Model):
 
     _inherit = "product.product"
 
+    @job(default_channel="root.background.process")
     def shopinvader_manual_export(self):
         bindings = self.with_context(active_test=False).mapped("shopinvader_bind_ids")
         bindings.recompute_json()
--- odoo/addons/shopinvader_search_engine_export/models/product_template.py
+++ odoo/addons/shopinvader_search_engine_export/models/product_template.py
@@ -4,10 +4,13 @@
 
 from odoo import models
 
+from odoo.addons.queue_job.job import job
+
 
 class ProductTemplate(models.Model):
 
     _inherit = "product.template"
 
+    @job(default_channel="root.background.process")
     def shopinvader_manual_export(self):
         self.mapped("product_variant_ids").shopinvader_manual_export()
