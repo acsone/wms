aabcfcb309a92d342ff3b1d4670cf948a8d72970
nans <nans.lefebvre@acsone.eu>
2022-02-22 14:08:42 +0100

    [IMP] mixin_past: add is_future field

--- odoo/addons/mixin_past/models/mixin_past.py
+++ odoo/addons/mixin_past/models/mixin_past.py
@@ -6,11 +6,12 @@ from odoo import api, fields, models
 
 
 class MixinPast(models.AbstractModel):
-    """Adds an 'is_past' field, computed using an existing date_end field."""
+    """Adds 'is_past' and 'is_future' fields, using existing date_start/end fields."""
 
     _name = "mixin.past"
 
     is_past = fields.Boolean(compute="_compute_is_past", store=False)
+    is_future = fields.Boolean(compute="_compute_is_future", store=False)
 
     @api.model
     def _is_past_date(self, maybe_date_end, reference_date=False):
@@ -23,8 +24,25 @@ class MixinPast(models.AbstractModel):
             maybe_date_end = fields.Date.from_string(maybe_date_end)
         return maybe_date_end < reference_date
 
+    @api.model
+    def _is_future_date(self, maybe_date_start, reference_date=False):
+        if not maybe_date_start:
+            return False
+        reference_date = reference_date or fields.Date.context_today(self)
+        if isinstance(reference_date, str):
+            reference_date = fields.Date.from_string(reference_date)
+        if isinstance(maybe_date_start, str):
+            maybe_date_start = fields.Date.from_string(maybe_date_start)
+        return maybe_date_start > reference_date
+
     @api.depends("date_end")
     def _compute_is_past(self):
         today = fields.Date.context_today(self)
         for record in self:
             record.is_past = self._is_past_date(record.date_end, today)
+
+    @api.depends("date_start")
+    def _compute_is_future(self):
+        today = fields.Date.context_today(self)
+        for record in self:
+            record.is_future = self._is_future_date(record.date_start, today)
