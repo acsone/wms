78179ff51152d42f80674724c318565e2dee0ea0
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-03-22 17:49:36 +0100

    [IMP] eshop: Add new cart service to use with shopinvader-js-cart

---
+++ odoo/addons/alc_eshop_api_v2/components/service_context_provider.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import logging
+
+from odoo.addons.component.core import Component
+
+_logger = logging.getLogger(__name__)
+
+
+class ShopinvaderAuthJwtServiceContextProvider(Component):
+    _inherit = "auth_jwt.shopinvader.service.context.provider"
+    _name = "auth_jwt.shopinvader.api.v2.service.context.provider"
+    _collection = "shopinvader.api.v2"
---
+++ odoo/addons/alc_eshop_sale_cart_confirm/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Eshop Sale Cart Confirm",
+    "description": """
+        Alcyon: Manage sale_cart confirmation""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["sale_cart_rest_api", "sale_confirm_background", "onchange_helper"],
+    "data": [],
+    "demo": [],
+}
