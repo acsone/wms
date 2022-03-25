0b3bdd908427117e6809448fef71aa24bb16462e
nans <nans.lefebvre@acsone.eu>
2022-03-25 19:52:02 +0100

    [FIX] alc_eshop_partner_veterinary: add dependency on alc_partner_veterinary

--- odoo/addons/alc_eshop_partner_veterinary/__manifest__.py
+++ odoo/addons/alc_eshop_partner_veterinary/__manifest__.py
@@ -10,7 +10,7 @@
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["sale_cart_rest_api", "shopinvader"],
+    "depends": ["sale_cart_rest_api", "shopinvader", "alc_partner_veterinary"],
     "data": [],
     "demo": [],
 }
