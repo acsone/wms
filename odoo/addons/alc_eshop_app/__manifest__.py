807ffac590a92345cd2bd08fe42bfa2a6b873885
nans <nans.lefebvre@acsone.eu>
2022-03-29 18:38:52 +0200

    [IMP] alc_eshop_app: add dependency on alc_eshop_product_domain

--- odoo/addons/alc_eshop_app/__manifest__.py
+++ odoo/addons/alc_eshop_app/__manifest__.py
@@ -15,6 +15,7 @@
         "alc_eshop_ads",
         "alc_documents_eshop_services",
         "alc_eshop_models_url",
+        "alc_eshop_product_domain",
         "alc_eshop_partner_veterinary",
         "alc_eshop_product_expiry",
         "alc_eshop_product_on_order",
