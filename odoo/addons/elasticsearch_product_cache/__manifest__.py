8676c83cba1d08873ce4da4b23857d4c3dbe54ac
nans <nans.lefebvre@acsone.eu>
2022-04-01 19:07:09 +0200

    [ADD] elasticsearch_product_cache

---
+++ odoo/addons/elasticsearch_product_cache/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "ElasticSearch Product Cache",
+    "description": """Use ES as a product cache""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["connector_elasticsearch", "alc_eshop"],
+    "application": False,
+    "data": [],
+    "demo": [],
+    "external_dependencies": {"python": ["elasticsearch_dsl"]},
+}
