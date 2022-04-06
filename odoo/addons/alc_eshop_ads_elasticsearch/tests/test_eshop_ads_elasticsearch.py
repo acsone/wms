b3321e9b45565a5447748eb62917a805c28eded6
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-04-06 14:00:59 +0200

    [FIX] alc_eshop_ads_elasticsearch: Fix singleton error on action export
    fixes #58637

--- odoo/addons/alc_eshop_ads_elasticsearch/tests/test_eshop_ads_elasticsearch.py
+++ odoo/addons/alc_eshop_ads_elasticsearch/tests/test_eshop_ads_elasticsearch.py
@@ -97,3 +97,9 @@ class TestEShopAdsElasticsearch(common.TestEShopAdsCommon):
             - existing_jobs
         )
         self.assertEqual(1, len(new_jobs))
+
+    def test_action_export(self):
+        number_index = len(self.all_ads.mapped("se_index_ids"))
+        with mock.patch.object(self.adapter.__class__, "index") as patched_index:
+            self.all_ads.action_export_to_se()
+            self.assertEqual(number_index, patched_index.call_count)
