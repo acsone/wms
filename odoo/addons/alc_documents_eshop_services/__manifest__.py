962c37193d4f9bd5df2a512b4a37d0c098cb6647
nans <nans.lefebvre@acsone.eu>
2022-04-12 14:01:24 +0200

    [REF] alc_documents: use authenticated_partner.mixin

--- odoo/addons/alc_documents_eshop_services/__manifest__.py
+++ odoo/addons/alc_documents_eshop_services/__manifest__.py
@@ -9,6 +9,11 @@
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["alc_cerberus_utils", "alc_documents", "base_jsonify", "base_rest"],
+    "depends": [
+        "alc_cerberus_utils",
+        "alc_documents",
+        "base_jsonify",
+        "authenticated_partner_mixin",
+    ],
     "demo": [],
 }
