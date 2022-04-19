56c187c7a7c74e35c834e8ba3a1217c6a459064e
nans <nans.lefebvre@acsone.eu>
2022-04-19 17:09:43 +0200

    Bump version: 1.84.36 → 1.84.37

--- odoo/addons/alc_shopfloor_mobile/static/wms/src/screen.js
+++ odoo/addons/alc_shopfloor_mobile/static/wms/src/screen.js
@@ -36,7 +36,7 @@ Vue.component("nav-item-action").extendOptions.template = `
 
 Vue.component("app-bar-actions").extendOptions.template = `
     <div :class="$options._componentTag">
-        <span> version 1.84.36 </span>
+        <span> version 1.84.37 </span>
         <v-btn icon @click="$router.push({'name': 'scan_anything'})" :disabled="this.$route.name=='scan_anything'">
             <v-icon >mdi-magnify</v-icon>
         </v-btn>
