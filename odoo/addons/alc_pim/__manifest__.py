bd50d1d709108819031091a8a719e072e114bf09
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-04-20 17:02:01 +0200

    [FIX] alc_pim: Add translations for categories
    refs #54986

--- odoo/addons/alc_pim/__manifest__.py
+++ odoo/addons/alc_pim/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Alc PIM",
     "description": """Alcyon PIM""",
-    "version": "10.0.1.0.2",
+    "version": "10.0.1.0.3",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
--- odoo/addons/alc_pim/hooks.py
+++ odoo/addons/alc_pim/hooks.py
@@ -2,13 +2,17 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
 import os
+from collections import defaultdict
 
 import unicodecsv as csv
 
 import odoo
 
 langs_alcyon = {"fr_BE", "nl_BE"}
+
+_logger = logging.getLogger()
 
 
 def pre_init_hook(cr):
@@ -23,18 +27,43 @@ def pre_init_hook(cr):
 
 def post_init_hook(cr, registry=None):
     _load_attribute_options_translations(cr)
+    _load_categories_translations(cr)
 
 
 def _load_attribute_options_translations(cr):
+    _logger.info("Load translations for attributes options")
     env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
     path = os.path.join(os.path.dirname(__file__), "static", "alc_options.csv")
     with open(path) as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=",")
         all_options_records = env["attribute.option"].search([])
-        options_by_en_name = {r.name: r for r in all_options_records}
+        option_ids_by_en_name = defaultdict(list)
+        for r in all_options_records:
+            option_ids_by_en_name[r.name].append(r.id)
         for r in csv_reader:
-            record = options_by_en_name.get(r["en_GB"])
-            if not record:
+            ids = option_ids_by_en_name.get(r["en_GB"])
+            if not ids:
                 continue
+            records = env["attribute.option"].browse(ids)
             for lang in langs_alcyon:
-                record.with_context(lang=lang).write({"name": r[lang]})
+                records.with_context(lang=lang).write({"name": r[lang]})
+
+
+def _load_categories_translations(cr):
+    _logger.info("Load translations for categories")
+    env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
+    path = os.path.join(os.path.dirname(__file__), "static", "categories.csv")
+    with open(path) as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=";")
+        all_categories_records = env["product.category"].search([("is_web", "=", True)])
+        categorie_ids_by_en_name = defaultdict(list)
+        for r in all_categories_records:
+            categorie_ids_by_en_name[r.name].append(r.id)
+        for r in csv_reader:
+            ids = categorie_ids_by_en_name.get(r["fr_BE"])
+            if not ids:
+                _logger.info("%s not found", r["fr_BE"])
+                continue
+            records = env["product.category"].browse(ids)
+            for lang in ["en_US"] + list(langs_alcyon):
+                records.with_context(lang=lang).write({"name": r[lang]})
