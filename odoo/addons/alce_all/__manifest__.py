3e11e72c5711ecba061f35e216784d2862aa3a02
nans <nans.lefebvre@acsone.eu>
2022-05-06 10:50:38 +0200

    Bump version: 1.84.66 → 1.84.67

--- odoo/addons/alce_all/__manifest__.py
+++ odoo/addons/alce_all/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Alc All Enterprise",
     "description": """
         Alcyon addons under Odoo Enterprise licence""",
-    "version": "10.0.1.84.66",
+    "version": "10.0.1.84.67",
     "license": "Other proprietary",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
