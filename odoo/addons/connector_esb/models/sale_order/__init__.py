9e629c83b38f2c5fef703cd10e115fa3fd291b17
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-05-09 16:21:35 +0200

    [IMP] connector_esb: deactivate listeners exporting info to the old ESB

--- odoo/addons/connector_esb/models/sale_order/__init__.py
+++ odoo/addons/connector_esb/models/sale_order/__init__.py
@@ -1,5 +1,4 @@
 from . import adapter
 from . import common
 from . import exporter
-from . import listener
 from . import webservice
--- odoo/addons/connector_esb/models/stock/__init__.py
+++ odoo/addons/connector_esb/models/stock/__init__.py
@@ -1,5 +1,3 @@
 from . import adapter
 from . import common
 from . import exporter
-from . import listener_product_product
-from . import listener_product_template
