b12cb4d5cf5f184954254e1f8a6e9e993da0f2e4
nans <nans.lefebvre@acsone.eu>
2022-05-19 11:55:49 +0200

    [FIX] alc_eshop_services_deliveries: returned pickings containing cancelled move lines

--- odoo/addons/alc_eshop_services_deliveries/__manifest__.py
+++ odoo/addons/alc_eshop_services_deliveries/__manifest__.py
@@ -9,6 +9,11 @@
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["authenticated_partner_mixin", "sale_stock", "account_tax_one_vat"],
+    "depends": [
+        "authenticated_partner_mixin",
+        "sale_stock",
+        "account_tax_one_vat",
+        "stock_groupbypartner",  # customer_id
+    ],
     "demo": [],
 }
