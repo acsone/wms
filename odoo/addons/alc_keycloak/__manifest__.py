ba2f088d61e733554ac62b63678aee5c78473405
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-06-27 16:19:17 +0200

    [IMP] alc_keycloack: Export info required to get net price into payload
    refs #59136

--- odoo/addons/alc_keycloak/__manifest__.py
+++ odoo/addons/alc_keycloak/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Alcyon Keycloak",
     "description": """Alcyon Keycloak""",
-    "version": "10.0.1.0.2",
+    "version": "10.0.1.0.3",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
