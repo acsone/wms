06f2666b73be93beff9ecad67d9f1d0fe5e67e46
nans <nans.lefebvre@acsone.eu>
2022-06-30 21:47:56 +0200

    [ADD] alc_price_cache_exclusive

---
+++ odoo/addons/alc_price_cache_exclusive/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Alcyon Price Cache: Exclusive Discounts",
+    "description": """Alcyon Price Cache: Exclusive Discounts""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["alc_price_cache", "alc_price_triple_discount_exclusive"],
+    "application": False,
+    "data": [],
+    "demo": [],
+    "installable": True,
+}
