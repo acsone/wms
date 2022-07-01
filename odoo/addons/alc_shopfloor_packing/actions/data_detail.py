4de8f0acc894df05299fd930afb0a1c0d273a9f4
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-07-01 15:20:26 +0200

    [IMP] alc_shopfloor_packing: Return only informations required for the put in pack screen
    refs 60151

--- odoo/addons/alc_shopfloor_packing/actions/data_detail.py
+++ odoo/addons/alc_shopfloor_packing/actions/data_detail.py
@@ -11,6 +11,24 @@ class DataDetailAction(Component):
 
     @ensure_model("stock.picking")
     def pack_picking_detail(self, record, **kw):
-        data = self.picking_detail(record, **kw)
-        data["scanned_packs"] = list(record._packing_scanned_packs)
-        return data
+        return {
+            "id": record.id,
+            "name": record.name,
+            "partner": {"id": record.partner_id.id, "name": record.partner_id.name},
+            "scanned_packs": list(record._packing_scanned_packs),
+            "operations": [
+                self._pack_picking_operation_detail(op)
+                for op in record.pack_operation_ids
+            ],
+        }
+
+    def _pack_picking_operation_detail(self, record):
+        return {
+            "id": record.id,
+            "qty_done": record.qty_done,
+            "product": self.product(
+                record.product_id or record.package_id.single_product_id
+            ),
+            "package_src": self.package(record.package_id, record.picking_id),
+            "package_dest": self.package(record.result_package_id, record.picking_id),
+        }
