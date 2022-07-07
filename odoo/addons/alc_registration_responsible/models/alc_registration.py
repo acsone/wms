c7a022dd083318fed1fea8c081b2c7bd91d07ff5
nans <nans.lefebvre@acsone.eu>
2022-07-07 16:19:31 +0200

    [IMP] alc_registration_responsible

---
+++ odoo/addons/alc_registration_responsible/models/alc_registration.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class AlcRegistration(models.Model):
+
+    _name = "alc.registration"
+    _inherit = ["mixin.user_id", "alc.registration"]
+
+    def _get_default_user_id(self):
+        res = super(AlcRegistration, self)._get_default_user_id()
+        user = self.env.ref("__setup__.res_user_jmercy", raise_if_not_found=False)
+        return user or res
+
+    @api.model
+    def create(self, vals):
+        res = super(AlcRegistration, self).create(vals)
+        template_xmlid = "alc_registration_responsible.create_mail_template"
+        res.notify_responsible(template_xmlid)
+        return res
---
+++ odoo/addons/alc_registration_responsible/tests/test_registrations.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.addons.alc_registration.tests.common import TestRegistration
+
+
+class TestRegistrationFlow(TestRegistration):
+    def test_creation_flow(self):
+        vals = self._get_registration_vals()
+
+        registration = self.model.create(vals)
+
+        domain_message = [
+            ("partner_ids", "in", registration.user_id.partner_id.ids),
+            ("model", "=", "alc.registration"),
+        ]
+        message = self.env["mail.message"].search(domain_message)
+        self.assertTrue(registration.name in message.subject)
