32cceec3acb51294cf1d86f5d344efcdad959f5e
nans <nans.lefebvre@acsone.eu>
2022-07-22 16:48:37 +0200

    [IMP] alc_pricing: test edge case (standard_price percentage discount)

--- odoo/addons/alc_pricing/tests/test_pricing.py
+++ odoo/addons/alc_pricing/tests/test_pricing.py
@@ -85,3 +85,25 @@ class TestPricingFlow(TestPricing):
         self.assertEqual(line_2.discount2, 20)
         self.assertEqual(line_2.discount3, 50)
         self.assertEqual(line_2.discount_item_id, self.item_2_2)
+
+    @freeze_time("2022-01-01 12:00:00")
+    @mute_logger("odoo.addons.queue_job.models.base")
+    def test_discount_based_on_cost(self):
+        """For a percentage discount item, basing on standard_price has no effect."""
+        # given: percentage item based on standard_price (cost), differing from price
+        self.product_3.standard_price = 5
+        vals = self._get_item_vals(
+            pricelist=self.customer_1.discount_pricelist_ids[0],
+            applied_on="0_product_variant",
+            product_id=self.product_3.id,
+            compute_price="percentage",
+            base="standard_price",
+            percent_price=50,
+        )
+        self.model_pl_item_nodelay.create(vals)
+
+        # when
+        line_1 = self._new_sale_line(self.so_1, self.product_3)
+
+        # then: the base does not really matter, only the percentage is taken into account
+        self.assertEqual(line_1.discount3, 50)
