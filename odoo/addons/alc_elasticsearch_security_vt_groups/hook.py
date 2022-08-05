326e21f15118f4b9220b436846349763c2653c74
nans <nans.lefebvre@acsone.eu>
2022-08-05 16:16:28 +0200

    [ADD] alc_elasticsearch_security_vt_groups

---
+++ odoo/addons/alc_elasticsearch_security_vt_groups/hook.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import SUPERUSER_ID, api
+
+
+def post_init_hook(cr, registry=None):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    vt_groups = env["veterinary.group"].search([])
+    for vt_group in vt_groups:
+        vt_group.delay_create_or_update_linked_role()
