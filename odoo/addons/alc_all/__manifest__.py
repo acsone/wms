d047c9406ddc2a880ef6c9de71b4b06e9058f4ca
nans <nans.lefebvre@acsone.eu>
2022-09-27 11:23:37 +0200

    IMP] alc_all: depend on alc_internal_package_groupbypartner

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -119,7 +119,7 @@
         "alc_geo_delivery_rounds",
         "alc_gls_putinpack",
         "alc_invoices_audit",
-        "alc_internal_stock_quant_package",
+        "alc_internal_package_groupbypartner",
         "alc_label_printing_gls",
         "alc_label_printing_reception",
         "alc_logiweb",
