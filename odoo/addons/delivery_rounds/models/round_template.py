cacbd250499d97ea7251cf2a505ec494e1695bc3
nans <nans.lefebvre@acsone.eu>
2022-10-13 14:53:02 +0200

    [IMP] delivery_rounds: add active field on template versions

--- odoo/addons/delivery_rounds/models/round_template.py
+++ odoo/addons/delivery_rounds/models/round_template.py
@@ -13,6 +13,7 @@ re_name = re.compile(r"(\w+) ?- ?(\w+)( .*)?")
 class RoundTemplateVersion(models.Model):
     _name = "round.template.version"
 
+    active = fields.Boolean(default=True)
     name = fields.Char("Name", required=True)
     template_ids = fields.Many2many("round.template", string="Templates")
     is_default_version = fields.Boolean("Default version")
