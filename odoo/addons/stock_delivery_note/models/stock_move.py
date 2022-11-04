9ca4125b8697f67d449bcc51d701ebc69ffe37a4
nans <nans.lefebvre@acsone.eu>
2022-11-04 14:52:14 +0100

    [REF] stock_delivery_note: extract suite logic to a field

--- odoo/addons/stock_delivery_note/models/stock_move.py
+++ odoo/addons/stock_delivery_note/models/stock_move.py
@@ -2,6 +2,8 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+from datetime import datetime
+
 from odoo import api, fields, models
 
 
@@ -17,6 +19,8 @@ class StockMove(models.Model):
     )
 
     order_id = fields.Many2one("sale.order", related="order_line_id.order_id")
+
+    suite_name = fields.Char(compute="_compute_suite_name")
 
     @api.multi
     def get_lots(self, only_with_lot=True):
@@ -46,3 +50,30 @@ class StockMove(models.Model):
 
         # Sort lot by name
         return sorted(result, key=lambda lot: lot[0])
+
+    def _compute_suite_name(self):
+        for move in self:
+            move.suite_name = move._get_suite_name()
+
+    def _get_suite_name(self, sale_order=None, delivery_date=None):
+        """ Compute last column of the delivery note.
+
+        Don't know what it is called but it is also found on the
+        deliverslip report.
+        """
+        self.ensure_one()
+        if not sale_order:
+            sale_order = self.order_line_id.order_id
+        if not delivery_date:
+            delivery_date = self.picking_id.date_done or self.picking_id.date
+        customer = sale_order.partner_id
+        depot_number = customer.vet_depot_number or customer.parent_id.vet_depot_number
+        if not depot_number:
+            return sale_order.client_order_ref or ""
+        return "/".join(
+            [
+                datetime.strptime(delivery_date, "%Y-%m-%d %H:%M:%S").strftime("%y"),
+                depot_number,
+                sale_order.suite_name or "0000",
+            ]
+        )
