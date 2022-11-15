d661309ae13026a5a71dcf5935b4fab4d7a3a642
Lindsay <lindsay.marion@acsone.eu>
2022-11-15 16:28:24 +0100

    [IMP] Add picking info on all screens

---
+++ odoo/addons/alc_shopfloor_mobile_cluster_picking_informations/static/src/js/components/picking-detail.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2022 ACSONE SA/NV
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+ */
+
+Vue.component("picking-detail", {
+  props: ["record"],
+  methods: {
+    detail_fields() {
+      return [{path: "name", renderer: this.render_infos}];
+    },
+    render_infos(record, field) {
+      return [
+        record["name"] + " -",
+        " Op: " + record["operation_count"] + ", ",
+        "Weight: " + Math.round(record["weight"] * 100) / 100 + " kg",
+      ].join(" ");
+    },
+  },
+  template: `
+    <div class="detail with-bottom-actions" v-if="!_.isEmpty(record)">
+      <div class="review">
+      <item-detail-card :card_color="utils.colors.color_for('screen_step_todo')"
+      :record="record" :options="{no_title:true, fields: detail_fields()}" />
+      </div>
+    </div>
+  `,
+});
