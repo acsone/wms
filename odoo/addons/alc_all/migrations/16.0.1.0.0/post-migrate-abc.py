8eff2db4fd3a88131c07dc592ac211fea829b23f
Denis Roussel <denis.roussel@acsone.eu>
2022-11-16 15:52:40 +0100

    [ADD] Migration script for alc_product_abc_classification
    refs #62638

---
+++ odoo/addons/alc_all/migrations/16.0.1.0.0/post-migrate-abc.py
@@ -0,0 +1,18 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+def _rename_abc(env):
+    modules = [
+        (
+            "alc_product_abc_classification",
+            "stock_storage_type_putaway_abc_classification",
+        )
+    ]
+    openupgrade.update_module_names(env.cr, modules, merge_modules=True)
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _rename_abc(env)
--- odoo/addons/alc_product_abc_classification/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import abc_classification_level
-from . import product_product
-from . import product_template
