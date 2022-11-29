344ed63247f03910eb6298e1b1b6992be7f6a8ee
Denis Roussel <denis.roussel@acsone.eu>
2022-11-29 09:01:11 +0100

    [MIG] product_packaging_type => product_packaging_level
    refs #63028

--- odoo/addons/alc_putaway_app/__manifest__.py
+++ odoo/addons/alc_putaway_app/__manifest__.py
@@ -15,6 +15,7 @@
         "alc_stock_scrap_responsible",
         "product_abc_classification_sale_stock",
         "product_dimension",
+        "product_packaging_level",
         "stock_dynamic_routing",
         "stock_location_product_restriction",
         "stock_move_auto_assign",
