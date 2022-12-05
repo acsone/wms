5b7bb1bc4139f80bc31bd31f9ce145c1cdf522e2
Denis Roussel <denis.roussel@acsone.eu>
2022-12-05 14:35:49 +0100

    [MIG] alc_product_packaging
    refs #62657

---
+++ odoo/addons/alc_product_template_packaging_search/README.rst
@@ -0,0 +1,6 @@
+=====================================
+Alc Product Template Packaging Search
+=====================================
+
+Allows to search on computed field 'packaging_ids' on product template level
+
---
+++ odoo/addons/alc_product_template_packaging_search/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Product Template Packaging Search",
+    "description": """
+        Allows to search on computed field 'packaging_ids' on product template level""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": ["product_template_has_one_variant"],
+}
