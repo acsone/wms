852f0bd5f11a45a877c3e964519ff8b9759bda83
Laurent Mignon <laurent.mignon@acsone.eu>
2022-12-05 16:17:55 +0000

    Revert "Merge branch '62685-MIG-stock_delivery_note-hda' into 'master'"
    This reverts merge request !1036

--- odoo/addons/alc_order_picking_app/__manifest__.py
+++ odoo/addons/alc_order_picking_app/__manifest__.py
@@ -11,7 +11,6 @@
     "website": "https://acsone.eu/",
     "depends": [
         "product_total_weight_from_packaging",
-        "stock_delivery_note",
         "stock_dynamic_routing",
         "stock_move_common_dest",
         "stock_picking_completion_info",
