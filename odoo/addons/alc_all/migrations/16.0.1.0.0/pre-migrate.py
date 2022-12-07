1eeb12648b576c0904a73c912f6bc6de9a9df913
Denis Roussel <denis.roussel@acsone.eu>
2022-12-07 10:31:51 +0100

    [REM] alc_stock_storage_type_fixed_location
    refs #62762

--- odoo/addons/alc_all/migrations/16.0.1.0.0/pre-migrate.py
+++ odoo/addons/alc_all/migrations/16.0.1.0.0/pre-migrate.py
@@ -12,6 +12,7 @@ def migrate(cr, version):
     addons_to_uninstall = [
         "alc_stock_location_content_relocation",
         "alc_stock_picking_policy_block",
+        "alc_stock_storage_type_fixed_location",
         "stock_inventory_controller",
         "stock_inventory_products",
         "stock_location",
