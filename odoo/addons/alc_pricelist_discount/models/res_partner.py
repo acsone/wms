4f36d071fd1de898e4a395adecb5a170304e25c6
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-01-25 11:12:11 +0100

    [FIX] alc_pricelist_discount: Temp fix on field definition due to a bug with type annotation
    the comodel_name should comes from the initial declaration of the field. Unfortunately it seems to not be the case

--- odoo/addons/alc_pricelist_discount/models/res_partner.py
+++ odoo/addons/alc_pricelist_discount/models/res_partner.py
@@ -6,11 +6,17 @@ from odoo.exceptions import ValidationError
 
 from odoo.addons.base.models.res_partner import Partner
 
+from .product_pricelist import ProductPricelist
+
 
 class ResPartner(Partner):
 
-    discount_pricelist_ids = fields.Many2many(domain=[("is_discount", "=", True)])
-    property_product_pricelist = fields.Many2one(domain=[("is_discount", "=", False)])
+    discount_pricelist_ids = fields.Many2many[ProductPricelist](
+        comodel_name="product.pricelist", domain=[("is_discount", "=", True)]
+    )
+    property_product_pricelist = fields.Many2one[ProductPricelist](
+        comodel_name="product.pricelist", domain=[("is_discount", "=", False)]
+    )
 
     @api.constrains("property_product_pricelist", "discount_pricelist_ids")
     def _constrain_discount_pricelists(self):
