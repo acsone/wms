8d1a51bdc0ad77ac5cb3f3b873fdf730c47b5d6e
hda <hughes.damry@acsone.eu>
2023-01-30 14:30:59 +0100

    [MIG] alc_partner_veterinary refs #62623

--- odoo/addons/alc_partner_veterinary/__manifest__.py
+++ odoo/addons/alc_partner_veterinary/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Alcyon Veterinary Partner",
     "description": """Alcyon Veterinary Partner""",
-    "version": "10.0.3.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
@@ -13,6 +12,5 @@
     "application": False,
     "data": ["views/res_partner.xml"],
     "demo": [],
-    "pre_init_hook": "pre_init_hook",
-    'installable': False
+    "installable": True,
 }
