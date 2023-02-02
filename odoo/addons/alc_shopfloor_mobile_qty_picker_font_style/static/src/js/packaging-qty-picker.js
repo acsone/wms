1674f279f7c012d1d4715e669ce95e6e8cbe97e5
Lindsay <lindsay.marion@acsone.eu>
2023-02-02 14:46:49 +0100

    [FIX] Highlight the quantity even if it's just 1 piece to take + add background color if unit of measure is not of type "unit"

--- odoo/addons/alc_shopfloor_mobile_qty_picker_font_style/static/src/js/packaging-qty-picker.js
+++ odoo/addons/alc_shopfloor_mobile_qty_picker_font_style/static/src/js/packaging-qty-picker.js
@@ -9,10 +9,21 @@ const qty_color_computed = computed.qty_color;
 
 computed.qty_color = function() {
   const result = qty_color_computed.bind(this)();
-  const font_style =
-    this.qty_todo > 1
-      ? "font-weight:950; font-size: 200%; color:rgb(102, 0, 0)"
-      : "font-weight:normal ; font-size: 100%; color:black";
+  const font_style = "font-weight:950; font-size: 200%; color:rgb(102, 0, 0)";
   const new_result = result + ";" + font_style;
+  return new_result;
+};
+
+const unit_color_computed = computed.unit_color;
+
+computed.unit_color = function() {
+  const result = unit_color_computed.bind(this)();
+  const font_style = "font-weight:950; font-size: 200%; color:rgb(102, 0, 0)";
+  const is_not_unit = this.opts.uom.id != 1; // id of unit of measure "unit" is 1.
+  const background_style = is_not_unit
+    ? "background-color: orangered!important"
+    : "background-color:transparent; ";
+
+  const new_result = result + ";" + font_style + ";" + background_style;
   return new_result;
 };
