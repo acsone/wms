1862f23424997e0c912cd551842ac2e5f4c0f878
Denis Roussel <denis.roussel@acsone.eu>
2023-02-16 10:47:11 +0100

    [REM] specific_stock: Remove method in procurement as used in dynamic_routing
    refs #66270

--- odoo/addons/specific_stock/models/procurement_order.py
+++ odoo/addons/specific_stock/models/procurement_order.py
@@ -20,26 +20,3 @@ class ProcurementOrder(models.Model):
         res = super(ProcurementOrder, self)._get_stock_move_values()
         res["restrict_lot_id"] = self.restrict_lot_id.id
         return res
-
-    @api.multi
-    def _search_suitable_rule(self, domain):
-        """
-        The MTO route contains several rules (one for each picking zone).
-        If the product uses the MTO route, we need to force the picking
-        zone.
-        :param domain: list - Odoo Domain (eg: [('location_id', 'in', [1, 2])])
-        :return:
-        """
-        self.ensure_one()
-
-        location_output = self.env.ref("stock.stock_location_output")
-        if self.location_id == location_output:
-            product_picking_zone = self.product_id.picking_zone_id
-            domain = expression.AND(
-                [
-                    [("picking_type_id.picking_zone_id", "=", product_picking_zone.id)],
-                    domain,
-                ]
-            )
-
-        return super(ProcurementOrder, self)._search_suitable_rule(domain)
