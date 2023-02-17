5e6b0f3d64557bdd34c84d4b682262b010adf3a4
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-02-17 14:40:01 +0100

    [FIX] alc_supplier_promotion: Add module to force the use of editform for product supplierinfo

---
+++ odoo/addons/alc_product_supplierinfo_force_edit_form/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Product Supplierinfo Force Edit Form",
+    "description": """
+        Alcyon: Force the use of edit form to add or modify a supplierinfo line into the product view""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["purchase"],
+    "data": ["views/product_supplierinfo.xml"],
+    "demo": [],
+}
