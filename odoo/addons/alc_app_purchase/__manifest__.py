3b1a5b4d1c32a0ed42d2789dd28b7f14e9fa28d4
hda <hughes.damry@acsone.eu>
2023-02-22 12:49:15 +0100

    [ADD] alc_supplier_purchase_manager_account refs #62768

--- odoo/addons/alc_app_purchase/__manifest__.py
+++ odoo/addons/alc_app_purchase/__manifest__.py
@@ -16,7 +16,9 @@
         "alc_purchase_order_cleaner",
         "alc_stock_move_list",
         "alc_supplier_purchase_manager",
+        "alc_supplier_purchase_manager_account",
         "alc_product_supplierinfo_force_edit_form",
         "alc_product_state",
+        "partner_manual_rank",
     ],
 }
