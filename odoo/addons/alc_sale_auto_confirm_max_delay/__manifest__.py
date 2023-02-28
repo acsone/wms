c1b1406594569f2d03fe9e97442b73fe7bdf2ddf
sbejaoui <souheil.bejaoui@acsone.eu>
2023-02-28 16:07:15 +0100

    [16.0][MIG] - alc_sale_auto_confirm_max_delay
    refs: #62828

--- odoo/addons/alc_sale_auto_confirm_max_delay/__manifest__.py
+++ odoo/addons/alc_sale_auto_confirm_max_delay/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,11 +7,11 @@
          Adds a maximum sale delay on customer. It is use to know if a sale
          order is taking too long to be processed by the jobs.
          """,
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Camptocamp SA",
+    "author": "Camptocamp SA,ACSONE SA/NV",
     "website": "https://www.camptocamp.com",
     "depends": ["sale"],
     "data": ["views/res_partner.xml"],
-    'installable': False
+    "pre_init_hook": "pre_init_hook",
 }
