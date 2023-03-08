0b188d3c7e8fd28397840a901f58bcfea54f735b
sbejaoui <souheil.bejaoui@acsone.eu>
2023-03-08 13:10:43 +0100

    [ADD] - add stock_release_channel_geoengine

---
+++ stock_release_channel_geoengine/models/stock_release_channel.py
@@ -0,0 +1,16 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+import logging
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class StockReleaseChannel(models.Model):
+
+    _inherit = "stock.release.channel"
+
+    restrict_to_delivery_zone = fields.Boolean()
+    delivery_zone = fields.GeoMultiPolygon()
