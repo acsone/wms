7216702c19b164d79d469673736e3ad08085dcc1
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-03-08 17:38:16 +0100

    [IMP] alc_eshop_auth_api_key: Display header in doc when appropriated

--- odoo/addons/alc_eshop_auth_api_key/services/service.py
+++ odoo/addons/alc_eshop_auth_api_key/services/service.py
@@ -4,22 +4,34 @@
 from odoo.addons.component.core import AbstractComponent
 
 
+class BaseRestService(AbstractComponent):
+    _inherit = "base.rest.service"
+
+    def _get_openapi_default_parameters(self):
+        defaults = super(BaseRestService, self)._get_openapi_default_parameters()
+        if self._collection in ("shopinvader.backend", "shopinvader.api.v2"):
+            defaults.append(
+                {
+                    "name": "PARTNER-IDENTITY",
+                    "description": "Partner identity: The partner identity is "
+                    "the concatenation of the partner reference and the partner "
+                    "login into the alcyon website separated by a pipe character. "
+                    "Example: 123456789|john.doe. This field is required when "
+                    "authenticated by auth_api_key.",
+                    "required": False,
+                    "schema": {"type": "string"},
+                    "style": "simple",
+                    "in": "header",
+                }
+            )
+        return defaults
+
+
 class BaseShopinvaderService(AbstractComponent):
     _inherit = "base.shopinvader.service"
 
     def _get_openapi_default_parameters(self):
         defaults = super(BaseShopinvaderService, self)._get_openapi_default_parameters()
-        for default in defaults:
-            if default["name"] == "PARTNER-EMAIL":
-                default.update(
-                    {
-                        "name": "PARTNER-IDENTITY",
-                        "description": "Partner identity: The partner identity is "
-                        "the concatenation of the partner reference and the partner "
-                        "login into the alcyon website separated by a pipe character. "
-                        "Example: 123456789|john.doe. This field is required when "
-                        "authenticated by auth_api_key.",
-                        "required": True,
-                    }
-                )
+        if self._collection in ("shopinvader.backend", "shopinvader.api.v2"):
+            defaults = [d for d in defaults if d["name"] != "PARTNER-EMAIL"]
         return defaults
