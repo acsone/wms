b43b5ca3b33910883642a40d72016aed157a1023
Pierre Halleux <pierre.halleux@acsone.eu>
2023-03-14 10:48:48 +0100

    [MIG] alc_older_stock_production_lot: Migration to 16.0

--- odoo/addons/alc_older_stock_production_lot/__manifest__.py
+++ odoo/addons/alc_older_stock_production_lot/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,12 +5,12 @@
     "name": "Alc Older Stock Production Lot",
     "description": """
         Alcyon: Managed older production lot available for a product""",
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["product_expiry", "specific_base"],
+    "depends": ["product_expiry", "alc_stock_location_data"],
     "data": [],
     "demo": [],
-    'installable': False
+    "installable": True,
 }
