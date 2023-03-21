50ace8196f3e0cd3f7e7ec31ef7fd42917c4b4ed
hda <hughes.damry@acsone.eu>
2023-03-21 14:12:00 +0100

    [MIG] alc_restocking_exclude_not_salable refs #62680

--- odoo/addons/alc_restocking_exclude_not_salable/wizards/stock_return_picking.py
+++ odoo/addons/alc_restocking_exclude_not_salable/wizards/stock_return_picking.py
@@ -1,35 +1,34 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields
+
+from odoo.addons.stock.wizard.stock_picking_return import ReturnPicking
 
 
-class StockReturnPicking(models.TransientModel):
-
-    _inherit = "stock.return.picking"
+class StockReturnPicking(ReturnPicking):
 
     has_archived_product = fields.Boolean(default=False)
     archived_products_message = fields.Html(readonly=True)
     has_not_salable_product = fields.Boolean(default=False)
 
-    @api.model
-    def default_get(self, _fields):
-
-        products_archived = []
-        res = super(StockReturnPicking, self).default_get(_fields)
-        moves = []
-        for move in res.get("product_return_moves", []):
-            product = self.env["product.product"].browse(move[2].get("product_id"))
-            move[2]["not_salable_product"] = not product.sale_ok
-            res["has_not_salable_product"] = True
+    @api.onchange("picking_id")
+    def _onchange_picking_id(self):
+        # override odoo
+        res = super()._onchange_picking_id()
+        inactive_product_moves = self.env["stock.return.picking.line"]
+        for move in self.product_return_moves:
+            product = move.product_id
+            move.not_salable_product = not product.sale_ok
+            self.has_not_salable_product = (
+                self.has_not_salable_product or move.not_salable_product
+            )
             if not product.active:
-                res["has_archived_product"] = True
-                products_archived.append(product)
-            else:
-                moves.append(move)
-        res["product_return_moves"] = moves
-        res["archived_products_message"] = self.env.ref(
-            "alc_restocking_exclude_not_salable.archived_products_message"
-        ).render({"products_archived": products_archived})
+                self.has_archived_product = True
+                inactive_product_moves |= move
+        self.product_return_moves -= inactive_product_moves
+        self.archived_products_message = self.env["ir.ui.view"]._render_template(
+            "alc_restocking_exclude_not_salable.archived_products_message",
+            {"inactive_product_moves": inactive_product_moves},
+        )
         return res
