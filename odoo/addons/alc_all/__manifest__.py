b639ae0aa778e991bf5e06276d73da3d27cb45ef
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-03-22 16:03:00 +0100

    [ADD] stock_location_barcode_search
    New addon select a location from its barcode into a one2many field
    
    refs #66849

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -167,6 +167,7 @@
         "alc_stock_available_immediately",
         "alc_stock_available_immediately_lot_loss",
         "alc_stock_grn_time_delay",
+        "alc_stock_location_barcode_search",
         "alc_stock_move_list",
         "alc_stock_picking_batch_delivery_rounds",
         "alc_stock_picking_no_pack_in_pack",
