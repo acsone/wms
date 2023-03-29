b1d605651d0bb3cca0ce6a880ffcfab6c3cd841b
Pierre Halleux <pierre.halleux@acsone.eu>
2023-03-29 13:39:34 +0200

    [MIG] alc_weighing_widget: Migration to 16.0

--- odoo/addons/alc_weighing_widget/__manifest__.py
+++ odoo/addons/alc_weighing_widget/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,13 +5,19 @@
     "name": "Alc Weighing Widget",
     "description": """
         Alcyon: Add widget to get weight from pywebdriver server with the mettler_toledo_driver""",
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["web", "alc_pywebdriver"],
-    "data": ["views/web_weighing_widget.xml"],
-    "qweb": ["static/src/xml/*.xml"],
+    "depends": ["web_notify", "alc_pywebdriver"],
+    "data": [],
     "demo": [],
-    'installable': False
+    "assets": {
+        "web.assets_backend": [
+            "alc_weighing_widget/static/src/components/web_scale/web_scale.js",
+            "alc_weighing_widget/static/src/components/web_scale/web_scale.scss",
+            "alc_weighing_widget/static/src/components/web_scale/web_scale.xml",
+        ]
+    },
+    "installable": True,
 }
