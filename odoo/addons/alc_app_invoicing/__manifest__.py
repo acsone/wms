e4d04773b0c94e7eea07ce58b3aa44b54b1af4b0
sbejaoui <souheil.bejaoui@acsone.eu>
2023-04-12 10:47:57 +0200

    [MIG] - base_unece
    refs: #62990

--- odoo/addons/alc_app_invoicing/__manifest__.py
+++ odoo/addons/alc_app_invoicing/__manifest__.py
@@ -22,6 +22,8 @@
         "account_invoice_tax_required",
         # OCA/reporting-engine
         "report_xml",
+        # OCA/community-data-files
+        "base_unece",
         # OCA/edi
         "base_ubl",
         # ALC
