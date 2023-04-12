8251eb0eec17dd141854b7a50325db41af4be81e
Denis Roussel <denis.roussel@acsone.eu>
2023-04-12 09:09:02 +0200

    [IMP] alc_product_is_new: Remove unneeded dependency

--- odoo/addons/alc_product_is_new/__manifest__.py
+++ odoo/addons/alc_product_is_new/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
-    "depends": ["product", "stock_storage_type"],
+    "depends": ["product", "stock"],
     "data": [
         "views/product_template_views.xml",
         "views/stock_package_type_views.xml",
