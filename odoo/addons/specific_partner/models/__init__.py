c89759c48e445d8676ee857341da6242fcfa0aa1
Pierre Halleux <pierre.halleux@acsone.eu>
2023-04-26 11:56:47 +0200

    [REM] specific_partner

--- odoo/addons/specific_partner/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_partner
-from . import legal_entity
-from . import base_contact
