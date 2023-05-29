bee96ae4fdbff4764e1246ad6749dfcfa1713f7b
sbejaoui <souheil.bejaoui@acsone.eu>
2023-05-29 13:01:35 +0200

    [MIG][REF] - specific_security to alc_sale_security
    refs: #62858

--- odoo/addons/alc_sale_security/__manifest__.py
+++ odoo/addons/alc_sale_security/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu",
-    "depends": ["sale", "stock"],
+    "depends": ["sale", "stock", "delivery"],
     "data": [],
     "demo": [],
 }
