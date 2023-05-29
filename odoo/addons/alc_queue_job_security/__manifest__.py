5bc6b29a04b811406fe138ec029745cca2250213
sbejaoui <souheil.bejaoui@acsone.eu>
2023-05-29 13:58:18 +0200

    [MIG][REF] - specific_security to alc_queue_job_security
    refs: #62858

---
+++ odoo/addons/alc_queue_job_security/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Queue Job Security",
+    "description": """Give read access to all users for queue jobs""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": ["queue_job"],
+    "data": ["security/ir.model.access.csv"],
+    "demo": [],
+}
