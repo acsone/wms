b904797f40d6eb892e980af8f307b803eb6cabc3
sbejaoui <souheil.bejaoui@acsone.eu>
2023-05-29 13:42:14 +0200

    [MIG][REF] - specific_security to alc_account_security
    refs: #62858, #62932

---
+++ odoo/addons/alc_account_security/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Account Security",
+    "description": """Specific ACL for account models""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": [
+        # Odoo
+        "account",
+        # OCA
+        "account_cutoff_base",
+    ],
+    "data": ["security/ir.model.access.csv"],
+    "demo": [],
+}
---
+++ odoo/addons/alc_app_account/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc App Account",
+    "description": """Gather all account related modules for Alcyon""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": [
+        # Odoo
+        "account",
+        # OCA
+        # ALC
+        "alc_account_security",
+    ],
+    "data": [],
+    "demo": [],
+}
