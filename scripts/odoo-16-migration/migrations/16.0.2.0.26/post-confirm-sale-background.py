4ca63fdc79f6da834aff23f25abefe415406ebb9
Denis Roussel <denis.roussel@acsone.eu>
2023-05-30 15:01:53 +0200

    [IMP] Confirm sale orders that are in confirm_background state
    refs #67784

---
+++ scripts/odoo-16-migration/migrations/16.0.2.0.26/post-confirm-sale-background.py
@@ -0,0 +1,33 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+def _confirm_background_sales(env):
+    """
+    Some sales can be in 'confirm_background' state at migration.
+
+    As the module is not used anymore, confirm them (with orm).
+    Before, set them as draft in order to avoid possible filters on state
+    """
+
+    query = """
+        SELECT id FROM sale_order
+            WHERE state = 'confirm_background'
+    """
+    env.cr.execute(env.cr, query)
+    ids = [i[0] for i in env.cr.fetchall()]
+
+    query = """
+        UPDATE sale_order
+            SET state = 'draft'
+            WHERE state = 'confirm_background'
+    """
+    env.cr.execute(env.cr, query)
+
+    env["sale.order"].browse(ids).action_confirm()
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _confirm_background_sales(env)
