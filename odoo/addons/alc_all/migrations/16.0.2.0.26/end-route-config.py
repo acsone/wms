0569eb9eccf942a6286865e82152597a02734736
Denis Roussel <denis.roussel@acsone.eu>
2023-05-30 12:20:05 +0200

    [IMP] Configure delivery route for release channels
    refs #68143

---
+++ odoo/addons/alc_all/migrations/16.0.2.0.26/end-route-config.py
@@ -0,0 +1,17 @@
+from openupgradelib import openupgrade
+
+
+def _configure_route(env):
+    # Set release channel behavior for delivery route
+    route = env["stock.route"].browse(3)
+    route.write(
+        {
+            "available_to_promise_defer_pull": True,
+            "no_backorder_at_release": True,
+        }
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _configure_route(env)
