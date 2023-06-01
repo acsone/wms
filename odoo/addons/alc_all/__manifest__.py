6315a843d1f1885635c15d9726595bebf19f64cc
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-06-01 14:48:02 +0200

    [ADD] peppol

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -231,6 +231,13 @@
         "stock_putaway_route",
         "stock_valuation",
         "web_autorefresh",
+        # pepol
+        "account_invoice_ubl",
+        "account_invoice_export_ubl",
+        "account_invoice_sent",
+        "account_invoice_sent_peppol",
+        "base_ubl_payment_mode_required",
+        "l10n_be_invoice_bba_account_e-invoice_generate",
     ],
     "application": True,
     "data": [
