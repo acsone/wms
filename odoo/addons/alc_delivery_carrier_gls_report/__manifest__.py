2dc936a4427e8d32c2848df17c4410805fb30baa
hda <hughes.damry@acsone.eu>
2023-06-05 12:07:35 +0200

    [MIG] alc_delivery_carrier_gls_report refs #62544

--- odoo/addons/alc_delivery_carrier_gls_report/__manifest__.py
+++ odoo/addons/alc_delivery_carrier_gls_report/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Alc Gls Report Logiweb Customizations",
     "description": """Alcyon: Gls Report Customizations for Logiweb""",
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["alc_delivery_carrier_gls_b2c", "specific_report"],
+    "depends": ["alc_delivery_carrier_gls_b2c"],
     "data": ["reports/report_delivery_report_gls.xml"],
     "demo": [],
-    'installable': False
+    "installable": True,
 }
