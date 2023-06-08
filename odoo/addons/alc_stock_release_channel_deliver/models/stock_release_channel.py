ee7cc635c3150853ded6fc3418bebdb69b78c6e4
sbejaoui <souheil.bejaoui@acsone.eu>
2023-06-08 13:47:04 +0200

    [IMP] - flag shipment advices that are in release channel auto-porcess
    refs: #62637

--- odoo/addons/alc_stock_release_channel_deliver/models/stock_release_channel.py
+++ odoo/addons/alc_stock_release_channel_deliver/models/stock_release_channel.py
@@ -8,6 +8,8 @@ from odoo.addons.stock_release_channel_shipment_advice.models.stock_release_chan
     StockReleaseChannel as StockReleaseChannelBase,
 )
 
+from .shipment_advice import ShipmentAdvice
+
 
 class StockReleaseChannel(StockReleaseChannelBase):
 
@@ -39,6 +41,20 @@ class StockReleaseChannel(StockReleaseChannelBase):
         compute="_compute_is_action_delivered_allowed"
     )
     delivering_error = fields.Text(readonly=True)
+    in_process_shipment_advice_ids = fields.One2many[ShipmentAdvice](
+        compute="_compute_in_process_shipment_advice_ids"
+    )
+
+    @api.depends("shipment_advice_ids", "process_end_date")
+    def _compute_in_process_shipment_advice_ids(self):
+        shipment_advice_model = self.env["shipment.advice"]
+        for rec in self:
+            rec.in_process_shipment_advice_ids = shipment_advice_model.search(
+                [
+                    ("in_release_channel_auto_process", "=", True),
+                    ("release_channel_id", "=", rec.id),
+                ]
+            )
 
     @api.depends("state")
     def _compute_is_action_delivering_allowed(self):
@@ -135,15 +151,6 @@ class StockReleaseChannel(StockReleaseChannelBase):
         if any(not_done_state in shipment_states for not_done_state in not_done_states):
             return
         self.action_delivered()
-
-    @api.depends("state")
-    def _compute_is_action_sleep_allowed(self):
-        res = super()._compute_is_action_sleep_allowed()
-        for rec in self:
-            rec.is_action_sleep_allowed = (
-                rec.is_action_sleep_allowed or rec.state == "delivered"
-            )
-        return res
 
     @api.model
     def _get_delivering_error_message(self, error, related_object):
--- odoo/addons/alc_stock_release_channel_deliver/tests/test_stock_release_channel_deliver.py
+++ odoo/addons/alc_stock_release_channel_deliver/tests/test_stock_release_channel_deliver.py
@@ -59,10 +59,10 @@ class TestStockReleaseChannelDeliver(ChannelReleaseCase):
         self.assertEqual(shipment_advice.state, "done")
         self.assertEqual(shipment_advice.planned_picking_ids, self.pickings)
         self.assertEqual(shipment_advice.loaded_picking_ids, self.pickings)
+        self.assertTrue(shipment_advice.in_release_channel_auto_process)
+        self.assertEqual(self.channel.in_process_shipment_advice_ids, shipment_advice)
         self.assertSetEqual(set(self.pickings.mapped("state")), {"done"})
         self.assertEqual(self.channel.state, "delivered")
-        self.channel.action_sleep()
-        self.assertEqual(self.channel.state, "asleep")
 
     @mute_logger("odoo.addons.alc_stock_release_channel_deliver.models.shipment_advice")
     def test_02(self):
