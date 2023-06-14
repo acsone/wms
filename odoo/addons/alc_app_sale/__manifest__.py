2004bde392bb0b22426a09a6951d8ab49e41ea89
hda <hughes.damry@acsone.eu>
2023-06-14 10:24:42 +0200

    [REM] alc_sale_product_qty_available_to_promise refs #68584

--- odoo/addons/alc_app_sale/__manifest__.py
+++ odoo/addons/alc_app_sale/__manifest__.py
@@ -65,7 +65,6 @@
         "alc_sale_order_line_unavailable_list",
         "alc_sale_order_sort_treeview_by_name",
         "alc_sale_product_qty_backorder",
-        "alc_sale_product_qty_available_to_promise",
         "alc_sale_qty_returned",
         "alc_sale_qty_returned_product_additional",
         "alc_sale_product_qty_unavailable",
