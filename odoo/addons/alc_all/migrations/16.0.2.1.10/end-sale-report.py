5333bca9b4556fe0f790b4ecafc9995bbae70587
Denis Roussel <denis.roussel@acsone.eu>
2023-06-16 11:47:48 +0200

    [IMP] Remove attachment name in sale order report
    refs #68607

---
+++ odoo/addons/alc_all/migrations/16.0.2.1.10/end-sale-report.py
@@ -0,0 +1,15 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+def _unset_attachment(env):
+    """Remove former value (was in specific report)."""
+    # TODO: Remove this after migrating specific_report
+    report = env.ref("sale.report_sale_order")
+    report.attachment = ""
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _unset_attachment(env)
