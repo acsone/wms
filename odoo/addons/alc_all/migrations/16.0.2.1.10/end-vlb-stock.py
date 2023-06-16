c2774c8488cb0c271a442c3d75f83cc619588d29
Denis Roussel <denis.roussel@acsone.eu>
2023-06-16 16:19:09 +0200

    [FIX] Migration scripts

--- odoo/addons/alc_all/migrations/16.0.2.1.10/end-vlb-stock.py
+++ odoo/addons/alc_all/migrations/16.0.2.1.10/end-vlb-stock.py
@@ -7,6 +7,12 @@ def _set_vlb_orderpoint(env):
     """Set VLB as destination of orderpoints."""
 
     vlb = env.ref("alc_stock_location_data.stock_location_vlb")
+    orderpoints_to_delete = (
+        env["stock.warehouse.orderpoint"]
+        .with_context(active_test=False)
+        .search([("location_id", "=", vlb.id), ("active", "=", False)])
+    )
+    orderpoints_to_delete.unlink()
     orderpoints = env["stock.warehouse.orderpoint"].search(
         [("location_id", "!=", vlb.id)]
     )
