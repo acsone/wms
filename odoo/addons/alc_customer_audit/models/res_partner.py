ee383dc0dc6bc8b4ea232e0c8915bb98e91b25d5
hda <hughes.damry@acsone.eu>
2023-06-16 10:43:44 +0200

    [MIG] alc_customer_audit refs #62540

--- odoo/addons/alc_customer_audit/models/res_partner.py
+++ odoo/addons/alc_customer_audit/models/res_partner.py
@@ -1,47 +1,55 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields
+
+from odoo.addons.stock_release_channel_geoengine.models.res_partner import (
+    ResPartner as Partner,
+)
 
 
-class ResPartner(models.Model):
+class ResPartner(Partner):
 
-    _inherit = "res.partner"
-
-    no_delivery_round = fields.Boolean(
-        default=False, compute="_compute_no_delivery_round", store=True, index=True,
+    no_release_channel = fields.Boolean(
+        default=False,
+        compute="_compute_no_release_channel",
+        store=True,
+        index=True,
     )
 
     no_pharmacist = fields.Boolean(
-        default=False, compute="_compute_no_pharmacist", store=True, index=True,
+        default=False,
+        compute="_compute_no_pharmacist",
+        store=True,
+        index=True,
     )
     has_anomaly = fields.Boolean(
         default=False, compute="_compute_has_anomaly", store=True, index=True
     )
 
-    @api.depends("active", "customer", "round_itinerary_ids")
-    def _compute_no_delivery_round(self):
+    @api.depends("active", "is_customer", "stock_release_channel_ids")
+    def _compute_no_release_channel(self):
         alcyon_delivery_id = self.env.ref(
             "__setup__.deliver_carrier_alcyon", raise_if_not_found=False
         )
         for partner in self:
             if (
-                partner.customer
+                partner.is_customer
                 and partner.active
                 and alcyon_delivery_id
                 and partner.property_delivery_carrier_id.id == alcyon_delivery_id.id
-                and not partner.round_itinerary_ids
+                and not partner.stock_release_channel_ids
+                and not partner.located_in_stock_release_channel_ids
             ):
-                partner.no_delivery_round = True
+                partner.no_release_channel = True
             else:
-                partner.no_delivery_round = False
+                partner.no_release_channel = False
 
-    @api.depends("active", "customer", "partner_type", "pharmacist_id")
+    @api.depends("active", "is_customer", "partner_type", "pharmacist_id")
     def _compute_no_pharmacist(self):
         for partner in self:
             if (
-                partner.customer
+                partner.is_customer
                 and partner.active
                 and partner.partner_type == "veterinary"
                 and not partner.pharmacist_id
@@ -50,10 +58,10 @@ class ResPartner(models.Model):
             else:
                 partner.no_pharmacist = False
 
-    @api.depends("no_delivery_round", "no_pharmacist")
+    @api.depends("no_release_channel", "no_pharmacist")
     def _compute_has_anomaly(self):
         for partner in self:
-            if partner.no_delivery_round or partner.no_pharmacist:
+            if partner.no_release_channel or partner.no_pharmacist:
                 partner.has_anomaly = True
             else:
                 partner.has_anomaly = False
--- odoo/addons/alc_customer_audit/tests/test_custom_filter_customer.py
+++ odoo/addons/alc_customer_audit/tests/test_custom_filter_customer.py
@@ -1,32 +1,33 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from shapely.geometry.multipolygon import MultiPolygon
+from shapely.geometry.polygon import Point, Polygon
 
-from odoo.tests.common import SavepointCase
+from odoo.fields import Command
+from odoo.tests.common import TransactionCase
 
 
-class TestCustomFilterCustomer(SavepointCase):
+class TestCustomFilterCustomer(TransactionCase):
     @classmethod
     def setUpClass(cls):
-        super(TestCustomFilterCustomer, cls).setUpClass()
+        super().setUpClass()
         alcyon_delivery_id = cls.env.ref(
             "__setup__.deliver_carrier_alcyon", raise_if_not_found=False
         )
-        delivery_template = cls.env["round.template"].create(
-            {"name": "Unittest delivery template"}
-        )
-        cls.delivery_round_1 = cls.env["round.instance"].create(
-            {"template_id": delivery_template.id, "date": "2017-01-01"}
-        )
 
         if not alcyon_delivery_id:
-
+            carrier_product = cls.env["product.product"].create(
+                {
+                    "name": "Test carrier product",
+                    "type": "service",
+                }
+            )
             delivery_carrier_alcyon = cls.env["delivery.carrier"].create(
                 {
                     "name": "Unittest delivery alcyon",
                     "delivery_type": "fixed",
                     "fixed_price": 10.0,
-                    "delivery_template_id": delivery_template.id,
+                    "product_id": carrier_product.id,
                 }
             )
             cls.env["ir.model.data"].create(
@@ -42,7 +43,7 @@ class TestCustomFilterCustomer(SavepointCase):
             {
                 "name": "Test Customer 1",
                 "active": True,
-                "customer": True,
+                "is_customer": True,
                 "partner_type": "veterinary",
             }
         )
@@ -51,7 +52,7 @@ class TestCustomFilterCustomer(SavepointCase):
             {
                 "name": "Test Customer 2",
                 "active": True,
-                "customer": True,
+                "is_customer": True,
                 "property_delivery_carrier_id": cls.env.ref(
                     "__setup__.deliver_carrier_alcyon", raise_if_not_found=False
                 ).id,
@@ -63,31 +64,50 @@ class TestCustomFilterCustomer(SavepointCase):
                 "ref": "116",
                 "name": "Peter",
                 "street": "Chemin des Oies, 1",
-                "street2": u"A côté de la fontaine",
+                "street2": "A côté de la fontaine",
                 "zip": "1010",
                 "city": "Lausanne",
                 "country_id": cls.env["res.country"]
                 .search([("code", "=", "CH")])[0]
                 .id,
-                "phone": "021123123",
-                "fax": "021121212",
                 "email": "peter@ch.ch",
             }
         )
+        cls.channel = cls.env["stock.release.channel"].create(
+            {
+                "name": "Release Channel",
+            }
+        )
+
+    @staticmethod
+    def _assign_multipolygon(release_channel):
+        polygon = Polygon(
+            [
+                [4.576938, 50.441663],
+                [4.605453, 50.446438],
+                [4.623138, 50.435544],
+                [4.606925, 50.425624],
+                [4.581423, 50.430586],
+            ]
+        )
+        multipolygon = MultiPolygon([polygon])
+        release_channel.delivery_zone = multipolygon
 
     def test_00(self):
         """
         Data:
+
             one customer which is a veterinary with depot allowed
-        Test case:
-            No pharmacist on the customer : this should put the flag "no_pharmacist" to True
-        Expected result:
+            No pharmacist on the customer
             no_pharmacist = True
+        Test case:
+            Set a pharmacist on the customer
+        Expected result:
+            no_pharmacist = False
         """
 
         self.partner1._compute_no_pharmacist()
         self.assertTrue(self.partner1.no_pharmacist)
-
         self.partner1.pharmacist_id = self.pharmacist_1.id
         self.partner1._compute_no_pharmacist()
         self.assertFalse(self.partner1.no_pharmacist)
@@ -95,26 +115,71 @@ class TestCustomFilterCustomer(SavepointCase):
     def test_01(self):
         """
         Data:
+
             one customer which is active
+            No release channel associated to the customer
+            no_release_channel = True
         Test case:
-            No delivery round associated to the customer : no_delivery_round should be true
+            Link a release channel to the customer
         Expected result:
-            no_delivery_round = True
+            no_release_channel = False
         """
+        self.partner2._compute_no_release_channel()
+        self.assertTrue(self.partner2.no_release_channel)
+        self.partner2.stock_release_channel_ids = [Command.set([self.channel.id])]
+        self.partner2._compute_no_release_channel()
+        self.assertFalse(self.partner2.no_release_channel)
 
-        self.partner2._compute_no_delivery_round()
-        self.assertTrue(self.partner2.no_delivery_round)
+    def test_02(self):
+        """
+        Data:
 
-        itinerary = self.env["round.itinerary"].create(
-            {
-                "name": "Itinerary 17C",
-                "code": "T17C",
-                "sequence": 22,
-                "partner_position_ids": [
-                    (0, 0, {"sequence": 10, "partner_id": self.partner2.id})
-                ],
-            }
-        )
-        self.delivery_round_1.itinerary_ids = itinerary
-        self.partner2._compute_no_delivery_round()
-        self.assertFalse(self.partner2.no_delivery_round)
+            one customer which is active
+            One release channel with a delivery zone
+            no_release_channel = True
+        Test case:
+            set the geo_point of customer in delivery zone of the release channel
+        Expected result:
+            no_release_channel = False
+        """
+        self._assign_multipolygon(self.channel)
+        self.partner2._compute_no_release_channel()
+        self.assertTrue(self.partner2.no_release_channel)
+        self.partner2.geo_point = Point([4.602541, 50.435587])
+        self.partner2._compute_no_release_channel()
+        self.assertFalse(self.partner2.no_release_channel)
+
+    def test_03(self):
+        """
+        Data:
+
+            one customer which is active and veterinary
+            One release channel with a delivery zone
+            No pharmacist on the customer
+            no_release_channel = True
+            no_pharmacist = True
+            has_anomaly = True
+        Test case:
+            set the geo_point of customer in delivery zone of the release channel
+            Set a pharmacist on the customer
+        Expected result:
+            no_release_channel = False
+            no_pharmacist = False
+            has_anomaly = False
+        """
+        self.partner2.partner_type = "veterinary"
+        self._assign_multipolygon(self.channel)
+        self.partner1._compute_no_pharmacist()
+        self.partner2._compute_no_release_channel()
+        self.partner2._compute_has_anomaly()
+        self.assertTrue(self.partner2.no_pharmacist)
+        self.assertTrue(self.partner2.no_release_channel)
+        self.assertTrue(self.partner2.has_anomaly)
+        self.partner2.pharmacist_id = self.pharmacist_1.id
+        self.partner2.geo_point = Point([4.602541, 50.435587])
+        self.partner2._compute_no_release_channel()
+        self.partner1._compute_no_pharmacist()
+        self.partner2._compute_has_anomaly()
+        self.assertFalse(self.partner2.no_pharmacist)
+        self.assertFalse(self.partner2.no_release_channel)
+        self.assertFalse(self.partner2.has_anomaly)
