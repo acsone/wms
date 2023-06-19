c8e09467c47af800db9ae5dd957d64af0e342c01
hda <hughes.damry@acsone.eu>
2023-06-19 14:37:03 +0200

    [REM] monitoring_status refs #63014

--- scripts/odoo-16-migration/tasks/tasks.py
+++ scripts/odoo-16-migration/tasks/tasks.py
@@ -556,6 +556,7 @@ def set_modules_to_remove():
             "alc_delivery_rounds_assign_blocking_unavailable_product",  # replaced by alc_stock_release_channel_assign_blocking_unavailable_product
             "alc_sale_product_expected_receipt_date",  # replaced by alc_sale_order_line_forecast_expected_date
             "alc_sale_product_qty_available_to_promise",  # useless
+            "monitoring_status",  # release 2.2
         ]
         query = """
             UPDATE ir_module_module
