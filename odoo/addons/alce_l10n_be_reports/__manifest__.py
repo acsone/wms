ca954ba67f01eb4ccc5df97b3cbc84eae0bb7490
hda <hughes.damry@acsone.eu>
2023-06-26 17:41:45 +0200

    [MIG] odoo/addons/alce_l10n_be_reports: Migration to 16.0

--- odoo/addons/alce_l10n_be_reports/__manifest__.py
+++ odoo/addons/alce_l10n_be_reports/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 
 {
     "name": "Alce L10n Be Reports",
     "description": """
         In the case of Alcyon, VAT reports need to take archived partners into account""",
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "Other proprietary",
     "author": "ACSONE SA/NV",
     "depends": ["l10n_be_reports"],
     "data": [],
     "demo": [],
-    'installable': False
+    'installable': True
 }
--- odoo/addons/alce_l10n_be_reports/models/l10n_be_report_partner_vat_listing.py
+++ odoo/addons/alce_l10n_be_reports/models/l10n_be_report_partner_vat_listing.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 
 from odoo import api, models
