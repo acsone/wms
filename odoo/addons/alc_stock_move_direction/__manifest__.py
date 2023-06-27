82c542fabc4ea38148a0afc39ff16c77ec73220b
sbejaoui <souheil.bejaoui@acsone.eu>
2023-06-27 11:10:43 +0200

    [MIG] - alc_stock_move_direction
    refs: #62737

--- odoo/addons/alc_stock_move_direction/__manifest__.py
+++ odoo/addons/alc_stock_move_direction/__manifest__.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Alc Stock Move Direction",
     "description": """
-        Alcyon: Add method to know if a move is incoming or outgoing""",
-    "version": "10.0.1.0.0",
+        Technical module used to know if a stock move is incoming or outgoing based on
+        its destination location""",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["stock", "stock_helper"],
+    "depends": ["stock"],
     "data": [],
     "demo": [],
-    'installable': False
 }
