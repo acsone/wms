1cc4180938d23f8f63e8a362288211c661b6edaf
hda <hughes.damry@acsone.eu>
2023-06-27 08:37:35 +0200

    Change alc_stock_picking_backorder_reason for license compatibility refs #62780

--- odoo/addons/alc_stock_picking_backorder_reason/README.rst
+++ odoo/addons/alc_stock_picking_backorder_reason/README.rst
@@ -2,22 +2,7 @@
 Alc Stock Picking Backorder Reason
 ==================================
 
-Allows to define specific flows for Alcyon when managing backorder reasons
-
-Configuration
-=============
-
-To configure this module, you need to:
-
-#. Go to ...
-
-Usage
-=====
-
-To use this module, you need to:
-
-#. Go to ...
-
+Defines data for Alcyon when managing backorder reasons
 
 Changelog
 =========
--- odoo/addons/alc_stock_picking_backorder_reason/__manifest__.py
+++ odoo/addons/alc_stock_picking_backorder_reason/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2023 ACSONE SA/NV
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Alc Stock Picking Backorder Reason",
     "description": """
-        Allows to define specific flows for Alcyon when managing backorder reasons""",
+        Defines data for Alcyon when managing backorder reasons""",
     "version": "16.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu",
-    "depends": ["stock_picking_backorder_reason", "stock_picking_backorder_reason_grn"],
+    "depends": ["stock_picking_backorder_reason"],
     "data": [
         "data/stock_backorder_reason.xml",
     ],
