07965a5847f2486d5f3e1270d4436aa315726de8
hda <hughes.damry@acsone.eu>
2023-06-27 08:37:59 +0200

    Add alc_stock_picking_backorder_reason_grn for license compatibility refs #62780

---
+++ odoo/addons/alc_stock_picking_backorder_reason_grn/README.rst
@@ -0,0 +1,9 @@
+==================================
+Alc Stock Picking Backorder Reason
+==================================
+
+Allows to refine data for Alcyon when managing backorder reasons according to
+grn.
+
+Changelog
+=========
---
+++ odoo/addons/alc_stock_picking_backorder_reason_grn/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 ACSONE SA/NV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Alc Stock Picking Backorder Reason",
+    "description": """
+        Allows to refine data for Alcyon when managing backorder reasons
+        according to grn""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": ["stock_picking_backorder_reason", "stock_picking_backorder_reason_grn"],
+    "data": [
+        "data/stock_backorder_reason.xml",
+    ],
+}
