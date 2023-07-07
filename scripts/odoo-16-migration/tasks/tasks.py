f23c3a27a3a3b971cd26fc4e390b5d28928f7aa9
hda <hughes.damry@acsone.eu>
2023-07-07 09:55:25 +0200

    [REM] account_mass_reconncile + partner refs #62948 refs #62949

--- scripts/odoo-16-migration/tasks/tasks.py
+++ scripts/odoo-16-migration/tasks/tasks.py
@@ -587,6 +587,8 @@ def set_modules_to_remove():
         "mass_editing",  # replaced by server_action_mass_edit
         "alce_l10n_be_reports",
         "account_invoice_merge_purchase",  # done by account_invoice_merge
+        "account_mass_reconcile_partner",
+        "account_mass_reconcile",
     ]
     _logger.info("Modules to remove: %s", ",".join(modules_list))
     # with cursor(DB_16_POSTMIG) as cr:
