724b8aa46d31570323be59e9a6237b572401e29f
Denis Roussel <denis.roussel@acsone.eu>
2023-07-14 09:09:54 +0200

    [REM] stock_refill
    refs #62895

---
+++ odoo/addons/alc_all/migrations/16.0.2.2.14/pre-uninstall-refill.py
@@ -0,0 +1,16 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    _logger.info("Uninstall stock_refill ")
+    cr.execute(
+        """
+      UPDATE ir_module_module
+      SET state='to remove'
+      WHERE name = 'stock_refill'
+      """
+    )
--- odoo/addons/stock_refill/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import ir_config_parameter
-from . import stock_location
-from . import product
-from . import stock_picking_type
-from . import stock_picking
