b48bf3695240525ab602482a49f6f2355b9ab94c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-07-18 15:02:04 +0200

    [FIX] alc_stock_picking_rank: Fix order by
    Ensure default value on rank into the table to avoid error with null first
    
    refs #62884

--- odoo/addons/alc_stock_picking_rank/models/stock_picking.py
+++ odoo/addons/alc_stock_picking_rank/models/stock_picking.py
@@ -1,6 +1,8 @@
 # Copyright 2016 BCIM sprl, Camptocamp
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from psycopg2.extensions import AsIs
+
 from odoo import fields
 from odoo.tools import sql
 
@@ -12,7 +14,6 @@ class StockPicking(Picking):
 
     rank = fields.Integer(
         "Rank",
-        default=-1,
         states={"done": [("readonly", True)], "cancel": [("readonly", True)]},
     )
 
@@ -32,6 +33,25 @@ class StockPicking(Picking):
             self._table,
             ["picking_type_id", "priority desc", "rank desc", "date asc", "id desc"],
         )
+
+        # add default value for rank column definition if not exists
+        # check definition of rank column
+        self.env.cr.execute(
+            "SELECT column_default "
+            "FROM information_schema.columns "
+            "WHERE table_name = %s AND column_name = %s",
+            (self._table, "rank"),
+        )
+        res = self.env.cr.fetchone()
+        if res[0] is None:
+            # add default value for rank column to avoid error on sort with null value
+            # set first
+            self.env.cr.execute(
+                "ALTER TABLE %s ALTER COLUMN rank SET DEFAULT 0", (AsIs(self._table),)
+            )
+            self.env.cr.execute(
+                "update %s set rank = 0 where rank is null", (AsIs(self._table),)
+            )
 
     def button_rank_recompute(self):
         pass
