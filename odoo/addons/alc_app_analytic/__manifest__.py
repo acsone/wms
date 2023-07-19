c2c229179949f385993391f9621346bea5f11191
Denis Roussel <denis.roussel@acsone.eu>
2023-07-19 10:31:50 +0200

    [MIG] product_analytic
    refs #63019

---
+++ odoo/addons/alc_app_analytic/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc App Analytic",
+    "description": """
+        Allows to gather all modules for analytic flows""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": [
+        "analytic",
+        "product_analytic",
+    ],
+}
