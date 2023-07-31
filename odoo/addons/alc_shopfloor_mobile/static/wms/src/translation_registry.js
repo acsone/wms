0b48ed37ed8466dc86e92f4dc8c0a04d3e61a360
sbejaoui <souheil.bejaoui@acsone.eu>
2023-07-31 17:30:40 +0200

    [MIG] - shopfloor_mobile
    refs: #62710

--- odoo/addons/alc_shopfloor_mobile/static/wms/src/translation_registry.js
+++
@@ -1,5 +0,0 @@
-import {translation_registry} from "/shopfloor_mobile_base/static/wms/src/services/translation_registry.js";
-
-translation_registry.load("fr-FR", "/alc_shopfloor_mobile/static/wms/src/i18n/fr.json");
-translation_registry.load("en-US", "/alc_shopfloor_mobile/static/wms/src/i18n/en.json");
-translation_registry.set_default_lang("fr-FR");
--- scripts/odoo-16-migration/tasks/tasks.py
+++ scripts/odoo-16-migration/tasks/tasks.py
@@ -597,6 +597,7 @@ def set_modules_to_remove():
         "account_mass_reconcile_partner",
         "account_mass_reconcile",
         "alc_shopfloor",  # replaced by OCA module shopfloor
+        "alc_shopfloor_mobile",  # replaced by shopfloor_mobile
     ]
     _logger.info("Modules to remove: %s", ",".join(modules_list))
     # with cursor(DB_16_POSTMIG) as cr:
