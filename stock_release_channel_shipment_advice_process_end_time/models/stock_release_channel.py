b1a244f071b6f4d858ec501bf78eab87ddd4072a
sbejaoui <souheil.bejaoui@acsone.eu>
2023-08-04 17:24:48 +0200

    [IMP] - improve form view

--- stock_release_channel_shipment_advice_process_end_time/models/stock_release_channel.py
+++ stock_release_channel_shipment_advice_process_end_time/models/stock_release_channel.py
@@ -16,6 +16,11 @@ class StockReleaseChannel(models.Model):
         help="The delay between the release channel process end time and the arrival "
         "of shipments to the dock.",
     )
+    shipment_advice_arrival_time = fields.Float(
+        "Arrival to dock at",
+        compute="_compute_shipment_advice_arrival_time",
+        inverse="_inverse_shipment_advice_arrival_time",
+    )
     shipment_advice_departure_delay = fields.Integer(
         compute="_compute_shipment_advice_departure_delay",
         store=True,
@@ -23,6 +28,37 @@ class StockReleaseChannel(models.Model):
         help="The delay between the release channel process end time and the departure "
         "of shipments to the dock.",
     )
+    shipment_advice_departure_time = fields.Float(
+        "Departure from dock at",
+        compute="_compute_shipment_advice_departure_time",
+        inverse="_inverse_shipment_advice_departure_time",
+    )
+
+    @api.depends("process_end_time", "shipment_advice_departure_delay")
+    def _compute_shipment_advice_departure_time(self):
+        for rec in self:
+            rec.shipment_advice_departure_time = rec.process_end_time + (
+                rec.shipment_advice_departure_delay / 60
+            )
+
+    def _inverse_shipment_advice_departure_time(self):
+        for rec in self:
+            rec.shipment_advice_departure_delay = (
+                rec.shipment_advice_departure_time - rec.process_end_time
+            ) * 60
+
+    @api.depends("process_end_time", "shipment_advice_arrival_delay")
+    def _compute_shipment_advice_arrival_time(self):
+        for rec in self:
+            rec.shipment_advice_arrival_time = rec.process_end_time + (
+                rec.shipment_advice_arrival_delay / 60
+            )
+
+    def _inverse_shipment_advice_arrival_time(self):
+        for rec in self:
+            rec.shipment_advice_arrival_delay = (
+                rec.shipment_advice_arrival_time - rec.process_end_time
+            ) * 60
 
     @api.depends("warehouse_id")
     def _compute_shipment_advice_arrival_delay(self):
