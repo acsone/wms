4ab2c1be97b8ff7aeb87340c9ef976147f2a85a5
sbejaoui <souheil.bejaoui@acsone.eu>
2023-08-13 23:41:36 +0200

    [ADD] - shopfloor_mobile_packing

---
+++ shopfloor_mobile_packing/static/src/js/translation_registry.js
@@ -0,0 +1,10 @@
+import {translation_registry} from "/shopfloor_mobile_base/static/wms/src/services/translation_registry.js";
+
+translation_registry.load(
+    "fr-FR",
+    "/alc_shopfloor_mobile_packing/static/src/js/i18n/fr.json"
+);
+translation_registry.load(
+    "en-US",
+    "/alc_shopfloor_mobile_packing/static/src/js/i18n/en.json"
+);
