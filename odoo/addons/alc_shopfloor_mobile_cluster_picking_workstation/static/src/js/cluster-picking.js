840d29ef0567356283e6623f6938af374c640772
sbejaoui <souheil.bejaoui@acsone.eu>
2023-08-15 18:54:20 +0200

    [MIG] alc_shopfloor_mobile_cluster_picking_workstation
    refs: #62713

--- odoo/addons/alc_shopfloor_mobile_cluster_picking_workstation/static/src/js/cluster-picking.js
+++ odoo/addons/alc_shopfloor_mobile_cluster_picking_workstation/static/src/js/cluster-picking.js
@@ -8,18 +8,18 @@ import {process_registry} from "/shopfloor_mobile_base/static/wms/src/services/p
 const registry_key = "cluster_picking";
 const ClusterPickingBase = process_registry.get(registry_key);
 
-let data_result_method = ClusterPickingBase.component.data;
-let data = function() {
-  // we must bin the original method to this to put it into
+const data_result_method = ClusterPickingBase.component.data;
+const data = function () {
+  // We must bin the original method to this to put it into
   // the object context
-  let result = data_result_method.bind(this)();
-  // add our new state
+  const result = data_result_method.bind(this)();
+  // Add our new state
   result.states.scan_workstation = {
     display_info: {
       title: this.$t("cluster_picking.select_workstation.title"),
       scan_placeholder: this.$t("cluster_picking.select_workstation.scan_placeholder"),
     },
-    on_scan: scanned => {
+    on_scan: (scanned) => {
       let endpoint, endpoint_data;
       endpoint = "scan_workstation";
       endpoint_data = {
