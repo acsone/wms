15bee5fcb8b8399ebccd8cb8b31b49c9a50fbad7
sbejaoui <souheil.bejaoui@acsone.eu>
2023-08-16 13:03:10 +0200

    [MIG] alc_shopfloor_assignation_issue_message
    invluded in base module
    
    refs: #62701

---
+++ odoo/addons/test_shopfloor_assignation_issue_message/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Test Shopfloor Assignation Issue message",
+    "description": """check that it's not allowed to reserve the same quantity""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "depends": ["shopfloor"],
+    "data": [],
+    "demo": [],
+}
