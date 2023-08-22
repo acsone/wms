aac54fc352c143d45c8ba891b11133f258b957b7
Denis Roussel <denis.roussel@acsone.eu>
2023-08-22 18:33:12 +0200

    [ADD] partner_invoicing_mode_fourteen_days
    refs #69710

--- odoo/addons/alc_app_account/__manifest__.py
+++ odoo/addons/alc_app_account/__manifest__.py
@@ -18,6 +18,7 @@
         "mis_builder_budget",
         "partner_invoicing_mode",
         "partner_invoicing_mode_at_shipping",
+        "partner_invoicing_mode_fourteen_days",
         "partner_invoicing_mode_monthly",
         "partner_invoicing_mode_ten_days",
         # ALC
