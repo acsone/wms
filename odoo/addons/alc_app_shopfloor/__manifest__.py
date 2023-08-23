ac978453f1d4deedf07cfb47778a89f60ba23722
sbejaoui <souheil.bejaoui@acsone.eu>
2023-08-23 15:55:12 +0200

    [MIG] alc_shopfloor_mobile_stock_reserve
    refs: #62719

--- odoo/addons/alc_app_shopfloor/__manifest__.py
+++ odoo/addons/alc_app_shopfloor/__manifest__.py
@@ -38,6 +38,7 @@
         "alc_shopfloor_product_info",
         "alc_shopfloor_stock_refill",
         "alc_shopfloor_stock_reserve",
+        "alc_shopfloor_mobile_stock_reserve",
     ],
     "data": [],
     "demo": [],
--- odoo/addons/alc_shopfloor_mobile_stock_reserve/static/src/js/location-content-transfer.js
+++ odoo/addons/alc_shopfloor_mobile_stock_reserve/static/src/js/location-content-transfer.js
@@ -9,21 +9,21 @@ const registry_key = "location_content_transfer";
 const LocationContentTransferBase = process_registry.get(registry_key);
 
 // Keep the pointer to the orginal method
-let actions_method = LocationContentTransferBase.component.methods.line_actions;
+const actions_method = LocationContentTransferBase.component.methods.line_actions;
 
-LocationContentTransferBase.component.methods.line_actions = function() {
-  let line_actions = actions_method.bind(this)();
+LocationContentTransferBase.component.methods.line_actions = function () {
+  const line_actions = actions_method.bind(this)();
   line_actions.push({name: "Declare overstock", event_name: "action_overstock"});
   return line_actions;
 };
 
-LocationContentTransferBase.component.methods.on_action_overstock = function() {
+LocationContentTransferBase.component.methods.on_action_overstock = function () {
   let endpoint, endpoint_data;
   const data = this.state.data;
   endpoint = "overstock_line";
   endpoint_data = {
-    location_id: data.operation.location_src.id,
-    operation_id: data.operation.id,
+    location_id: data.move_line.location_dest.id,
+    move_line_id: data.move_line.id,
   };
 
   this.wait_call(this.odoo.call(endpoint, endpoint_data));
