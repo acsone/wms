7d6d300501365c698cc3517882c3fdacd9aed9a0
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-08-25 17:36:24 +0200

    [DEL] alc_stock_put_remaining_to_reserve
    refs #62751

--- odoo/addons/alc_stock_put_remaining_to_reserve/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import stock_pack_operation
-from . import stock_picking
-from . import stock_move
-from . import stock_picking_type
--- odoo/addons/alc_stock_put_remaining_to_reserve/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_put_remaining_to_reserve
