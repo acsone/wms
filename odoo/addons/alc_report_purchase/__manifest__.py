118c7113778ecaff92c4efb9ab6f7cfa2e489a02
hda <hughes.damry@acsone.eu>
2023-08-30 15:25:54 +0200

    [ADD] alc_report_purchase refs #69411

---
+++ odoo/addons/alc_report_purchase/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Report Purchase",
+    "summary": """
+        Purchase reporting for Alcyon""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": [
+        "purchase",
+        "alc_report_base",
+    ],
+    "data": [
+        "views/report_purchase_order.xml",
+    ],
+    "demo": [],
+    "installable": True,
+}
