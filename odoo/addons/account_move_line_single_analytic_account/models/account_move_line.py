82595e54faa50c38f5ddd1e1cd1b840379e2568e
Denis Roussel <denis.roussel@acsone.eu>
2023-08-31 14:03:51 +0200

    [FIX] account_move_line_single_analytic_account: Don't cast 'false' key values as int()
    refs #69935

--- odoo/addons/account_move_line_single_analytic_account/models/account_move_line.py
+++ odoo/addons/account_move_line_single_analytic_account/models/account_move_line.py
@@ -33,10 +33,9 @@ class AccountMoveLine(MoveLine):
                         distrib=rec.analytic_distribution,
                     )
                 )
-            rec.analytic_account_id = (
-                self.env["account.analytic.account"].browse(
-                    int(next(iter(rec.analytic_distribution.keys())))
-                )
-                if rec.analytic_distribution
-                else False
-            )
+            account_id = False
+            if rec.analytic_distribution:
+                key = next(iter(rec.analytic_distribution.keys()))
+                if isinstance(key, str) and key.isnumeric():
+                    account_id = int(key)
+            rec.analytic_account_id = account_id
