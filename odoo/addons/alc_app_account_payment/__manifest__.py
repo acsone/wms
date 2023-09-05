61896e04357bbba55ed9a00a04117c35e2e6eec4
sbejaoui <souheil.bejaoui@acsone.eu>
2023-09-05 15:05:43 +0200

    [ADD] account_banking_sepa_credit_transfer
    refs: #62924

---
+++ odoo/addons/alc_app_account_payment/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc App Account Payment",
+    "description": """Gather all account payment related modules for Alcyon""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": [
+        # OCA
+        "account_banking_sepa_credit_transfer",
+    ],
+}
