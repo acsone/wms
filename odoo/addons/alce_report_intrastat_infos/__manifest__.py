0885670941f87f998547a1f2aa642f4d82b7ea1c
hda <hughes.damry@acsone.eu>
2023-09-05 11:05:02 +0200

    ADD lgpl module alce_report_intrastat_infos to glue account_intrastat (OE) refs #6940

---
+++ odoo/addons/alce_report_intrastat_infos/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 ACSONE SA/NV
+
+{
+    "name": "Alce Report Intrastat Infos",
+    "summary": """
+        Alcyon: Infos coming from enterprise account_intrastat needed for reporting
+        """,
+    "version": "16.0.1.0.0",
+    "license": "Other proprietary",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": [
+        "account_intrastat",
+    ],
+    "data": [],
+    "demo": [],
+    "installable": True,
+}
