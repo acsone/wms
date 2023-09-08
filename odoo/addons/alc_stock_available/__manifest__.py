9c7bc2a4144d404a7e3ef3feaec7a4e73d1ad568
hda <hughes.damry@acsone.eu>
2023-09-08 15:46:51 +0200

    [FIX] Stock smart buttons filter in product view refs #69430

---
+++ odoo/addons/alc_stock_available/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Stock Available",
+    "summary": """
+        Change the default filter when opening quants from smart stock available
+        buttons.
+        """,
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["stock"],
+    "installable": True,
+}
