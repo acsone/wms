845f0ae6c8e85ebf0ce5cd39cfcd577afbe75462
hda <hughes.damry@acsone.eu>
2023-09-11 16:08:40 +0200

    [REM] account_invoice_merge refs #62941

--- scripts/odoo-16-migration/tasks/tasks.py
+++ scripts/odoo-16-migration/tasks/tasks.py
@@ -615,11 +615,12 @@ def set_modules_to_remove():
         "web_widget_many2many_tags_multi_selection",
         "mass_editing",  # replaced by server_action_mass_edit
         "alce_l10n_be_reports",
-        "account_invoice_merge_purchase",  # done by account_invoice_merge
+        "account_invoice_merge_purchase",
         "account_invoice_force_number",
         "account_group",
         "account_chart_update",
         "account_invoice_merge_attachment",
+        "account_invoice_merge",
         "account_cutoff_accrual_base",
         "account_cutoff_accrual_return",
         "account_type_menu",
