5b6b7f55d7f38d89672f14d352b084318f6bdffe
hda <hughes.damry@acsone.eu>
2023-09-12 20:50:33 +0200

    [REF] alc_product_consolidated_price_newpharma refs #62648

--- odoo/addons/alc_app_sale/__manifest__.py
+++ odoo/addons/alc_app_sale/__manifest__.py
@@ -137,5 +137,6 @@
         "alc_pim",
         "test_alc_shipping_fee_auto_process",
         "alc_product_consolidated_price_report",
+        "alc_product_consolidated_price_newpharma",
     ],
 }
