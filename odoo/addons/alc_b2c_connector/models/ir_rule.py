0b7d636598ba9506988299672f6c1eb14843e196
sbejaoui <souheil.bejaoui@acsone.eu>
2023-09-21 13:18:29 +0200

    [FIX] alc_b2c_connector: fix ir_rule eval context
    refs: #70159

--- odoo/addons/alc_b2c_connector/models/ir_rule.py
+++ odoo/addons/alc_b2c_connector/models/ir_rule.py
@@ -10,8 +10,10 @@ class IrRule(IrRuleBase):
     @api.model
     def _eval_context(self):
         ctx = super()._eval_context()
-        if "alc_b2c_client_id" in self.env.context:
-            ctx["alc_b2c_client_id"] = self.env.context["alc_b2c_client_id"]
+        # If 'alc_b2c_client_id' is not found in the context,
+        # we set it to -1 (an impossible ID) to prevent rules from crashing
+        # due to the missing key.
+        ctx["alc_b2c_client_id"] = self.env.context.get("alc_b2c_client_id", -1)
         return ctx
 
     def _compute_domain_keys(self):
