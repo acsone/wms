158c5905af48f2b648473fa69defc586d41b0a83
sbejaoui <souheil.bejaoui@acsone.eu>
2023-09-28 08:56:47 +0200

    [IMP] shopfloor user
    refs: #70255

---
+++ odoo/addons/alc_shopfloor_user/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Shopfloor User",
+    "description": """
+        Alcyon: Uses portal user as shopfloor user. The portal user comes from the api key""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["shopfloor_mobile_base_auth_api_key", "shopfloor_base"],
+    "data": ["views/auth_api_key.xml"],
+    "demo": [],
+}
