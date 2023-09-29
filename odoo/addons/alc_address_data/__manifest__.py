24a6f582f13f78859267de5551da9ff2f7e4e0d4
hda <hughes.damry@acsone.eu>
2023-09-29 16:08:04 +0200

    [MIG] alc_address_data refs #62527

--- odoo/addons/alc_address_data/__manifest__.py
+++ odoo/addons/alc_address_data/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Alcyon Address Data",
     "description": """Alcyon: Address Data""",
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
     "depends": ["base"],
     "data": ["data/states_belgium.xml"],
     "demo": [],
-    'installable': False
+    "installable": True,
 }
