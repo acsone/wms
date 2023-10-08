d329270d296b138dc8b6e65b153f61e13eeb0d0b
sbejaoui <souheil.bejaoui@acsone.eu>
2023-10-08 19:49:32 +0200

    [MIG] migration script for cutoff_type
    refs: #62932

---
+++ scripts/odoo-16-migration/migrations/16.0.2.4.7/per-account_cutoff_base.py
@@ -0,0 +1,11 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    fields = [("account.cutoff", "account_cutoff", "type", "cutoff_type")]
+    openupgrade.rename_fields(env, fields)
