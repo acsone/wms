b6bd9681946fe1d4f673176fcdf65325cf59922e
Denis Roussel <denis.roussel@acsone.eu>
2023-10-09 15:03:13 +0200

    [MIG] alc_stock_available_immediately_/lot/loss
    refs #62732

--- odoo/addons/alc_stock_available_immediately_loss/__manifest__.py
+++ odoo/addons/alc_stock_available_immediately_loss/__manifest__.py
@@ -1,17 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright 2020 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2020-2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Alc Stock Available Immediately Lot Loss",
     "description": """
         Remove lot loss location from stock immediately_available""",
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["alc_stock_available_immediately", "stock_lot_loss"],
+    "depends": [
+        "alc_stock_available_immediately",
+        "stock_picking_operation_loss_quantity",
+    ],
     "data": ["data/stock_location.xml"],
-    "demo": [],
-    'installable': False
+    "installable": True,
 }
