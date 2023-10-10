bfdca69c33522430fd9209af007d2f43e2b591c4
hda <hughes.damry@acsone.eu>
2023-10-10 10:58:13 +0200

    [MIG] alc_eshop_ordering_allowed refs #62573

--- odoo/addons/alc_eshop_ordering_allowed/models/res_partner.py
+++ odoo/addons/alc_eshop_ordering_allowed/models/res_partner.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import fields
+
+from odoo.addons.base.models.res_partner import Partner
 
 
-class ResPartner(models.Model):
-
-    _inherit = "res.partner"
+class ResPartner(Partner):
 
     eshop_ordering_allowed = fields.Boolean(
         help="If not set, this partner is not allowed to pass an order on the eshop.",
