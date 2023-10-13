81b1737dccb92881a04153eefa67cf3eb80ca586
hda <hughes.damry@acsone.eu>
2023-10-13 14:23:30 +0200

    [FIX] print pack if no pack in picking refs #70583

--- odoo/addons/alc_label_printing_base/models/stock_picking.py
+++ odoo/addons/alc_label_printing_base/models/stock_picking.py
@@ -2,6 +2,7 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, fields
+from odoo.exceptions import UserError
 
 from odoo.addons.alc_printing_base.utils import hw_print
 from odoo.addons.stock.models.stock_picking import Picking
@@ -17,6 +18,8 @@ class StockPicking(Picking):
 
     def print_packages_label(self, quantity=1, printer_id=False, packages=None):
         self.ensure_one()
+        if not packages:
+            raise UserError(_("No package in this picking"))
         if not self.partner_id:
             raise Warning(_("No destination partner defined"))
         hw_print(
