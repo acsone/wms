315a5194e08f15daf63a179b5fc623e75ca0f423
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-10-24 17:42:55 +0200

    [MIG] alc_registration_responsible: Migration from 10.0
    refs #62679

--- odoo/addons/alc_registration_responsible/models/alc_registration.py
+++ odoo/addons/alc_registration_responsible/models/alc_registration.py
@@ -1,17 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api
+
+from odoo.addons.alc_registration.models import alc_registration
+from odoo.addons.mixin_user_id.models.mixin_user_id import MixinUserId
 
 
-class AlcRegistration(models.Model):
+class AlcRegistration(alc_registration.AlcRegistration, MixinUserId):
 
     _name = "alc.registration"
-    _inherit = ["mixin.user_id", "alc.registration"]
 
     def _get_default_user_id(self):
-        res = super(AlcRegistration, self)._get_default_user_id()
+        res = super()._get_default_user_id()
         user = self.env.ref("__setup__.res_user_jmercy", raise_if_not_found=False)
         return user or res
 
--- odoo/addons/alc_registration_responsible/tests/test_registrations.py
+++ odoo/addons/alc_registration_responsible/tests/test_registrations.py
@@ -1,17 +1,21 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo.addons.alc_registration.tests.common import TestRegistration
+from odoo.addons.alc_registration.tests.common import TestRegistrationMixin
 
 
-class TestRegistrationFlow(TestRegistration):
+class TestRegistrationFlow(TestRegistrationMixin):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.model = cls.env["alc.registration"]
+
     def test_creation_flow(self):
         vals = self._get_registration_vals()
 
         registration = self.model.create(vals)
-
         domain_message = [("model", "=", "alc.registration")]
         message = self.env["mail.message"].search(domain_message)
         self.assertTrue(registration.name in message.subject)
