04d4ce8530bbc6b260c06f02464b37cf6f632edd
Denis Roussel <denis.roussel@acsone.eu>
2023-10-26 16:00:18 +0200

    [CHG] Migration script for pickings: Do an unrelease instead
    refs #70565

--- scripts/odoo-16-migration/migrations/16.0.2.4.15/end-migrate-pickings.py
+++ scripts/odoo-16-migration/migrations/16.0.2.4.15/end-migrate-pickings.py
@@ -4,16 +4,14 @@ import logging
 
 from openupgradelib import openupgrade
 
-from odoo.fields import Command
-
 _logger = logging.getLogger(__name__)
 
 
 def _migrate_pickings(env):
     """
-    Delete all pickings that are in 'confirmed' state and:
+    Unrelease all OUT pickings related to internal ones that are in 'confirmed' state and:
 
-    - have a void relese channel
+    - have a void release channel
     - a picking type in : Pick M, Pick aliments, Pick matériel, Pick frigo, Pick inconnu
     """
     picking_type_ids = [16, 15, 18, 24, 3]
@@ -26,18 +24,11 @@ def _migrate_pickings(env):
             ("picking_type_id", "in", picking_type_ids),
         ]
     )
-    pickings.mapped("move_ids").write({"move_dest_ids": [Command.clear()]})
-    if openupgrade.table_exists(env.cr, "stock_pack_operation_deleted"):
-        query = """
-            DELETE FROM stock_pack_operation_deleted
-        """
-        openupgrade.logged_query(env.cr, query)
-
     _logger.info(
-        "Following pickings will be deleted: %(picking_names)s",
+        "Following pickings will be unreleased: %(picking_names)s",
         {"picking_names": ",".join(pickings.mapped("name"))},
     )
-    pickings.unlink()
+    pickings.mapped("move_ids.move_dest_ids.picking_id").unrelease(True)
 
 
 @openupgrade.migrate()
