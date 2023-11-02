a28736a7e7283a0bcd93e23fb8d81541dcf75161
sbejaoui <souheil.bejaoui@acsone.eu>
2023-11-02 12:47:46 +0100

    [MIG] alc_eshop_product_expiry
    refs: #62577

--- odoo/addons/alc_eshop_product_expiry/models/product_product.py
+++ odoo/addons/alc_eshop_product_expiry/models/product_product.py
@@ -1,61 +1,29 @@
-# -*- coding: utf-8 -*-
-# Copyright 2022 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo.addons.shopinvader_search_engine_product_stock.models.product_product import (
+    ProductProduct as ProductProductBase,
+)
 
-from odoo.addons.queue_job.job import job
 
-
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-
-    @api.multi
-    @job(default_channel="root.search_engine.synchronize_stock")
+class ProductProduct(ProductProductBase):
     def synchronize_all_binding_stock_level(self, company_id=None):
-        # Use `sudo` because this action might be triggered
-        # from a low access level user (eg: external user on portal/website).
-        # In any case, the real operation is done w/ the backend user below.
-
-        # ensure user company propagation if the method has been delayed...
-        # this feature should be provided by queue job ...
-        res = super(ProductProduct, self).synchronize_all_binding_stock_level(
-            company_id=company_id
-        )
-        # At the same time we check for the stock, we also check for the expiry date
-        # aka best_before_date
+        res = super().synchronize_all_binding_stock_level(company_id=company_id)
         products = self
         if company_id:
-            products = self.with_context(force_company=company_id.id)
-        all_bindings = products.mapped("shopinvader_bind_ids")
-        backends = all_bindings.mapped("backend_id")
-        # ensure to invalidate cache since the date should be modified by an other move
-        # into the same running process
-        products.invalidate_cache(["best_before_date", "older_lot_id"], products.ids)
-        all_bindings.invalidate_cache(
-            ["best_before_date", "older_lot_id"], all_bindings.ids
-        )
-        for backend in backends:
-            bindings = all_bindings.filtered(lambda r, b=backend: r.backend_id == b)
-            # To avoid access rights issues, execute the job with sudo
-            bindings = bindings.sudo()
-            for binding in bindings:
-                if binding.sync_state == "new":
-                    # this binding have been not yet computed
-                    # so we do not care to update it as it's not yet
-                    # on the site. The right stock qty will be exported
-                    # at it's first export
+            products = self.with_company(company_id)
+        all_bindinds = products.mapped("se_binding_ids")
+        indexes = all_bindinds.mapped("index_id")
+        for index in indexes:
+            for product in products.with_context(index_id=index.id)._filter_by_index():
+                binding = product.sudo().se_binding_ids.filtered(
+                    lambda b, i=index: b.index_id == i
+                )
+                if (
+                    not binding.data
+                    or binding.state == "to_recompute"
+                    or binding.data.get("best_before_date") == product.best_before_date
+                ):
                     continue
-                data = binding.data
-                best_before_date = data.get("best_before_date") or None
-                if best_before_date != binding.best_before_date:
-                    data["best_before_date"] = binding.best_before_date or None
-                    vals = {"data": data}
-                    if binding.backend_id.synchronize_stock == "immediatly":
-                        binding.write(vals)
-                        binding.synchronize()
-                    elif binding.backend_id.synchronize_stock == "in_batch":
-                        if binding.sync_state == "done":
-                            vals["sync_state"] = "to_update"
-                        binding.write(vals)
+                binding.state = "to_recompute"
         return res
--- odoo/addons/alc_eshop_product_expiry/tests/test_product_product.py
+++ odoo/addons/alc_eshop_product_expiry/tests/test_product_product.py
@@ -1,123 +1,78 @@
-# -*- coding: utf-8 -*-
-# Copyright 2022 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-import logging
-from datetime import datetime, timedelta
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields
+from datetime import datetime
 
-from odoo.addons.connector_search_engine.tests.models import SeAdapterFake
-from odoo.addons.shopinvader_product_stock.tests.common import StockCommonCase
+from dateutil.relativedelta import relativedelta
+
+from odoo.addons.shopinvader_search_engine_product_stock.tests.common import (
+    StockCommonCase,
+)
+
+from ..schemas import ProductProduct
 
 
-class TestProductProduct(StockCommonCase):
+class TestUrlInSchemas(StockCommonCase):
     @classmethod
     def setUpClass(cls):
-        super(TestProductProduct, cls).setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, test_queue_job_no_delay=True))
-        cls.production_lot_model = cls.env["stock.production.lot"]
-        cls.inventory_model = cls.env["stock.inventory"]
-        cls.inventory_line_model = cls.env["stock.inventory.line"]
-        cls.stock_location = cls.env.ref("stock.stock_location_stock")
-        cls.customer_location = cls.env.ref("stock.stock_location_customers")
-        cls.loc_physical = cls.env.ref("alc_stock_location_data.stock_location_vlb")
-        cls.stock_location.location_id = cls.loc_physical
-        cls.env["stock.location"].search([])._parent_store_compute()
-        # avoid trouble with gitlab since odoo storage is not preserved with
-        # the cached db when running tests
-        for xml_id in [
-            "shopinvader_image.ir_exp_shopinvader_variant_images",
-            "shopinvader_image.ir_exp_shopinvader_category_images",
-        ]:
-            ir_export = cls.env.ref(xml_id, raise_if_not_found=False)
-            if ir_export:
-                ir_export.unlink()
-
-    def setUp(self):
-        super(TestProductProduct, self).setUp()
-        self.product.tracking = "lot"
-        self.shop_product = self.product.shopinvader_bind_ids
-        self.shop_product.data = {self.shop_product._get_stock_export_key(): {}}
-        self.shop_product.sync_state = "done"
-        self.production_lot = self.production_lot_model.create(
-            {"name": "000001", "product_id": self.product.id}
-        )
-        # mute logger
-        loggers = ["odoo.addons.queue_job.models.base"]
-        for logger in loggers:
-            logging.getLogger(logger).addFilter(self)
-
-        # pylint: disable=unused-variable
-        @self.addCleanup
-        def un_mute_logger():
-            for logger_ in loggers:
-                logging.getLogger(logger_).removeFilter(self)
-
-    def filter(self, record):
-        # required to mute logger
-        return 0
-
-    def _add_product_qty(self, product, production_lot, quantity):
-        self.inventory = self.inventory_model.create(
-            {
-                "name": "Unittest Inventory",
-                "location_id": self.stock_location.id,
-                "filter": "partial",
-            }
-        )
-        self.inventory.prepare_inventory()
-
-        self.inventory_line_model.create(
-            {
-                "inventory_id": self.inventory.id,
-                "product_id": product.id,
-                "location_id": self.stock_location.id,
-                "product_qty": quantity,
-                "prod_lot_id": production_lot.id,
-            }
-        )
-        self.inventory.action_done()
-
-    def test_best_order_date_on_lot_input(self):
-        self.production_lot.life_date = fields.Datetime.to_string(
-            datetime.now() + timedelta(weeks=2)
-        )
-        self.assertFalse(self.shop_product.data.get("best_before_date"), "")
-        with SeAdapterFake.mocked_calls():
-            self._add_product_qty(
-                self.product, production_lot=self.production_lot, quantity=10
+        super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context, test_queue_job_no_delay=True, index_id=cls.index.id
             )
-        self.assertEqual(
-            self.shop_product.data.get("best_before_date"),
-            self.production_lot.life_date[:10],
         )
-
-        # add a lot with an older date...
-        new_lot = self.production_lot.copy(
+        cls.product = cls.env["product.product"].create(
+            {"name": "test product", "tracking": "lot", "type": "product"}
+        )
+        cls.location_physical = cls.env.ref(
+            "alc_stock_location_data.stock_location_vlb"
+        )
+        cls.location = cls.env["stock.location"].create(
             {
-                "name": "lot2",
-                "life_date": fields.Datetime.to_string(
-                    datetime.now() + timedelta(weeks=1)
-                ),
+                "name": "Test physical",
+                "usage": "internal",
+                "location_id": cls.location_physical.id,
             }
         )
-        with SeAdapterFake.mocked_calls():
-            self._add_product_qty(self.product, production_lot=new_lot, quantity=10)
-        self.assertEqual(
-            self.shop_product.data.get("best_before_date"), new_lot.life_date[:10]
-        )
+        cls.binding = cls.product._add_to_index(cls.index)
+        cls.product._compute_binding_ids()
 
-    def test_best_before_date_in_data(self):
-        self.production_lot.life_date = fields.Datetime.to_string(
-            datetime.now() + timedelta(weeks=2)
+    def _create_lot_at_date(self, date):
+        lot = self.env["stock.lot"].create(
+            {
+                "name": date.isoformat(),
+                "product_id": self.product.id,
+                "expiration_date": date,
+            }
         )
-        with SeAdapterFake.mocked_calls():
-            self._add_product_qty(
-                self.product, production_lot=self.production_lot, quantity=10
-            )
-        self.shop_product.recompute_json()
-        self.assertIn("best_before_date", self.shop_product.data)
-        self.assertEqual(
-            self.production_lot.life_date[:10],
-            self.shop_product.data["best_before_date"],
-        )
+        self.env["stock.quant"].with_context(
+            inventory_mode=True, queue_job__no_delay=True
+        ).create(
+            {
+                "product_id": self.product.id,
+                "inventory_quantity": 10,
+                "location_id": self.location.id,
+                "lot_id": lot.id,
+            }
+        ).action_apply_inventory()
+        self.product.invalidate_recordset()
+
+    def assertBestBeforeDate(self, date):
+        date_iso = date.date().isoformat()
+        product = ProductProduct.from_product_product(self.product)
+        self.assertEqual(product.best_before_date, date_iso)
+        self.assertEqual(self.binding.state, "to_recompute")
+        self.binding.recompute_json()
+        self.assertEqual(self.binding.state, "to_export")
+        self.assertEqual(self.binding.data.get("best_before_date"), date_iso)
+
+    def test_0(self):
+        product = ProductProduct.from_product_product(self.product)
+        self.assertEqual(product.best_before_date, None)
+
+    def test_1(self):
+        best_before_date = datetime.now() + relativedelta(days=30)
+        self._create_lot_at_date(best_before_date)
+        self.assertBestBeforeDate(best_before_date)
+        self._create_lot_at_date(best_before_date - relativedelta(days=1))
+        self.assertBestBeforeDate(best_before_date - relativedelta(days=1))
