3014a6ce550b7b8697002ef2bbe75a583c659c90
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-11-16 16:33:12 +0100

    [MIG] alc_sale_cart_price_recalculation: Migration from 10.0
    refs #62683

--- odoo/addons/alc_sale_cart_price_recalculation/models/sale_order.py
+++ odoo/addons/alc_sale_cart_price_recalculation/models/sale_order.py
@@ -1,20 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import _, api, fields
 
-from odoo.addons.queue_job.job import job
+from odoo.addons.sale.models import sale_order
 
 
-class SaleOrder(models.Model):
-
-    _inherit = "sale.order"
-
+class SaleOrder(sale_order.SaleOrder):
     @api.model
     def _get_cart_to_recompute_price_domain(self):
-        """ Return a domain used to get the cart to recompute prices.
-        """
+        """Return a domain used to get the cart to recompute prices."""
         return [
             ("typology", "=", "cart"),
             ("date_order", "<", fields.Date.today()),
@@ -24,18 +19,11 @@ class SaleOrder(models.Model):
     def _cron_recompute_cart_price(self):
         carts = self.search(self._get_cart_to_recompute_price_domain())
         for cart in carts:
-            description = _("Recalculate prices on cart %s") % cart.name
+            description = _("Recalculate prices on cart %(name)s", name=cart.name)
             cart.with_delay(description=description)._recalculate_cart_price_to_now()
 
-    @job(default_channel="root.background.process")
     def _recalculate_cart_price_to_now(self):
-        """ Recompute prices on cart.
-        """
+        """Recompute prices on cart."""
         self.write({"date_order": fields.Datetime.now()})
-        return self._recalculate_cart_price_to_date_order()
-
-    @job(default_channel="root.background.process")
-    def _recalculate_cart_price_to_date_order(self):
         for cart in self:
-            cart.recalculate_prices()
-        return True
+            cart.action_update_prices()
--- odoo/addons/alc_sale_cart_price_recalculation/tests/test_alc_sale_cart_price_recalculation.py
+++ odoo/addons/alc_sale_cart_price_recalculation/tests/test_alc_sale_cart_price_recalculation.py
@@ -1,44 +1,54 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-import mock
+from unittest import mock
 
-from odoo.addons.alc_pricelist_discount.tests.common import PricelistDiscountCase
+from odoo.tests.common import TransactionCase
+from odoo.tools import mute_logger
 
 
-class TestAlcSaleCartPriceRecalculation(PricelistDiscountCase):
+class TestAlcSaleCartPriceRecalculation(TransactionCase):
     @classmethod
     def setUpClass(cls):
-        super(TestAlcSaleCartPriceRecalculation, cls).setUpClass()
+        super().setUpClass()
         cls.env = cls.env(
             context=dict(
-                cls.env.context, tracking_disable=True, test_queue_job_no_delay=True,
+                cls.env.context,
+                tracking_disable=True,
+                queue_job__no_delay=True,
             )
         )
-        cls.sale = cls.sale.with_context(**cls.env.context)
+        cls.partner = cls.env.ref("base.res_partner_1")
+        cls.partner.ref = "123321"
+        cls.sale = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "date_order": "2019-10-10",
+                "client_order_ref": "whatever the client want",
+                "order_line": [],
+            }
+        )
 
+    @mute_logger("odoo.addons.queue_job.utils")
     def test_cart_recompute_triggered(self):
-        """
-        Check that the cart recompute job is triggered
-        """
-        self.sale.write(dict(date_order="2017-01-01 00:00:00", typology="cart"))
+        """Check that the cart recompute job is triggered."""
+        self.sale.write({"date_order": "2017-01-01 00:00:00", "typology": "cart"})
         # recalculate only apply to past cart
         with mock.patch.object(
-            self.sale.__class__, "recalculate_prices"
+            self.sale.__class__, "action_update_prices"
         ) as mock_recalculate_prices:
             self.sale._cron_recompute_cart_price()
             self.assertTrue(mock_recalculate_prices.called)
         # once updated a new call to the cron is without effect
         with mock.patch.object(
-            self.sale.__class__, "recalculate_prices"
+            self.sale.__class__, "action_update_prices"
         ) as mock_recalculate_prices:
             self.sale._cron_recompute_cart_price()
             self.assertFalse(mock_recalculate_prices.called)
 
         # check that the cart is not updated if it is not a cart
-        self.sale.write(dict(date_order="2017-01-01 00:00:00", typology="sale"))
+        self.sale.write({"date_order": "2017-01-01 00:00:00", "typology": "sale"})
         with mock.patch.object(
-            self.sale.__class__, "recalculate_prices"
+            self.sale.__class__, "action_update_prices"
         ) as mock_recalculate_prices:
             self.sale._cron_recompute_cart_price()
             self.assertFalse(mock_recalculate_prices.called)
