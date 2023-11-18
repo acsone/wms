c351c1c76d6f90693a758832b96f0b2cd1953cac
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-11-18 15:54:57 +0100

    [IMP] alc_eshop_api_orders: Add missing dependency
    refs #62613

--- odoo/addons/alc_eshop_api_orders/tests/test_orders.py
+++ odoo/addons/alc_eshop_api_orders/tests/test_orders.py
@@ -1,10 +1,10 @@
 # Copyright 2022 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from .common import TestOrders
+from .common import TestOrdersCase
 
 
-class TestOrdersFlow(TestOrders):
+class TestOrders(TestOrdersCase):
     def test_orders_flow(self):
         with self._create_test_client(partner=self.partner) as test_client:
             params = {"page": 1, "per_page": 10}
