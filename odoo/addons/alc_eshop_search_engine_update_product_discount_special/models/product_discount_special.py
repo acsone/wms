240fd2e9b5a793c8de5938ccb14e45e631e8af5e
sbejaoui <souheil.bejaoui@acsone.eu>
2023-11-21 16:07:34 +0100

    [MIG] alc_eshop_search_engine_update_product_discount_special
    refs: #62846

--- odoo/addons/alc_eshop_search_engine_update_product_discount_special/models/product_discount_special.py
+++ odoo/addons/alc_eshop_search_engine_update_product_discount_special/models/product_discount_special.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import models
+from odoo.models import Model
 
 
-class ProductDiscountSpecial(models.Model):
+class ProductDiscountSpecial(Model):
     _name = "product.discount.special"
-    _inherit = ["product.discount.special", "product.update.mixin"]
+    _inherit = ["product.discount.special", "se.product.update.mixin"]
 
     def get_products(self):
         return self.mapped("product_template_id")
