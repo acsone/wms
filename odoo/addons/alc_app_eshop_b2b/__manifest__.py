b71b27414699bd46eb6aa01c4e04e70b7dc8f6a5
sbejaoui <souheil.bejaoui@acsone.eu>
2023-11-22 18:19:54 +0100

    [MIG] shopinvader_multi_category
    refs: #62907

--- odoo/addons/alc_app_eshop_b2b/__manifest__.py
+++ odoo/addons/alc_app_eshop_b2b/__manifest__.py
@@ -85,6 +85,7 @@
         "shopinvader_search_engine_product_stock",
         "shopinvader_search_engine_product_stock_state",
         "shopinvader_product_attribute_set",
+        "shopinvader_multi_category",
     ],
     "data": [],
     "demo": [],
