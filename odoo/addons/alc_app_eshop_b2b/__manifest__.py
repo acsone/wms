0b6d34e5ede78decbbde196217442be7d26518b0
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-11-23 14:38:04 +0100

    [MIG] ensures same key for thumbnail
    Ensure that the key used to get thumbnails for a product uses the info than in 10.0 to avoir to regenerated thumbnails

--- odoo/addons/alc_app_eshop_b2b/__manifest__.py
+++ odoo/addons/alc_app_eshop_b2b/__manifest__.py
@@ -64,6 +64,8 @@
         "alc_eshop_models_url",
         "alc_eshop_search_engine_update_veterinary_group",
         "alc_eshop_search_engine_update_product_discount_special",
+        "alc_eshop_search_engine_image",
+        "alc_eshop_search_engine_product_stock",
         # shopinvader
         "shopinvader_search_engine",
         "shopinvader_search_engine_image",
@@ -86,7 +88,6 @@
         "shopinvader_search_engine_product_stock_state",
         "shopinvader_product_attribute_set",
         "shopinvader_multi_category",
-        "alc_eshop_search_engine_product_stock",
     ],
     "data": [
         "security/res_groups.xml",
