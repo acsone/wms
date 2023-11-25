6a1653d95ce78f35984b6e11268dfce955479146
sbejaoui <souheil.bejaoui@acsone.eu>
2023-11-25 11:43:19 +0100

    [MIG] alc_elasticsearch_price_sorting
    refs: #62555

--- odoo/addons/alc_elasticsearch_price_sorting/models/se_index.py
+++ odoo/addons/alc_elasticsearch_price_sorting/models/se_index.py
@@ -1,42 +1,48 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
 from datetime import datetime, timedelta
 
-from odoo import _, models
+from odoo import _
+from odoo.exceptions import UserError
 
-from odoo.addons.queue_job.job import job
+from odoo.addons.connector_search_engine.models.se_index import SeIndex as SeIndexBase
+
+_logger = logging.getLogger(__name__)
 
 
-class SeIndex(models.Model):
-
-    _inherit = "se.index"
-
+class SeIndex(SeIndexBase):
     def _get_es_client(self):
         self.ensure_one()
-        return self.backend_id.specific_backend._get_es_client()
+        return self.backend_id._get_es_client()
 
     def execute_pipeline_set_current_price(self):
         for record in self:
             client = record._get_es_client()
-            task_def = client.update_by_query(
-                index=record.name.lower(),
-                pipeline="set-current-price",
-                wait_for_completion=False,
-            )
+            try:
+                task_def = client.update_by_query(
+                    index=record.name.lower(),
+                    pipeline="set-current-price",
+                    wait_for_completion=False,
+                )
+            except Exception as e:
+                _logger.error(e)
+                raise UserError(
+                    _("Fail to execute pipeline set current price.\n%(error)s", error=e)
+                ) from e
             record._delay_check_task(task_def["task"])
 
     def _delay_check_task(self, task_id, eta_delay_seconds=60):
         eta = datetime.now() + timedelta(seconds=eta_delay_seconds)
-        description = _("Check se-current-price task completion %s for index %s") % (
-            task_id,
-            self.name,
+        description = _(
+            "Check se-current-price task completion %(task)s for index %(name)s",
+            task=task_id,
+            name=self.name,
         )
         self.with_delay(eta=eta, description=description)._check_es_task_completion(
             task_id
         )
 
-    @job
     def _check_es_task_completion(self, task_id):
         client = self._get_es_client()
         task = client.tasks.get(task_id=task_id, wait_for_completion=False)
