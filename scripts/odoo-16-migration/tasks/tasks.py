52b881063255987d7288706202d7e00cb0b18fc2
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-11-26 11:37:20 +0100

    scripts: do not uninstall alce_stock_barcode_easy_operation
    It is used by alce_stock_barcode and
    alce_stock_backcode_picking_assignment

--- scripts/odoo-16-migration/tasks/tasks.py
+++ scripts/odoo-16-migration/tasks/tasks.py
@@ -754,7 +754,6 @@ def set_modules_to_remove():
         "alc_delivery_rounds_close_pickings_by_zone",  # replaced by alc_stock_release_channel_pick_allowed
         "alc_delivery_rounds_allatonce_assignment",
         "alc_delivery_rounds_partner_geolocalize",
-        "alce_stock_barcode_easy_operation",  # replaced by STD
         "web_decimal_numpad_dot",
         "alc_stock_picking_batch_delivery_rounds",  # replaced by alc_stock_release_channel_picking_batch_creation
         "specific_shipping_costs",  # replaced by alc_shipping_fee
