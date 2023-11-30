52bf2facb8de38353880df04dd24803c8e7972a8
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-11-30 16:35:06 +0100

    [FIX] alc_stock_move_direction: Add missing dependency

--- odoo/addons/alc_stock_move_direction/__manifest__.py
+++ odoo/addons/alc_stock_move_direction/__manifest__.py
@@ -10,7 +10,7 @@
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
-    "depends": ["stock"],
+    "depends": ["stock", "stock_helper"],
     "data": [],
     "demo": [],
 }
