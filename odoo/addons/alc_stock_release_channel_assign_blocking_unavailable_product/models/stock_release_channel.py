4a7c8a3add05bec79fc32100b801f75fea7b8eaf
sbejaoui <souheil.bejaoui@acsone.eu>
2023-12-03 23:00:32 +0100

    [IMP] add a message explaining when a picking is not assigned to a release channel
    refs: #7242

--- odoo/addons/alc_stock_release_channel_assign_blocking_unavailable_product/models/stock_release_channel.py
+++ odoo/addons/alc_stock_release_channel_assign_blocking_unavailable_product/models/stock_release_channel.py
@@ -1,11 +1,15 @@
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api
+import logging
+
+from odoo import _, api
 
 from odoo.addons.stock_release_channel.models.stock_release_channel import (
     StockReleaseChannel as StockReleaseChannelBase,
 )
+
+_logger = logging.getLogger(__name__)
 
 
 class StockReleaseChannel(StockReleaseChannelBase):
@@ -16,5 +20,12 @@ class StockReleaseChannel(StockReleaseChannelBase):
             not picking.ignore_release_channel_block
             and picking.delivery_requires_other_lines
         ):
-            return None
+            message = _(
+                "Transfer %(picking_name)s could not be assigned to a channel because "
+                "it's blocked. It requires either a new order to be unblocked or a "
+                "manual action from the user.",
+                picking_name=picking.name,
+            )
+            _logger.warning(message)
+            return message
         return super().assign_release_channel(picking)
