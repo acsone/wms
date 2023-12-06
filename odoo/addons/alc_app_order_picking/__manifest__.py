2dd6b8d2d46c7ec9cfdb8a961e9647e14c6cfb2a
Denis Roussel <denis.roussel@acsone.eu>
2023-12-06 17:22:31 +0100

    [ADD] stock_full_location_reservation
    refs #72438

--- odoo/addons/alc_app_order_picking/__manifest__.py
+++ odoo/addons/alc_app_order_picking/__manifest__.py
@@ -36,6 +36,7 @@
         "stock_available_base_exclude_location",
         "stock_available_location_orderpoint",
         "stock_dynamic_routing",
+        "stock_full_location_reservation",
         "stock_inventory",
         "stock_location_orderpoint",
         "stock_location_orderpoint_average_daily_sale",
