ab8ccf68f5a025345070360596d5a58476e9e9c0
Denis Roussel <denis.roussel@acsone.eu>
2023-12-06 18:48:27 +0100

    [UPD] Set full reservation visibility on reassort picking types
    refs #72438

---
+++ odoo/addons/alc_all/migrations/16.0.2.6.37/post-set-full-reassort.py
@@ -0,0 +1,16 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    # Set full reservation for réassorts picking types
+    ids = [14, 37, 13]
+    picking_types = env["stock.picking.type"].browse(ids)
+    picking_types.write(
+        {
+            "is_full_location_reservation_visible": True,
+        }
+    )
