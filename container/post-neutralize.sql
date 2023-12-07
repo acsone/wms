7d1e83731ef4b31dcf1c29c5b83e9e6e57c2c362
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-12-07 11:32:09 +0100

    Neutralize database in container entrypoint

---
+++ container/post-neutralize.sql
@@ -0,0 +1,7 @@
+-- Revert a few thing that the Odoo neutralization scripts did and we don't want.
+
+\set ON_ERROR_STOP
+
+-- we use mail_environment
+DELETE FROM ir_mail_server WHERE name like 'neutralization%';
+UPDATE ir_mail_server SET active=TRUE;
