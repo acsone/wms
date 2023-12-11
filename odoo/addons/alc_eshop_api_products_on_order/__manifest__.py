aca6f59fb9672ef7475e6399044b95c5d1420f84
Denis Roussel <denis.roussel@acsone.eu>
2023-12-11 19:01:59 +0100

    [FIX] alc_eshop_api_products_on_order: xml template should be applied
    The template should be updated manually during migration.
    
    Moreover, a duplicate still exists (as the former module does not exists anymore).
    
    refs #72486

--- odoo/addons/alc_eshop_api_products_on_order/__manifest__.py
+++ odoo/addons/alc_eshop_api_products_on_order/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Alc Eshop Product On Order",
     "description": """
         Aclyon EShop: Products on order management services""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
