01c35b078bb9c19f58c467bffe4978c0bf6123b9
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-12-18 11:05:45 +0100

    [FIX] alc_eshop_search_engine_ads: Export to right index
    Ensures ads are exported only into the index configured for the given lang is one is set on the record. Takes also into account that the lang could be updated on the record.
    
    refs #72604

--- odoo/addons/alc_eshop_search_engine_ads/models/alc_eshop_ads.py
+++ odoo/addons/alc_eshop_search_engine_ads/models/alc_eshop_ads.py
@@ -1,7 +1,7 @@
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import Command, api, fields
+from odoo import api, fields
 from odoo.models import Model
 
 from odoo.addons.connector_search_engine.models.se_index import SeIndex
@@ -13,10 +13,17 @@ class AlcEshopAds(Model):
     _inherit = ["alc.eshop.ads", "se.indexable.record"]
     se_index_ids = fields.Many2many[SeIndex](compute="_compute_se_index")
 
+    @api.depends("lang_id")
     def _compute_se_index(self):
         model = self.env.ref("alc_eshop_ads.model_alc_eshop_ads")
-        indexes = self.env["se.index"].search([("model_id", "=", model.id)])
-        self.update({"se_index_ids": [Command.set(indexes.ids)]})
+        all_indexes = self.env["se.index"].search([("model_id", "=", model.id)])
+        for rec in self:
+            indexes = all_indexes
+            if rec.lang_id:
+                indexes = all_indexes.filtered(
+                    lambda i, record=rec: i.lang_id == record.lang_id
+                )
+            rec.se_index_ids = indexes
 
     def action_toggle_is_published(self):
         res = super().action_toggle_is_published()
@@ -32,11 +39,32 @@ class AlcEshopAds(Model):
         for rec in self:
             rec.button_synchronize_ads()
 
+    def _ensure_ads_in_right_indexes(self):
+        for record in self:
+            if not record.is_published:
+                if record.se_binding_ids:
+                    record._remove_from_index(record.se_binding_ids.index_id)
+                continue
+            if record.lang_id:
+                wrong_bindings = record.se_binding_ids.filtered(
+                    lambda i, record=record: i.index_id.lang_id != record.lang_id
+                )
+                if wrong_bindings:
+                    record._remove_from_index(wrong_bindings.index_id)
+                if not record.se_binding_ids.filtered(
+                    lambda i, record=record: i.index_id.lang_id == record.lang_id
+                ):
+                    if record.se_index_ids:
+                        record._add_to_index(record.se_index_ids)
+
     def button_synchronize_ads(self):
         self.ensure_one()
-        self.filtered_domain(
-            self._get_ads_to_export_domain()
-        ).se_binding_ids.recompute_json()
+        # remove binding that are in an specific language and present in the
+        # wrong indexes
+        self._ensure_ads_in_right_indexes()
+        self.filtered_domain(self._get_ads_to_export_domain()).se_binding_ids.filtered(
+            lambda i: i.state != "to_delete"
+        ).recompute_json()
         self.filtered_domain(
             self._get_ads_to_delete_domain()
         ).action_toggle_is_published()
