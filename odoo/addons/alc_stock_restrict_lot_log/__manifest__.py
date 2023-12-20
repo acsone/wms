bcda03cd1312d7bc67f58201bc0ac22023f5f5f1
sbejaoui <souheil.bejaoui@acsone.eu>
2023-12-20 17:36:30 +0100

    [IMP] log traceback when a move line assigne to a different lot than the restrict
    refs: #72703

---
+++ odoo/addons/alc_stock_restrict_lot_log/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Stock Restrict Lot Log",
+    "description": """
+        This addon log the traceback each time a move line is assigned to a lot
+         different then it's restrict lot. To be removed after finding the process
+         responsible for this bug""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": ["stock_restrict_lot"],
+    "data": [],
+    "demo": [],
+}
