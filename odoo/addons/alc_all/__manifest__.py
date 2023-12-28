0dc50027c1e9a834d32b632505f4ce746e5014dc
sbejaoui <souheil.bejaoui@acsone.eu>
2023-12-28 11:59:25 +0100

    Bump version: 2.6.73 → 2.6.74

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Alc All",
     "description": """
         Alcyon Odoo App""",
-    "version": "16.0.2.6.73",
+    "version": "16.0.2.6.74",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
