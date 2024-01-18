705cfdc35a2cb86aa3c7bc1854b6476b451b5bed
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2024-01-18 11:57:28 +0100

    Bump version: 2.6.103 → 2.6.104

--- odoo/addons/alc_all/__manifest__.py
+++ odoo/addons/alc_all/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Alc All",
     "description": """
         Alcyon Odoo App""",
-    "version": "16.0.2.6.103",
+    "version": "16.0.2.6.104",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
