4785f96029516308feee4ae74f1edd25a1e5ab29
sbejaoui <souheil.bejaoui@acsone.eu>
2024-01-23 11:15:32 +0100

    [IMP] add failing test for #73090
    refs: #73090

--- odoo/addons/alc_sale_order_line_cancel_check/tests/test_sale_order_line_cancel.py
+++ odoo/addons/alc_sale_order_line_cancel_check/tests/test_sale_order_line_cancel.py
@@ -3,15 +3,28 @@
 
 from odoo.exceptions import UserError
 
-from odoo.addons.sale_order_line_cancel.tests.common import TestSaleOrderLineCancelBase
+from odoo.addons.alc_sale_order_cancel.tests.common import TestSaleOrderCancelBase
 
 
-class TestSaleOrderLineCancel(TestSaleOrderLineCancelBase):
+class TestSaleOrderLineCancel(TestSaleOrderCancelBase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.wiz = cls.env["sale.order.line.cancel"].create({})
+        cls.sol = cls.so.order_line
+
     def test_cancel_remaining_qty_started_picking(self):
         """Check printed picking can't be canceled."""
-        ship = self.sale.picking_ids
-        ship.printed = True
+        self.pick.printed = True
         with self.assertRaises(UserError):
             self.wiz.with_context(
-                active_id=self.sale.order_line.id
+                active_id=self.sol.id, active_model=self.sol._name
+            ).cancel_remaining_qty()
+
+    def test_cancel_remaining_qty_done_preparation(self):
+        """Check done picking can't be canceled."""
+        self._do_transfer(self.pick)
+        with self.assertRaises(UserError):
+            self.wiz.with_context(
+                active_id=self.sol.id, active_model=self.sol._name
             ).cancel_remaining_qty()
