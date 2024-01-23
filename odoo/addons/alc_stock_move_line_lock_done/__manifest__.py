ac82508ec51488f703c7905538d9def910d68d89
sbejaoui <souheil.bejaoui@acsone.eu>
2024-01-23 16:06:53 +0100

    [IMP] Restrict modifications for validated stock moves
    refs: #73328

---
+++ odoo/addons/alc_stock_move_line_lock_done/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "stock move line lock done",
+    "summary": """Restrict modifications for validated stock moves""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": ["stock_move_line_lock_qty_done"],
+    "data": [],
+    "demo": [],
+}
