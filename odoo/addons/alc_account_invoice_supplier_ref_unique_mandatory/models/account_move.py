61de8147c7c848e05e0c2e069797088e0a354137
sbejaoui <souheil.bejaoui@acsone.eu>
2024-01-30 18:22:19 +0100

    [FIX] supplier invoice: check for supplier ref only at post
    refs: #73301

--- odoo/addons/alc_account_invoice_supplier_ref_unique_mandatory/models/account_move.py
+++ odoo/addons/alc_account_invoice_supplier_ref_unique_mandatory/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Acsone
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api
+from odoo import _, api, fields
 from odoo.exceptions import ValidationError
 
 from odoo.addons.account_invoice_supplier_ref_unique.models.account_move import (
@@ -10,26 +10,22 @@ from odoo.addons.account_invoice_supplier_ref_unique.models.account_move import 
 
 
 class AccountMove(AccountMoveBase):
-    @api.constrains("supplier_invoice_number")
+    supplier_invoice_number_required = fields.Boolean(
+        compute="_compute_supplier_invoice_number_required"
+    )
+
+    @api.depends("company_id", "move_type", "state")
+    def _compute_supplier_invoice_number_required(self):
+        for rec in self:
+            rec.supplier_invoice_number_required = (
+                rec.company_id.check_invoice_supplier_number_mandatory
+                and rec.is_purchase_document(include_receipts=True)
+                and rec.state == "posted"
+            )
+
+    @api.constrains("supplier_invoice_number", "company_id", "move_type", "state")
     def _check_mandatory_supplier_invoice_number(self):
         """Check that the supplier_invoice_number is given on invoice/refund."""
-        if any(
-            rec.company_id.check_invoice_supplier_number_mandatory
-            and rec.is_purchase_document(include_receipts=True)
-            and not rec.supplier_invoice_number
-            for rec in self
-        ):
-            raise ValidationError(_("The supplier invoice number is mandatory."))
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        purchase_types = self.get_purchase_types(include_receipts=True)
-        if any(
-            self.env.company.check_invoice_supplier_number_mandatory
-            and "move_type" in vals
-            and vals["move_type"] in purchase_types
-            and "supplier_invoice_number" not in vals
-            for vals in vals_list
-        ):
-            raise ValidationError(_("The supplier invoice number is mandatory."))
-        return super().create(vals_list)
+        for rec in self:
+            if rec.supplier_invoice_number_required and not rec.supplier_invoice_number:
+                raise ValidationError(_("The supplier invoice number is mandatory."))
