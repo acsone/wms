4503892d139c8d92de8d900538e2c43256fa21d7
Denis Roussel <denis.roussel@acsone.eu>
2024-01-31 16:38:01 +0100

    [IMP] alc_report_invoice: Ensure taxes are well set after first accounting chart loading

--- odoo/addons/alc_report_invoice/tests/test_invoice_report.py
+++ odoo/addons/alc_report_invoice/tests/test_invoice_report.py
@@ -4,10 +4,11 @@ from odoo.fields import Command
 from odoo.tests import tagged
 
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo.addons.base.tests.common import BaseCommon
 
 
 @tagged("post_install", "-at_install")
-class TestReportInvoice(AccountTestInvoicingCommon):
+class TestReportInvoice(AccountTestInvoicingCommon, BaseCommon):
     @classmethod
     def setUpClass(cls, chart_template_ref=None):
         super().setUpClass(chart_template_ref="l10n_be.l10nbe_chart_template")
@@ -15,6 +16,15 @@ class TestReportInvoice(AccountTestInvoicingCommon):
         cls.sale_obj = cls.env["sale.order"]
         cls.product_obj = cls.env["product.product"]
         cls.apb_group = cls.env.ref("l10n_be_apb_tax.tax_group_apb")
+        cls.sale_group = cls.env.ref("l10n_be.tax_group_tva_21")
+        tax_21 = cls.env["account.tax"].search(
+            [
+                ("type_tax_use", "=", "sale"),
+                ("tax_group_id", "=", cls.sale_group.id),
+                ("company_id", "=", cls.env.company.id),
+            ],
+            limit=1,
+        )
         taxes_apb = cls.env["account.tax"].search(
             [
                 ("tax_group_id", "=", cls.apb_group.id),
@@ -22,13 +32,22 @@ class TestReportInvoice(AccountTestInvoicingCommon):
             ],
             limit=1,
         )
-        cls.partner = cls.env["res.partner"].create({"name": "Partner Test"})
+        pricelist = cls.env["product.pricelist"].search(
+            [("currency_id", "=", cls.env.company.currency_id.id)], limit=1
+        )
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Partner Test", "property_product_pricelist": pricelist.id}
+        )
         # As this module does not depends on stock, create products as service
         cls.product_1 = cls.product_obj.create(
-            {"name": "Product 1 APB", "type": "service"}
+            {"name": "Product 1 APB", "type": "service", "service_type": "manual"}
         )
-        cls.product_1.taxes_id |= taxes_apb
-        cls.product_2 = cls.product_obj.create({"name": "Product 2", "type": "service"})
+        # Ensure sale tax is well configured
+        cls.product_1.taxes_id = tax_21 | taxes_apb
+        cls.product_2 = cls.product_obj.create(
+            {"name": "Product 2", "type": "service", "service_type": "manual"}
+        )
+        cls.product_2.taxes_id = tax_21
 
     @classmethod
     def _create_sale_order(cls):
