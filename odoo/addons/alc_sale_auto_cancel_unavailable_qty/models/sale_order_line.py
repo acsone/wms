b7c8c5b4ec8197c2dbfed79a055ebacdd19f8722
Denis Roussel <denis.roussel@acsone.eu>
2024-02-08 15:04:18 +0100

    [ADD] stock_dropshipping, [FIX] alc_sale_auto_cancel_unavailable_qty function signature
    refs #73300

--- odoo/addons/alc_sale_auto_cancel_unavailable_qty/models/sale_order_line.py
+++ odoo/addons/alc_sale_auto_cancel_unavailable_qty/models/sale_order_line.py
@@ -7,7 +7,9 @@ from odoo.addons.sale.models.sale_order_line import SaleOrderLine as SaleOrderLi
 class SaleOrderLine(SaleOrderLineBase):
     def _get_qty_procurement(self, previous_product_uom_qty=False):
         self.ensure_one()
-        qty = super()._get_qty_procurement()
+        qty = super()._get_qty_procurement(
+            previous_product_uom_qty=previous_product_uom_qty
+        )
         if (
             self.order_partner_id.auto_cancel_unavailable_qty_sold
             and self.product_id.type != "service"
