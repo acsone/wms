40ba01248a08c42039d3f9fb5cb974f13defb41d
Laurent Stukkens <laurent.stukkens@acsone.eu>
2024-02-20 18:18:00 +0100

    [FIX] alc_account_invoice_email: ensure that credit notes are sent by mail using the rigth template
    refs: #73371

---
+++ odoo/addons/alc_account_invoice_email/migrations/16.0.1.0.1/pre-migrate.py
@@ -0,0 +1,17 @@
+# Copyright 2024 ACSONE SA/NV
+
+import logging
+
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    _logger.info("Forcing update of account.email_template_edi_credit_note")
+    openupgrade.load_data(
+        cr,
+        "alc_account_invoice_email",
+        "migrations/16.0.1.0.1/mail_template.xml",
+        mode="init",
+    )
