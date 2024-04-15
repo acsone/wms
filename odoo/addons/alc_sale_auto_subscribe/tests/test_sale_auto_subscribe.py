4eeb06268549de48f682aa2f059e5c390b6f9222
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2024-04-15 13:42:32 +0200

    [ADD] alc_sale_auto_subscribe: Auto subscribe callcenter to SO
    This addon auto subscribe a specific callcenter user to default notifications on SO to ensure that the call center is notified in case of notifications (ie incoming mails) without being spam by notification from others models.
    
    refs #74933

---
+++ odoo/addons/alc_sale_auto_subscribe/tests/test_sale_auto_subscribe.py
@@ -0,0 +1,29 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestSaleAutoSubscribe(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user_callcenter = cls.env.ref("alc_sale_auto_subscribe.alc_user_callcenter")
+
+    def test_auto_subscribe(self):
+        """
+        Test case:
+
+        - Create a sale order
+        - Check that the callcenter user is auto subscribed to the sale order
+        """
+        sale_order = self.env["sale.order"].create(
+            {
+                "partner_id": self.env.ref("base.res_partner_2").id,
+                "partner_invoice_id": self.env.ref("base.res_partner_2").id,
+                "partner_shipping_id": self.env.ref("base.res_partner_2").id,
+            }
+        )
+        self.assertIn(
+            self.user_callcenter.partner_id.id, sale_order.message_partner_ids.ids
+        )
