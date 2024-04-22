68000bcacac5b0a7a9ff4ae28a31ac331eb65ac0
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2024-04-22 16:22:12 +0200

    [IMP] alc_shopfloor_mobile_qty_picker_font_style: highlights uom in qty picker
    refs #75427

--- odoo/addons/alc_shopfloor_mobile_qty_picker_font_style/static/src/js/packaging-qty-picker.js
+++ odoo/addons/alc_shopfloor_mobile_qty_picker_font_style/static/src/js/packaging-qty-picker.js
@@ -14,16 +14,31 @@ computed.qty_color = function () {
   return new_result;
 };
 
-const unit_color_computed = computed.unit_color;
-
 computed.unit_color = function () {
-  const result = unit_color_computed.bind(this)();
   const font_style = "font-weight:950; font-size: 200%; color:rgb(102, 0, 0)";
-  const is_not_unit = this.opts.uom.id != 1; // Id of unit of measure "unit" is 1.
+  const is_not_unit = this.uom.id != 1; // Id of unit of measure "unit" is 1.
   const background_style = is_not_unit
     ? "background-color: orangered!important"
     : "background-color:transparent; ";
 
-  const new_result = result + ";" + font_style + ";" + background_style;
-  return new_result;
+  const result = ";" + font_style + ";" + background_style;
+  return result;
 };
+
+const template = PackagingQtyPicker.extendOptions.template;
+
+/* Replace the multiline
+    <v-col>
+        {{ unit_uom.name }}
+    </v-col>
+  with
+   <v-col :style="unit_color">
+      {{ unit_uom.name }}
+  </v-col>
+  */
+const new_template = template.replace(
+  /<v-col>\s*{{\s*unit_uom.name\s*}}\s*<\/v-col>/,
+  '<v-col :style="unit_color">{{ unit_uom.name }}</v-col>'
+);
+
+PackagingQtyPicker.extendOptions.template = new_template;
