5f39db3d15e180dd8136a45877b2ebdf7c8d2bc0
Denis Roussel <denis.roussel@acsone.eu>
2024-04-30 11:46:01 +0200

    [IMP] Set Vendor Bills journal 'avoid_zero_lines' property to True
    refs #75587

--- odoo/addons/alc_app_invoicing/__manifest__.py
+++ odoo/addons/alc_app_invoicing/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Alc Invoicing App",
     "description": """
         Gather all Invoicing related modules for Alcyon""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu",
