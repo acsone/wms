8624dd9166b92e64aacbd5553082628b2cb98d90
Denis Roussel <denis.roussel@acsone.eu>
2024-05-23 11:03:05 +0200

    [ADD] alc/stock_location_orderpoint_cleanup: Add module to clean replenishments + specific data for Alcyon
    refs #75903

---
+++ odoo/addons/alc_stock_location_orderpoint_cleanup/README.rst
@@ -0,0 +1,23 @@
+=====================================
+Alc Stock Location Orderpoint Cleanup
+=====================================
+
+Allows to define crons to clean replenishments
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+#. Go to ...
+
+Usage
+=====
+
+To use this module, you need to:
+
+#. Go to ...
+
+
+Changelog
+=========
---
+++ odoo/addons/alc_stock_location_orderpoint_cleanup/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Stock Location Orderpoint Cleanup",
+    "description": """
+        Allows to define crons to clean replenishments""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": [
+        "queue_job_cron",
+        "stock_location_orderpoint_cleanup",
+        "stock_location_orderpoint_average_daily_sale",
+    ],
+    "data": [
+        "data/res_users.xml",
+        "data/ir_cron.xml",
+    ],
+}
