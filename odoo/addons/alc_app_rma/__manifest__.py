7ff25b726dbcc3012b26d228872300329ae6279c
sbejaoui <souheil.bejaoui@acsone.eu>
2024-06-11 14:00:57 +0200

    [ADD] add rma

---
+++ odoo/addons/alc_app_rma/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc RMA App",
+    "description": """
+        Gather all rma related modules for Alcyon""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "application": True,
+    "depends": [
+        # OCA
+        "rma",
+    ],
+}
