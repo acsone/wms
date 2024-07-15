3903ce61dd5e46892ff17c905a24bb1300364911
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2024-07-15 16:05:10 +0200

    Neutralize keycloak_user

--- container/post-neutralize.sql
+++ container/post-neutralize.sql
@@ -5,3 +5,5 @@
 -- we use mail_environment
 DELETE FROM ir_mail_server WHERE name like 'neutralization%';
 UPDATE ir_mail_server SET active=TRUE;
+DELETE FROM keycloak_user where id <> 1;
+UPDATE keycloak_user SET username='acsone', keycloak_username='acsone' where id=1;
