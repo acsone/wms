6aedf6540ce280829a2889cb44b77a19715daa7a
Denis Roussel <denis.roussel@acsone.eu>
2024-07-17 15:17:57 +0200

    [IMP] Update existing package types (PAL, ...) with their categories
    refs #77269

--- odoo/addons/alc_app_order_picking/migrations/16.0.1.0.6/end-migrate.py
+++ odoo/addons/alc_app_order_picking/migrations/16.0.1.0.6/end-migrate.py
@@ -12,7 +12,7 @@ def _update_existing_package_types(env):
     # Setting existing Boites to category 'Médicaments'
     _logger.info("Updating Médicaments boxes")
     category = env.ref(
-        "alc_stock_package_type_category.alc_package_type_category_medicament"
+        "alc_stock_package_type_category_data.alc_package_type_category_medicament"
     )
     package_types = env["stock.package.type"].search(
         [
@@ -28,10 +28,14 @@ def _create_new_package_types(env):
     # Create missing boxes types
     _logger.info("Creating missing boxes")
     package_type_obj = env["stock.package.type"]
-    ali = env.ref("alc_stock_package_type_category.alc_package_type_category_aliment")
-    frigo = env.ref("alc_stock_package_type_category.alc_package_type_category_frigo")
+    ali = env.ref(
+        "alc_stock_package_type_category_data.alc_package_type_category_aliment"
+    )
+    frigo = env.ref(
+        "alc_stock_package_type_category_data.alc_package_type_category_frigo"
+    )
     materiel = env.ref(
-        "alc_stock_package_type_category.alc_package_type_category_materiel"
+        "alc_stock_package_type_category_data.alc_package_type_category_materiel"
     )
     categories = ali | frigo | materiel
     for category in categories:
@@ -51,7 +55,7 @@ def _update_existing_package_types_other(env):
     # Setting existing package types (PAL, etc...)
     _logger.info("Updating Médicaments package types")
     category = env.ref(
-        "alc_stock_package_type_category.alc_package_type_category_medicament"
+        "alc_stock_package_type_category_data.alc_package_type_category_medicament"
     )
     package_types = env["stock.package.type"].search(
         [
@@ -65,7 +69,7 @@ def _update_existing_package_types_other(env):
     package_types.write({"category_id": category.id})
 
     category = env.ref(
-        "alc_stock_package_type_category.alc_package_type_category_aliment"
+        "alc_stock_package_type_category_data.alc_package_type_category_aliment"
     )
     _logger.info("Updating Aliments package types")
     package_types = env["stock.package.type"].search(
@@ -80,7 +84,7 @@ def _update_existing_package_types_other(env):
     package_types.write({"category_id": category.id})
 
     category = env.ref(
-        "alc_stock_package_type_category.alc_package_type_category_frigo"
+        "alc_stock_package_type_category_data.alc_package_type_category_frigo"
     )
     _logger.info("Updating Frigo package types")
     package_types = env["stock.package.type"].search(
@@ -95,7 +99,7 @@ def _update_existing_package_types_other(env):
     package_types.write({"category_id": category.id})
 
     category = env.ref(
-        "alc_stock_package_type_category.alc_package_type_category_materiel"
+        "alc_stock_package_type_category_data.alc_package_type_category_materiel"
     )
     _logger.info("Updating Materiel package types")
     package_types = env["stock.package.type"].search(
@@ -108,8 +112,150 @@ def _update_existing_package_types_other(env):
     package_types.write({"category_id": category.id})
 
 
+def _create_and_update_package_types_for_internal_packages(env):
+    # Create package types for TA / TM / PA packages
+    # and affect it to them
+    package_type_obj = env["stock.package.type"]
+
+    # Aliments
+    category = env.ref(
+        "alc_stock_package_type_category_data.alc_package_type_category_aliment"
+    )
+
+    # PA
+    pa = package_type_obj.create(
+        {
+            "name": "PA1 Interne",
+            "package_carrier_type": False,
+            "category_id": category.id,
+        }
+    )
+    packages = env["stock.quant.package"].search(
+        [
+            ("is_internal", "=", True),
+            ("name", "like", "PA1-%"),
+            ("package_type_id", "=", False),
+        ]
+    )
+    packages.write(
+        {
+            "package_type_id": pa.id,
+        }
+    )
+
+    # TA09
+    ta09 = package_type_obj.create(
+        {
+            "name": "TA09 Interne",
+            "package_carrier_type": False,
+            "category_id": category.id,
+        }
+    )
+    packages = env["stock.quant.package"].search(
+        [
+            ("is_internal", "=", True),
+            ("name", "like", "TA09-%"),
+            ("package_type_id", "=", False),
+        ]
+    )
+    packages.write(
+        {
+            "package_type_id": ta09.id,
+        }
+    )
+
+    # TA1
+    ta1 = package_type_obj.create(
+        {
+            "name": "TA1 Interne",
+            "package_carrier_type": False,
+            "category_id": category.id,
+        }
+    )
+    packages = env["stock.quant.package"].search(
+        [
+            ("is_internal", "=", True),
+            ("name", "like", "TA1-%"),
+            ("package_type_id", "=", False),
+        ]
+    )
+    packages.write(
+        {
+            "package_type_id": ta1.id,
+        }
+    )
+
+    # TA12
+    ta12 = package_type_obj.create(
+        {
+            "name": "TA12 Interne",
+            "package_carrier_type": False,
+            "category_id": category.id,
+        }
+    )
+    packages = env["stock.quant.package"].search(
+        [
+            ("is_internal", "=", True),
+            ("name", "like", "TA12-%"),
+            ("package_type_id", "=", False),
+        ]
+    )
+    packages.write(
+        {
+            "package_type_id": ta12.id,
+        }
+    )
+
+    # Médicaments
+    category = env.ref(
+        "alc_stock_package_type_category_data.alc_package_type_category_medicament"
+    )
+
+    # TM1
+    tm1 = package_type_obj.create(
+        {
+            "name": "TM1 Interne",
+            "package_carrier_type": False,
+            "category_id": category.id,
+        }
+    )
+    packages = env["stock.quant.package"].search(
+        [
+            ("is_internal", "=", True),
+            ("name", "like", "TM1-%"),
+            ("package_type_id", "=", False),
+        ]
+    )
+    packages.write(
+        {
+            "package_type_id": tm1.id,
+        }
+    )
+    # TM6
+    tm6 = package_type_obj.create(
+        {
+            "name": "TM6 Interne",
+            "package_carrier_type": False,
+            "category_id": category.id,
+        }
+    )
+    packages = env["stock.quant.package"].search(
+        [
+            ("is_internal", "=", True),
+            ("name", "like", "TM6-%"),
+            ("package_type_id", "=", False),
+        ]
+    )
+    packages.write(
+        {
+            "package_type_id": tm6.id,
+        }
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     _update_existing_package_types(env)
     _create_new_package_types(env)
     _update_existing_package_types_other(env)
+    _create_and_update_package_types_for_internal_packages(env)
