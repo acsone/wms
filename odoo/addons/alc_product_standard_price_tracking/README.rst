df75828a93ab35863630514101719178c84c20ca
sbejaoui <souheil.bejaoui@acsone.eu>
2024-07-17 19:27:16 +0200

    [IMP] make standard price field tracked in product chat
    refs: #77175

---
+++ odoo/addons/alc_product_standard_price_tracking/README.rst
@@ -0,0 +1,23 @@
+===================================
+Alc Product Standard Price Tracking
+===================================
+
+This addon make standard price field tracked in product chat
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+#. Go to ...
+
+Usage
+=====
+
+To use this module, you need to:
+
+#. Go to ...
+
+
+Changelog
+=========
---
+++ odoo/addons/alc_product_standard_price_tracking/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Product Standard Price Tracking",
+    "description": """
+        This addon make standard price field tracked in product chat""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": ["product"],
+    "data": [],
+    "demo": [],
+}
