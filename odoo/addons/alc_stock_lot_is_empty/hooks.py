3804e24e49f99dee242a1686ae35c4ec6322390d
sbejaoui <souheil.bejaoui@acsone.eu>
2024-08-02 09:34:07 +0200

    [FIX] lot is emptyt if all his quant are not internal
    refs: #77584

--- odoo/addons/alc_stock_lot_is_empty/hooks.py
+++ odoo/addons/alc_stock_lot_is_empty/hooks.py
@@ -11,12 +11,17 @@ def pre_init_hook(cr):
     # Update the is_empty value to False for lots linked to quants
     cr.execute(
         """
-            UPDATE stock_lot
-            SET is_empty = FALSE
-            WHERE id IN (
-                SELECT DISTINCT lot_id
-                FROM stock_quant
-                WHERE lot_id IS NOT NULL
-            );
+        UPDATE stock_lot
+        SET is_empty = FALSE
+        WHERE id IN (
+            SELECT DISTINCT lot_id
+            FROM stock_quant
+            WHERE lot_id IS NOT NULL
+            AND location_id IN (
+                SELECT id
+                FROM stock_location
+                WHERE usage = 'internal'
+            )
+        );
         """
     )
