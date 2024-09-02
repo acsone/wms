de31c0cf9f2b9c2b5aeeaae464816ec8ab84d1c8
Denis Roussel <denis.roussel@acsone.eu>
2024-09-02 15:56:57 +0200

    [ADD] Tests for shopfloor reassort
    refs #77949

---
+++ odoo/addons/test_alcyon_shopfloor_reassort/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Test Alcyon Shopfloor Reassort",
+    "description": """Tests for reassort using Shopfloor""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": [
+        # OCA
+        "shopfloor_full_location_reservation",
+        "stock_full_location_reservation",
+        "stock_location_orderpoint",
+    ],
+}
