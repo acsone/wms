0c09372d3425b748469f72dfd043bf9686262d10
sbejaoui <souheil.bejaoui@acsone.eu>
2024-09-24 18:01:53 +0200

    Bump version: 2.17.16 → 2.17.17

--- odoo/addons/alce_all/__manifest__.py
+++ odoo/addons/alce_all/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Alc All Enterprise",
     "description": """
         Alcyon addons under Odoo Enterprise licence""",
-    "version": "16.0.2.17.16",
+    "version": "16.0.2.17.17",
     "license": "Other proprietary",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
