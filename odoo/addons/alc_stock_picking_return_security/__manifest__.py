5c68632db8db58dded79ea573a10dbbc599946e5
sbejaoui <souheil.bejaoui@acsone.eu>
2024-10-10 16:19:49 +0200

    [ADD] alc_stock_picking_return_security
    ser group to restrict access to the return action in stock pickings
    
    refs: #79035

---
+++ odoo/addons/alc_stock_picking_return_security/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Stock Picking Return Security",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu",
+    "depends": [
+        # Others
+        "stock",
+    ],
+    "data": [
+        "security/groups.xml",
+        "views/stock_picking.xml",
+    ],
+    "demo": [],
+}
