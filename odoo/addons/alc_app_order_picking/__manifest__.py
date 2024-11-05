ed8563e147b0690416dd8cd6b6a690c9f42bf153
Denis Roussel <denis.roussel@acsone.eu>
2024-11-05 16:25:30 +0100

    [ADD] stock_location_child_location
    refs #78543

--- odoo/addons/alc_app_order_picking/__manifest__.py
+++ odoo/addons/alc_app_order_picking/__manifest__.py
@@ -29,6 +29,7 @@
         "stock_available_to_promise_release_restrict_lot",
         "stock_dynamic_routing",
         "stock_full_location_reservation",
+        "stock_location_child_location",
         "stock_location_orderpoint",
         "stock_location_orderpoint_average_daily_sale",
         "stock_location_orderpoint_change_priority",
