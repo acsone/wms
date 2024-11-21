e419207a0aa4acdef892f0f2c70fc4cbe9ebd4f3
Denis Roussel <denis.roussel@acsone.eu>
2024-11-21 09:04:48 +0100

    Bump version: 2.18.12 → 2.18.13

--- odoo/addons/alce_all/__manifest__.py
+++ odoo/addons/alce_all/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Alc All Enterprise",
     "description": """
         Alcyon addons under Odoo Enterprise licence""",
-    "version": "16.0.2.18.12",
+    "version": "16.0.2.18.13",
     "license": "Other proprietary",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
