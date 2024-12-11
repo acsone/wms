a59b2684fc6feee31f19568819461bfb110d92bd
Laurent Mignonn (ACSONE) <laurent.mignon@acsone.eu>
2024-12-11 11:06:22 +0100

    [FIX] alc_eshop_api_orders: Only return normal orders
    refs #80639

--- odoo/addons/alc_eshop_api_orders/routers/orders.py
+++ odoo/addons/alc_eshop_api_orders/routers/orders.py
@@ -47,6 +47,7 @@ def search(
         domain.append(
             ("sale_channel_id", "in", env["sale.channel"].sudo()._get_internal_ids())
         )
+    domain.append(("order_type", "=", "order"))
     model = env["sale.order"].sudo()
     total_count = model.search_count(domain)
     offset = per_page * (page - 1)
