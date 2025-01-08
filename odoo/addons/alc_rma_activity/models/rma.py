9cce27e564feeb7b2a88739702ba38633d68b19e
sbejaoui <souheil.bejaoui@acsone.eu>
2025-01-08 17:58:51 +0100

    [IMP] rma: create activity at confirmation
    refs: #79613

--- odoo/addons/alc_rma_activity/models/rma.py
+++ odoo/addons/alc_rma_activity/models/rma.py
@@ -7,10 +7,10 @@ from odoo.addons.rma.models.rma import Rma as RmaBase
 
 
 class Rma(RmaBase):
-    def _create_inventory_activity_reception(self):
+    def _create_inventory_activity(self):
         for rec in self:
-            if rec.operation_id.create_inventory_activity_reception:
-                summary = _("Inventory actions required after reception")
+            if rec.operation_id.create_inventory_activity:
+                summary = _("Inventory actions required")
                 self.activity_schedule(
                     "mail.mail_activity_data_todo",
                     date_deadline=None,
@@ -18,13 +18,7 @@ class Rma(RmaBase):
                     user_id=rec.user_id.id if rec.user_id else rec.create_uid.id,
                 )
 
-    def _create_inventory_activity_delivery(self):
-        for rec in self:
-            if rec.operation_id.create_inventory_activity_delivery:
-                summary = _("Inventory actions required after delivery")
-                self.activity_schedule(
-                    "mail.mail_activity_data_todo",
-                    date_deadline=None,
-                    summary=summary,
-                    user_id=rec.user_id.id if rec.user_id else rec.create_uid.id,
-                )
+    def action_confirm(self):
+        res = super().action_confirm()
+        self._create_inventory_activity()
+        return res
--- odoo/addons/alc_rma_activity/tests/test_rma_activity.py
+++ odoo/addons/alc_rma_activity/tests/test_rma_activity.py
@@ -1,51 +1,20 @@
 # Copyright 2024 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import Form
 
 from odoo.addons.rma.tests.test_rma import TestRma
 
 
 class TestRmaActivity(TestRma):
-    def test_receive(self):
-        self.operation.create_inventory_activity_reception = True
+    def test_00(self):
+        self.operation.create_inventory_activity = True
         rma = self._create_rma(self.partner, self.product, 10, self.rma_loc)
         rma.action_confirm()
-        rma.reception_move_id.quantity_done = 10
-        rma.reception_move_id.picking_id._action_done()
         activity = self.env["mail.activity"].search(
             [
                 ("res_model_id", "=", self.env["ir.model"]._get("rma").id),
                 ("res_id", "=", rma.id),
-                ("summary", "=", "Inventory actions required after reception"),
-            ]
-        )
-        self.assertEqual(len(activity), 1, "Activity was not created after reception.")
-
-    def test_replace(self):
-        self.operation.create_inventory_activity_delivery = True
-        rma = self._create_confirm_receive(self.partner, self.product, 10, self.rma_loc)
-        product_2 = self.product_product.create(
-            {"name": "Product 2 test", "type": "product"}
-        )
-        delivery_form = Form(
-            self.env["rma.delivery.wizard"].with_context(
-                active_ids=rma.ids,
-                rma_delivery_type="replace",
-            )
-        )
-        delivery_form.product_id = product_2
-        delivery_form.product_uom_qty = 2
-        delivery_wizard = delivery_form.save()
-        delivery_wizard.action_deliver()
-        picking = rma.delivery_move_ids.picking_id
-        picking.move_ids.quantity_done = 8
-        picking.button_validate()
-        activity = self.env["mail.activity"].search(
-            [
-                ("res_model_id", "=", self.env["ir.model"]._get("rma").id),
-                ("res_id", "=", rma.id),
-                ("summary", "=", "Inventory actions required after delivery"),
+                ("summary", "=", "Inventory actions required"),
             ]
         )
         self.assertEqual(len(activity), 1, "Activity was not created after reception.")
