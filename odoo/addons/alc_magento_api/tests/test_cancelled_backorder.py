2e962843cb0efdd6f4703c2bac078f0bd2a452a6
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2025-04-15 17:06:40 +0200

    [FIX] alc_magento_api: Improves tests repeatability

--- odoo/addons/alc_magento_api/tests/test_cancelled_backorder.py
+++ odoo/addons/alc_magento_api/tests/test_cancelled_backorder.py
@@ -11,20 +11,24 @@ class TestCancelledBackorder(TestFacadePickings):
     def test_backorders_cancelled(self):
         cancelled_backoder_facade = self._get_service_facade("cancelled_backorder")
         result, _error, _location = cancelled_backoder_facade(date="1900-01-01")
+        # use search to get the ids in the order expected by the facade
+        pickings_cancelled = self.env["stock.picking"].search(
+            [("id", "in", [self.picking_half.id, self.picking_cancel.id])]
+        )
         expeced_result = f"""<?xml version="1.0" encoding="UTF-8" ?>
         <backorders_cancelled>
-            <order><internal_order_id>{self.picking_half.id}</internal_order_id>
+            <order><internal_order_id>{pickings_cancelled[0].id}</internal_order_id>
                 <item>
                     <qty_ordered>1.0</qty_ordered>
-                    <date_cancelled>{self.picking_half.date.date()}</date_cancelled>
+                    <date_cancelled>{pickings_cancelled[0].date.date()}</date_cancelled>
                     <sku>CNL</sku>
                 </item>
             </order>
             <order>
-                <internal_order_id>{self.picking_cancel.id}</internal_order_id>
+                <internal_order_id>{pickings_cancelled[1].id}</internal_order_id>
                 <item>
                     <qty_ordered>1.0</qty_ordered>
-                    <date_cancelled>{self.picking_cancel.date.date()}</date_cancelled>
+                    <date_cancelled>{pickings_cancelled[1].date.date()}</date_cancelled>
                     <sku>CNL</sku>
                 </item>
             </order>
