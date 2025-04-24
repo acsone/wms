87d191aaad217c5dac9e424a927527bc83c3b6b0
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2025-04-24 15:08:13 +0200

    [IMP] alc_product_abc_classification_link: rewrite migration script using sql instead of the orm. Ref #83816

--- odoo/addons/alc_product_abc_classification_link/migrations/16.0.1.0.1/pre-migrate.py
+++ odoo/addons/alc_product_abc_classification_link/migrations/16.0.1.0.1/pre-migrate.py
@@ -3,8 +3,6 @@
 
 import logging
 
-from odoo import SUPERUSER_ID, api
-
 _logger = logging.getLogger(__name__)
 
 
@@ -12,12 +10,21 @@ def migrate(cr, version):
     _logger.info(
         "Ensures the field `abc_storage` matches the first element of the field `abc_classification_product_level_ids` on product templates."
     )
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    templates = env["product.template"].search([])
-    for template in templates:
-        if template.abc_classification_product_level_ids:
-            template.abc_storage = template.abc_classification_product_level_ids[
-                0
-            ].level_id.name
-        else:
-            template.abc_storage = "b"
+    query = """
+        WITH first_classification AS (
+            SELECT DISTINCT ON (pt.id)
+                pt.id AS product_template_id,
+                COALESCE(acl.name, 'b') AS classification_name
+            FROM
+                product_template pt
+            LEFT JOIN product_product pp ON pp.product_tmpl_id = pt.id
+            LEFT JOIN abc_classification_product_level acpl ON acpl.product_id = pp.id
+            LEFT JOIN abc_classification_level acl ON acpl.level_id = acl.id
+            ORDER BY pt.id, acpl.id ASC
+        )
+        UPDATE product_template pt
+        SET abc_storage = fc.classification_name
+        FROM first_classification fc
+        WHERE pt.id = fc.product_template_id;
+    """
+    cr.execute(query)
