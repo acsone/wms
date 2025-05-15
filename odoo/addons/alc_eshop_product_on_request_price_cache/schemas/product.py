61befbbffb702ff24c52404590917636fff49f4f
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2025-05-15 16:56:30 +0200

    [ADD] alc_eshop_product_on_request_price_cache: add a module to prevent price info to be exported in case of quotation only.
    refs #84354

---
+++ odoo/addons/alc_eshop_product_on_request_price_cache/schemas/product.py
@@ -0,0 +1,16 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from odoo.addons.shopinvader_product.schemas.product import (
+    ProductProduct as BaseProductProduct,
+)
+
+
+class ProductProduct(BaseProductProduct, extends=True):
+    @classmethod
+    def from_product_product(cls, odoo_rec):
+        obj = super().from_product_product(odoo_rec)
+        if odoo_rec.shop_order_mode == "quotation_only":
+            obj.price = {}
+        return obj
