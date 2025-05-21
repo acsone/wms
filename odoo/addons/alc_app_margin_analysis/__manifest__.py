d9cee8130efae25e3cab02a54cb3a2858ac5f84e
Denis Roussel <denis.roussel@acsone.eu>
2025-05-21 14:04:22 +0200

    [FIX] Restore users in margin read group
    refs #84471

--- odoo/addons/alc_app_margin_analysis/__manifest__.py
+++ odoo/addons/alc_app_margin_analysis/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Alc App Margin Analysis",
     "description": """
         Add sale margin capabitlities to odoo""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://www.acsone.eu",
