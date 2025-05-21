68a0f467ae1ee3c1d37052ab73a5de226b920ea2
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2025-05-21 15:57:27 +0200

    [ADD] alc_eshop_cart_no_quotation_only_products: add a module to prevent the confirmation of a cart if there are "quotation_only" products inside it.
    refs #84501

---
+++ odoo/addons/alc_eshop_cart_no_quotation_only_products/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Eshop Cart No Quotation Only Products",
+    "summary": """Makes impossible to confirm a cart if one of the products has a shop_order_mode set to “quotation_only”.""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": [
+        # Third-party
+        "shopinvader_product_order_mode_on_request",
+    ],
+    "data": [],
+    "demo": [],
+}
