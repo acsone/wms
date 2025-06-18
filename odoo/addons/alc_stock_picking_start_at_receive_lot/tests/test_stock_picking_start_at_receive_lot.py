2fba1a40a9a518632ca137bb5b805b13928fe77b
sbejaoui <souheil.bejaoui@acsone.eu>
2025-06-18 11:31:52 +0200

    [IMP] stock_picking_start: Avoid rewriting started if not modified
    refs: #85129

--- odoo/addons/alc_stock_picking_start_at_receive_lot/tests/test_stock_picking_start_at_receive_lot.py
+++ odoo/addons/alc_stock_picking_start_at_receive_lot/tests/test_stock_picking_start_at_receive_lot.py
@@ -1,11 +1,13 @@
 # Copyright 2025 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from odoo.tests.common import TransactionCase
+
 from odoo.addons.alc_stock_receive_lot.tests.common import PackOperationLotAddCommon
 
 
-class TestStockPickingStartAtReceiveLot(PackOperationLotAddCommon):
+class TestStockPickingStartAtReceiveLot(PackOperationLotAddCommon, TransactionCase):
     def test_1(self):
         self.assertFalse(self.picking.started)
-        self.button_receive()
+        self.picking.button_receive()
         self.assertTrue(self.picking.started)
