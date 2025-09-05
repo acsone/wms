d9cc66460c619ee649b9c0811f27d5beac3ff91a
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2025-09-05 16:58:05 +0200

    [IMP] shopfloor_location_destination_suggestion_mobile: More relevant title
    Makes the suggestion part of the title for the location destination card

--- shopfloor_location_destination_suggestion_mobile/static/scenario/cluster_picking.js
+++ shopfloor_location_destination_suggestion_mobile/static/scenario/cluster_picking.js
@@ -19,3 +19,32 @@ methods.location_operation_detail_fields = function () {
     ];
     return new_result;
 };
+
+const created_original =
+    location_operation_detail_component.extendOptions.created || function () {};
+
+const created_method = function () {
+    created_original.bind(this)();
+    // Manipulate the 'record' object before the component is rendered
+    if (this.record) {
+        const location_dest_name = this.record.location_dest?.name || "";
+        if (this.record.location_destination_suggestions) {
+            this.record.title =
+                location_dest_name +
+                " => " +
+                this.record.location_destination_suggestions;
+        } else {
+            this.record.title = this.record.location_dest?.name || "";
+        }
+    }
+};
+location_operation_detail_component.extendOptions.created = created_method;
+
+// override the title to display the destination location name and suggestion
+const location_operation_detail_options_method =
+    methods.location_operation_detail_options;
+methods.location_operation_detail_options = function () {
+    const result = location_operation_detail_options_method.bind(this)();
+    result.key_title = "title";
+    return result;
+};
